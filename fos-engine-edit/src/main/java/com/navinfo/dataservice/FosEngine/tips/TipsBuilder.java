032f96ee498684f650732b474552574b46fcdefd
lilei3774 <lilei3774@navinfo.com>
2015-12-18 17:10:36 +0800

    添加在导入tips前初始化和销毁方法

--- fos-engine-edit/src/main/java/com/navinfo/dataservice/FosEngine/tips/TipsBuilder.java
+++ fos-engine-edit/src/main/java/com/navinfo/dataservice/FosEngine/tips/TipsBuilder.java
@@ -1,6 +1,7 @@
 package com.navinfo.dataservice.FosEngine.tips;
 
 import java.io.IOException;
+import java.sql.Statement;
 
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
@@ -12,7 +13,6 @@ import org.apache.hadoop.hbase.client.Table;
 import com.navinfo.dataservice.FosEngine.comm.db.HBaseAddress;
 import com.navinfo.dataservice.FosEngine.comm.db.OracleAddress;
 import com.navinfo.dataservice.FosEngine.comm.service.ProgressService;
-import com.navinfo.dataservice.FosEngine.edit.model.bean.rd.restrict.RdRestriction;
 import com.navinfo.dataservice.FosEngine.tips.Rotary.RotaryTipsBuilder;
 import com.navinfo.dataservice.FosEngine.tips.bridge.BridgeTipsBuilder;
 import com.navinfo.dataservice.FosEngine.tips.connexity.RdLaneConnexityTipsBuilder;
@@ -30,7 +30,7 @@ import com.navinfo.dataservice.FosEngine.tips.speedLimit.RdSpeedLimitTipsBuilder
  * 创建Tips
  */
 public class TipsBuilder {
-	
+
 	/**
 	 * 初始化
 	 * 
@@ -38,7 +38,7 @@ public class TipsBuilder {
 	 * @param tabName
 	 * @throws IOException
 	 */
-	public static void createTabIfNotExists(Connection connection,
+	private static void createTabIfNotExists(Connection connection,
 			String tabName) throws IOException {
 
 		Admin admin = connection.getAdmin();
@@ -57,6 +57,38 @@ public class TipsBuilder {
 	}
 
 	/**
+	 * 在tips导入前，对需要组合的数据进行合并组织
+	 * 
+	 * @param conn
+	 */
+	private static void prepareAuxData(java.sql.Connection conn) throws Exception {
+
+		String sql = "create table tmp_tips_order as  select in_link_pid,        node_pid,        row_number() over(partition by in_link_pid, node_pid order by seq_order) seq_order,        count(*) over(partition by in_link_pid, node_pid) cnt   from (select in_link_pid, node_pid, 2 seq_order           from rd_restriction         union all         select in_link_pid, node_pid, 8           from rd_branch         union all         select in_link_pid, node_pid, 1           from rd_lane_connexity)";
+
+		Statement stmt = conn.createStatement();
+		
+		stmt.execute(sql);
+		
+		stmt.close();
+	}
+
+	/**
+	 * 销毁临时使用的辅助数据
+	 * 
+	 * @param conn
+	 */
+	private static void destroyAuxData(java.sql.Connection conn)  throws Exception {
+
+		String sql = "drop table tmp_tips_order purge";
+		
+		Statement stmt = conn.createStatement();
+		
+		stmt.execute(sql);
+		
+		stmt.close();
+	}
+
+	/**
 	 * 执行创建Tips
 	 * 
 	 * @param fmgdbOA
@@ -70,65 +102,70 @@ public class TipsBuilder {
 	 */
 	public boolean run(OracleAddress fmgdbOA, OracleAddress pmOA, String uuid)
 			throws Exception {
-		
-		Connection hbaseConn = HBaseAddress.getHBaseConnection();
-		
-		createTabIfNotExists(hbaseConn, "tips");
-		
-		Table htab = hbaseConn.getTable(TableName.valueOf("tips"));
 
-		ProgressService progressManager = new ProgressService(pmOA.getConn(), uuid);
+		Connection hbaseConn = HBaseAddress.getHBaseConnection();
+
+		createTabIfNotExists(hbaseConn, "tips");
+
+		Table htab = hbaseConn.getTable(TableName.valueOf("tips"));
 		
+		prepareAuxData(pmOA.getConn());
+
+		ProgressService progressManager = new ProgressService(pmOA.getConn(),
+				uuid);
+
 		progressManager.updateProgress("完成度:" + 10 + "%");
-		
+
 		BridgeTipsBuilder.importTips(fmgdbOA.getConn(), htab);
-		
+
 		progressManager.updateProgress("完成度:" + 15 + "%");
 
 		RdLaneConnexityTipsBuilder.importTips(fmgdbOA.getConn(), htab);
-		
+
 		progressManager.updateProgress("完成度:" + 25 + "%");
-		
+
 		ConstructTipsBuilder.importTips(fmgdbOA.getConn(), htab);
-		
+
 		progressManager.updateProgress("完成度:" + 30 + "%");
-		
+
 		RdCrossTipsBuilder.importTips(fmgdbOA.getConn(), htab);
-		
+
 		progressManager.updateProgress("完成度:" + 35 + "%");
-		
+
 		DirectTipsBuilder.importTips(fmgdbOA.getConn(), htab);
-		
+
 		progressManager.updateProgress("完成度:" + 40 + "%");
-		
+
 		RdElectTipsBuilder.importTips(fmgdbOA.getConn(), htab);
-		
+
 		progressManager.updateProgress("完成度:" + 45 + "%");
-		
+
 		ForbiCrossTipsBuilder.importTips(fmgdbOA.getConn(), htab);
-		
+
 		progressManager.updateProgress("完成度:" + 50 + "%");
-		
+
 		HighwayTipsBuilder.importTips(fmgdbOA.getConn(), htab);
-		
+
 		progressManager.updateProgress("完成度:" + 55 + "%");
-		
+
 		LinkNameTipsBuilder.importTips(fmgdbOA.getConn(), htab);
-		
+
 		progressManager.updateProgress("完成度:" + 60 + "%");
-		
+
 		RdRestrictionTipsBuilder.importTips(fmgdbOA.getConn(), htab);
-		
+
 		progressManager.updateProgress("完成度:" + 70 + "%");
-		
+
 		RotaryTipsBuilder.importTips(fmgdbOA.getConn(), htab);
-		
+
 		progressManager.updateProgress("完成度:" + 80 + "%");
-		
+
 		RdSpeedLimitTipsBuilder.importTips(fmgdbOA.getConn(), htab);
-		
+
 		progressManager.updateProgress("完成度:" + 100 + "%");
 		
+		destroyAuxData(pmOA.getConn());
+
 		return false;
 	}
 

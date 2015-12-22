1c9f77cb11322b5f28a699b0f14d87f4877c6c7b
lilei3774 <lilei3774@navinfo.com>
2015-12-22 19:05:11 +0800

    添加solr服务

---
+++ solr-core/src/main/java/com/navinfo/dataservice/solr/core/App.java
@@ -0,0 +1,13 @@
+package com.navinfo.dataservice.solr.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

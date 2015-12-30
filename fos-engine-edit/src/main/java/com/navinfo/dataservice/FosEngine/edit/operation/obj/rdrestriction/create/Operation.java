0bc5f4969f61eaeaaa4a96aa0e99f9a6362d5a8b
wangshishuai3966 <wangshishuai3966@navinfo.com>
2015-12-30 14:17:52 +0800

    修改交限的创建

--- fos-engine-edit/src/main/java/com/navinfo/dataservice/FosEngine/edit/operation/obj/rdrestriction/create/Operation.java
+++ fos-engine-edit/src/main/java/com/navinfo/dataservice/FosEngine/edit/operation/obj/rdrestriction/create/Operation.java
@@ -1,7 +1,10 @@
 package com.navinfo.dataservice.FosEngine.edit.operation.obj.rdrestriction.create;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,9 +15,9 @@ import com.navinfo.dataservice.FosEngine.edit.model.bean.rd.restrict.RdRestricti
 import com.navinfo.dataservice.FosEngine.edit.model.bean.rd.restrict.RdRestrictionDetail;
 import com.navinfo.dataservice.FosEngine.edit.model.bean.rd.restrict.RdRestrictionVia;
 import com.navinfo.dataservice.FosEngine.edit.operation.IOperation;
-import com.navinfo.dataservice.FosEngine.edit.operation.obj.rdrestriction.Helper;
 import com.navinfo.dataservice.commons.geom.AngleCalculator;
 import com.navinfo.dataservice.commons.service.PidService;
+import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.LineSegment;
 
 public class Operation implements IOperation {
@@ -59,29 +62,23 @@ public class Operation implements IOperation {
 
 		restrict.setNodePid(command.getNodePid());
 
-		List<Integer> outLinkPids = null;
+		List<Integer> outLinkPids = command.getOutLinkPids();
 
-		if (command.getOutLinkPids() != null) {
-			outLinkPids = command.getOutLinkPids();
-		}
+		this.calViaLinks(command.getInLinkPid(), command.getNodePid(),
+				outLinkPids);
 
-		else {
-			outLinkPids = Helper.calOutLinks(conn, command.getInLinkPid(),
-					command.getNodePid());
-		}
-		
-		Helper.calViaLinks(conn, command.getInLinkPid(), command.getNodePid(),
-				outLinkPids, inLinkSegment, outLinkSegmentMap, viaLinkPidMap, relationTypeMap);
+		this.calViaLinks(command.getInLinkPid(), command.getNodePid(),
+				outLinkPids);
 
 		List<IRow> details = new ArrayList<IRow>();
 
 		List<Integer> restricInfos = new ArrayList<Integer>();
-		
-		List<Integer> newRestricInfos = command.getRestricInfos();
-		
-		for (int outLinkPid : outLinkPids) {
+
+		for (int outLinkPid:outLinkPids) {
 
 			RdRestrictionDetail detail = new RdRestrictionDetail();
+
+			detail.setPid(PidService.getInstance().applyRestrictionDetailPid());
 
 			detail.setRestricPid(restrict.getPid());
 
@@ -93,23 +90,15 @@ public class Operation implements IOperation {
 			double angle = AngleCalculator.getAngle(inLinkSegment,
 					outLinkSegment);
 
-			int restricInfo = Helper.calRestricInfo(angle);
-			
-			if (newRestricInfos != null){
-				if (newRestricInfos.contains(restricInfo)){
-					newRestricInfos.remove(Integer.valueOf(restricInfo));
-				}
-				else{
-					continue;
-				}
-			}
-			
-			detail.setPid(PidService.getInstance().applyRestrictionDetailPid());
+			int restricInfo = this.calRestricInfo(angle);
 
 			detail.setRestricInfo(restricInfo);
+			
+			detail.setRelationshipType(relationTypeMap.get(detail.getOutLinkPid()));
 
-			detail.setRelationshipType(relationTypeMap.get(detail
-					.getOutLinkPid()));
+			if (!restricInfos.contains(restricInfo)) {
+				restricInfos.add(restricInfo);
+			}
 
 			List<Integer> viaLinkPids = viaLinkPidMap.get(detail
 					.getOutLinkPid());
@@ -138,41 +127,24 @@ public class Operation implements IOperation {
 			details.add(detail);
 
 		}
-		
-		if (newRestricInfos != null){
-			//找不到退出link的也要创建detail
-			for(Integer newRestricInfo : newRestricInfos){
-				RdRestrictionDetail detail = new RdRestrictionDetail();
-	
-				detail.setRestricPid(restrict.getPid());
-	
-				detail.setOutLinkPid(0);
-	
-				detail.setPid(PidService.getInstance().applyRestrictionDetailPid());
-	
-				detail.setRestricInfo(newRestricInfo);
-	
-				details.add(detail);
-			}
-		}
-		
+
 		restrict.setDetails(details);
 
 		if (restricInfos.size() > 0) {
 			StringBuilder sb = new StringBuilder();
-
+			
 			for (Integer restricInfo : restricInfos) {
 				sb.append("[");
-
+				
 				sb.append(restricInfo);
-
+				
 				sb.append("]");
-
+				
 				sb.append(",");
 			}
-
-			sb.deleteCharAt(sb.length() - 1);
-
+			
+			sb.deleteCharAt(sb.length()-1);
+			
 			restrict.setRestricInfo(sb.toString());
 		}
 
@@ -181,5 +153,145 @@ public class Operation implements IOperation {
 		return null;
 	}
 
+	/**
+	 * 计算经过线
+	 * 
+	 * @param inLinkPid
+	 * @param nodePid
+	 * @param outLinkPids
+	 * @throws Exception
+	 */
+	private void calViaLinks(int inLinkPid, int nodePid, List<Integer> outLinkPids)
+			throws Exception {
 
+		outLinkSegmentMap = new HashMap<Integer, LineSegment>();
+
+		viaLinkPidMap = new HashMap<Integer, List<Integer>>();
+
+		relationTypeMap = new HashMap<Integer, Integer>();
+
+		String sql = "select * from table(package_utils.get_restrict_points(:1,:2,:3))";
+
+		PreparedStatement pstmt = null;
+
+		ResultSet resultSet = null;
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setInt(1, inLinkPid);
+
+			pstmt.setInt(2, nodePid);
+
+			StringBuilder sb = new StringBuilder();
+
+			for (int pid : outLinkPids) {
+
+				sb.append(pid);
+
+				sb.append(",");
+			}
+			
+			sb.deleteCharAt(sb.length()-1);
+
+			pstmt.setString(3, sb.toString());
+
+			resultSet = pstmt.executeQuery();
+
+			while (resultSet.next()) {
+
+				if (inLinkSegment == null) {
+					String inNode1 = resultSet.getString("in_node1");
+
+					String inNode2 = resultSet.getString("in_node2");
+
+					String[] splits = inNode1.split(",");
+
+					Coordinate p1 = new Coordinate(Double.valueOf(splits[0]),
+							Double.valueOf(splits[1]));
+
+					splits = inNode2.split(",");
+
+					Coordinate p2 = new Coordinate(Double.valueOf(splits[0]),
+							Double.valueOf(splits[1]));
+
+					inLinkSegment = new LineSegment(p1, p2);
+				}
+
+				int outLinkPid = resultSet.getInt("link_pid");
+
+				int relationType = resultSet.getInt("relation_type");
+
+				relationTypeMap.put(outLinkPid, relationType);
+
+				String outNode1 = resultSet.getString("out_node1");
+
+				String outNode2 = resultSet.getString("out_node2");
+
+				String[] splits = outNode1.split(",");
+
+				Coordinate p1 = new Coordinate(Double.valueOf(splits[0]),
+						Double.valueOf(splits[1]));
+
+				splits = outNode2.split(",");
+
+				Coordinate p2 = new Coordinate(Double.valueOf(splits[0]),
+						Double.valueOf(splits[1]));
+
+				LineSegment line = new LineSegment(p1, p2);
+
+				outLinkSegmentMap.put(outLinkPid, line);
+
+				String viaPath = resultSet.getString("via_path");
+
+				List<Integer> viaLinks = new ArrayList<Integer>();
+
+				if (viaPath != null) {
+
+					splits = viaPath.split(",");
+
+					for (String s : splits) {
+						if (!s.equals("")) {
+							viaLinks.add(Integer.valueOf(s));
+						}
+					}
+
+				}
+
+				viaLinkPidMap.put(outLinkPid, viaLinks);
+			}
+
+		} catch (Exception e) {
+			throw e;
+
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+			} catch (Exception e) {
+			}
+
+		}
+
+	}
+
+	/**
+	 * 计算限制信息
+	 * 
+	 * @param angle
+	 * @return
+	 */
+	private int calRestricInfo(double angle) {
+		if (angle > 45 && angle <= 135) {
+			return 3;
+		} else if (angle > 135 && angle <= 225) {
+			return 4;
+		} else if (angle > 225 && angle <= 315) {
+			return 2;
+		} else {
+			return 1;
+		}
+
+	}
 }

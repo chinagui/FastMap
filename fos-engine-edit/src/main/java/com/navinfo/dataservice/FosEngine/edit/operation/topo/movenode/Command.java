c966b6616423c99069168b96e53a0d7b30424e32
lilei3774 <lilei3774@navinfo.com>
2015-12-31 17:29:19 +0800

    填充移动node操作command程序

--- fos-engine-edit/src/main/java/com/navinfo/dataservice/FosEngine/edit/operation/topo/movenode/Command.java
+++ fos-engine-edit/src/main/java/com/navinfo/dataservice/FosEngine/edit/operation/topo/movenode/Command.java
@@ -1,27 +1,77 @@
 package com.navinfo.dataservice.FosEngine.edit.operation.topo.movenode;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
 import com.navinfo.dataservice.FosEngine.edit.model.ObjType;
 import com.navinfo.dataservice.FosEngine.edit.operation.ICommand;
 import com.navinfo.dataservice.FosEngine.edit.operation.OperType;
 
 public class Command implements ICommand {
+	
+	private int nodePid;
+	
+	private double longitude;
+	
+	private double latitude;
+	
+	private int projectId;
+	
+	private String requester;
+	
+	private JSONArray mountLinks;
+	
+	public Command(JSONObject json,String requester,int projectId){
+		
+		this.nodePid = json.getInt("nodePid");
+		
+		this.longitude = json.getJSONObject("data").getDouble("longitude");
+		
+		this.latitude = json.getJSONObject("data").getDouble("latitude");
+		
+		this.projectId = projectId;
+		
+		this.mountLinks = json.getJSONArray("mountLinks");
+		
+	}
 
 	@Override
 	public OperType getOperType() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return OperType.MOVENODE;
 	}
 
 	@Override
 	public String getRequester() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return requester;
 	}
 
 	@Override
 	public ObjType getObjType() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return ObjType.RDLINK;
 	}
 
+	public int getNodePid() {
+		return nodePid;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public int getProjectId() {
+		return projectId;
+	}
+
+	public JSONArray getMountLinks() {
+		return mountLinks;
+	}
+
+	
 }

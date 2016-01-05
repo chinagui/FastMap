d282edf32d1397789988aca7afceac5b3432e381
lilei3774 <lilei3774@navinfo.com>
2016-01-05 13:25:50 +0800

    填充从项目库查询车信数据

--- fos-engine-edit/src/main/java/com/navinfo/dataservice/FosEngine/edit/search/RdLaneConnexitySearch.java
+++ fos-engine-edit/src/main/java/com/navinfo/dataservice/FosEngine/edit/search/RdLaneConnexitySearch.java
@@ -1,14 +1,35 @@
 package com.navinfo.dataservice.FosEngine.edit.search;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import net.sf.json.JSONObject;
+
+import oracle.spatial.geometry.JGeometry;
+import oracle.spatial.util.WKT;
+import oracle.sql.STRUCT;
 
 import com.navinfo.dataservice.FosEngine.edit.model.IObj;
 import com.navinfo.dataservice.FosEngine.edit.model.selector.rd.laneconnexity.RdLaneConnexitySelector;
 import com.navinfo.dataservice.commons.db.ConfigLoader;
 import com.navinfo.dataservice.commons.db.DBOraclePoolManager;
+import com.navinfo.dataservice.commons.geom.Geojson;
+import com.navinfo.dataservice.commons.mercator.MercatorProjection;
+import com.navinfo.dataservice.commons.util.DisplayUtils;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKTReader;
 
 public class RdLaneConnexitySearch implements ISearch {
+	private static final WKT wktSpatial = new WKT();
+	
+	private static final WKTReader wktReader = new WKTReader();
+
 
 	private Connection conn;
 
@@ -42,8 +63,109 @@ public class RdLaneConnexitySearch implements ISearch {
 	@Override
 	public List<SearchSnapshot> searchDataByTileWithGap(int x, int y, int z,
 			int gap) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+
+		List<SearchSnapshot> list = new ArrayList<SearchSnapshot>();
+
+		String sql = "with tmp1 as  (select link_pid, geometry     from rd_link    where sdo_relate(geometry,                     sdo_geometry(:1,                                  8307),                     'mask=anyinteract') = 'TRUE') select  /*+ index(c) */ a.pid,a.lane_info,b.geometry link_geom,c.geometry point_geom  from rd_lane_connexity a,tmp1 b,rd_node c where a.in_link_pid = b.link_pid and a.node_pid = c.node_pid";
+
+		PreparedStatement pstmt = null;
+
+		ResultSet resultSet = null;
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			
+			String wkt = MercatorProjection.getWktWithGap(x, y, z, gap);
+
+			pstmt.setString(1, wkt);
+
+			resultSet = pstmt.executeQuery();
+			
+			double px = MercatorProjection.tileXToPixelX(x);
+			
+			double py = MercatorProjection.tileYToPixelY(y);
+
+			while (resultSet.next()) {
+
+				SearchSnapshot snapshot = new SearchSnapshot();
+				
+				JSONObject jsonM = new JSONObject();
+
+				snapshot.setI(String.valueOf(resultSet.getInt("pid")));
+				
+				snapshot.setT(5);
+
+				jsonM.put("b",resultSet.getString("lane_info"));
+
+				STRUCT struct1 = (STRUCT) resultSet.getObject("link_geom");
+
+				JGeometry geom1 = JGeometry.load(struct1);
+
+				String linkWkt = new String(wktSpatial.fromJGeometry(geom1));
+
+				STRUCT struct2 = (STRUCT) resultSet.getObject("point_geom");
+
+				JGeometry geom2 = JGeometry.load(struct2);
+
+				String pointWkt = new String(wktSpatial.fromJGeometry(geom2));
+
+				int direct = getDirect(linkWkt, pointWkt);
+				
+				double angle = DisplayUtils.calIncloudedAngle(linkWkt, direct);
+
+				jsonM.put("c", String.valueOf((int)angle));
+
+				double[][] point = DisplayUtils
+						.getLinkPointPos(linkWkt, pointWkt, 1, 0);
+
+				snapshot.setG(Geojson.lonlat2Pixel(point[1][0],point[1][1],z,px,py));
+				
+				snapshot.setM(jsonM);
+
+				list.add(snapshot);
+			}
+		} catch (Exception e) {
+			
+			throw new SQLException(e);
+		} finally {
+			if (resultSet != null) {
+				try {
+					resultSet.close();
+				} catch (Exception e) {
+					
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e) {
+					
+				}
+			}
+
+		}
+
+		return list;
+	}
+	
+	private static int getDirect(String linkWkt,String pointWkt) throws ParseException{
+		
+		int direct = 2;
+		
+		Geometry link = wktReader.read(linkWkt);
+		
+		Geometry point = wktReader.read(pointWkt);
+		
+		Coordinate[] csLink = link.getCoordinates();
+		
+		Coordinate cPoint = point.getCoordinate();
+		
+		if (csLink[0].x != cPoint.x || csLink[1].y != cPoint.y){
+			direct = 3;
+		}
+		
+		return direct;
 	}
 	
 	public static void main(String[] args) throws Exception {

38baef804d5137289310e78154a69388e99d489d
wangshishuai3966 <wangshishuai3966@navinfo.com>
2016-01-05 10:47:50 +0800

    修改导出道路底图程序，添加rdnode

--- fos-engine-edit/src/main/java/com/navinfo/dataservice/FosEngine/export/ExportBaseData.java
+++ fos-engine-edit/src/main/java/com/navinfo/dataservice/FosEngine/export/ExportBaseData.java
@@ -38,8 +38,8 @@ public class ExportBaseData {
 	 */
 	public static void exportRdLine(OracleAddress oa, String dir)
 			throws Exception {
-		String sql = "select a.*,        display_text.name,        styleFactors1.types,        styleFactors2.lane_types,        speedlimits.from_speed_limit,        speedlimits.to_speed_limit,        forms.forms   from rd_link a,        (select a.link_pid, b.name           from rd_link_name a, rd_name b          where a.name_class = 1            and a.seq_num = 1            and a.name_groupid = b.name_groupid            and b.lang_code = 'CHI'            and a.u_record != 2) display_text,        (select link_pid,                listagg(type, ',') within group(order by type) types           from (select a.link_pid, type                   from rd_link_limit a                  where type in (0, 4, 5, 6) and a.u_record != 2)          group by link_pid) styleFactors1,        (select link_pid,                listagg(lane_type, ',') within group(order by lane_type) lane_types           from rd_lane a          where a.u_record != 2          group by link_pid) styleFactors2,        (select link_pid, from_speed_limit, to_speed_limit           from rd_link_speedlimit a          where speed_type = 0            and a.u_record != 2) speedlimits,        (select link_pid,                listagg(form_of_way, ',') within group(order by form_of_way) forms           from rd_link_form          where u_record != 2          group by link_pid) forms  where a.link_pid = display_text.link_pid(+)    and a.link_pid = styleFactors1.link_pid(+)    and a.link_pid = styleFactors2.link_pid(+)    and a.link_pid = speedlimits.link_pid(+)    and a.link_pid = forms.link_pid(+)    and a.u_record != 2";
-
+		String sql = "select a.*,        display_text.name,        styleFactors1.types,        styleFactors2.lane_types,        speedlimits.from_speed_limit,        speedlimits.to_speed_limit,        forms.forms   from rd_link a,        (select max(name1)||'/'||max(name2)||'/'||max(name3)||'/'||max(name4) name,link_pid from ( select a.link_pid, case when name_class=1 and seq_num=1 then b.name else null end as name1,  case when name_class=1 and seq_num=2 then b.name else null end as name2,  case when name_class=2 and seq_num=1 then b.name else null end as name3,    case when name_class=2 and seq_num=2 then b.name else null end as name4            from rd_link_name a, rd_name b          where a.name_groupid = b.name_groupid            and b.lang_code = 'CHI'            and a.u_record != 2) group by link_pid) display_text,        (select link_pid,                listagg(type, ',') within group(order by type) types           from (select a.link_pid, type                   from rd_link_limit a                  where type in (0, 4, 5, 6)                    and a.u_record != 2)          group by link_pid) styleFactors1,        (select link_pid,                listagg(lane_type, ',') within group(order by lane_type) lane_types           from rd_lane a          where a.u_record != 2          group by link_pid) styleFactors2,        (select link_pid, from_speed_limit, to_speed_limit           from rd_link_speedlimit a          where speed_type = 0            and a.u_record != 2) speedlimits,        (select link_pid,                listagg(form_of_way, ',') within group(order by form_of_way) forms           from rd_link_form          where u_record != 2          group by link_pid) forms  where a.link_pid = display_text.link_pid(+)    and a.link_pid = styleFactors1.link_pid(+)    and a.link_pid = styleFactors2.link_pid(+)    and a.link_pid = speedlimits.link_pid(+)    and a.link_pid = forms.link_pid(+)    and a.u_record != 2";
+		
 		File mkdirFile = new File(dir + "/tmp");
 
 		mkdirFile.mkdirs();
@@ -95,10 +95,41 @@ public class ExportBaseData {
 		String meshid = rs.getString("mesh_id");
 
 		json.put("meshid", meshid);
+		
+		JSONArray names = new JSONArray();
 
 		String name = rs.getString("name");
-
-		json.put("display_text", name != null ? name : JSONNull.getInstance());
+		
+		String display_text = "";
+		
+		if( name != null){
+		
+			String[] sss = name.split("/");
+			
+			boolean flag1=false;
+			
+			for(String s : sss){
+				if(s!=null){
+					if(flag1){
+						display_text+="/";
+					}
+					
+					display_text+=s;
+					
+					JSONObject namejson = new JSONObject();
+					
+					namejson.put("name", s);
+					
+					names.add(namejson);
+					
+					flag1=true;
+				}
+			}
+		}
+		
+		json.put("names", names);
+		
+		json.put("display_text", display_text);
 
 		int kind = rs.getInt("kind");
 
@@ -256,8 +287,7 @@ public class ExportBaseData {
 	public static void exportBaseData2Sqlite(OracleAddress oa, String dir)
 			throws Exception {
 
-		String oraSql = "select a.*,        display_text.name,        styleFactors1.types,        styleFactors2.lane_types,        speedlimits.from_speed_limit,        speedlimits.to_speed_limit,        forms.forms   from rd_link a,        (select a.link_pid, b.name           from rd_link_name a, rd_name b          where a.name_class = 1            and a.seq_num = 1            and a.name_groupid = b.name_groupid            and b.lang_code = 'CHI'            and a.u_record != 2) display_text,        (select link_pid,                listagg(type, ',') within group(order by type) types           from (select a.link_pid, type                   from rd_link_limit a                  where type in (0, 4, 5, 6) and a.u_record != 2)          group by link_pid) styleFactors1,        (select link_pid,                listagg(lane_type, ',') within group(order by lane_type) lane_types           from rd_lane a          where a.u_record != 2          group by link_pid) styleFactors2,        (select link_pid, from_speed_limit, to_speed_limit           from rd_link_speedlimit a          where speed_type = 0            and a.u_record != 2) speedlimits,        (select link_pid,                listagg(form_of_way, ',') within group(order by form_of_way) forms           from rd_link_form          where u_record != 2          group by link_pid) forms  where a.link_pid = display_text.link_pid(+)    and a.link_pid = styleFactors1.link_pid(+)    and a.link_pid = styleFactors2.link_pid(+)    and a.link_pid = speedlimits.link_pid(+)    and a.link_pid = forms.link_pid(+)    and a.u_record != 2";
-
+		
 		File mkdirFile = new File(dir + "/tmp");
 
 		mkdirFile.mkdirs();
@@ -274,7 +304,7 @@ public class ExportBaseData {
 
 		// create a database connection
 		sqliteConn = DriverManager.getConnection("jdbc:sqlite:" + dir
-				+ "/tmp/rdline.sqlite", config.toProperties());
+				+ "/tmp/gdbdata.sqlite", config.toProperties());
 		Statement stmt = sqliteConn.createStatement();
 		stmt.setQueryTimeout(30); // set timeout to 30 sec.
 
@@ -285,15 +315,32 @@ public class ExportBaseData {
 		// using v.2.4.0 this automatically initializes SPATIAL_REF_SYS and
 		// GEOMETRY_COLUMNS
 		stmt.execute("SELECT InitSpatialMetadata()");
-
+		
 		sqliteConn.setAutoCommit(false);
 
+		String operateDate = StringUtils.getCurrentTime();
+		
+		exportRdLine2Sqlite(sqliteConn, stmt, oa, operateDate);
+		
+		exportRdNode2Sqlite(sqliteConn, stmt, oa, operateDate);
+		
+		sqliteConn.close();
+
+		// 压缩文件
+		ZipUtils.zipFile(dir + "/tmp/", dir + "/" + operateDate + ".zip");
+
+		FileUtil.deleteDirectory(new File(dir + "/tmp"));
+
+	}
+	
+	public static void exportRdLine2Sqlite(Connection sqliteConn, Statement stmt, OracleAddress oa, String operateDate) throws Exception{
 		// creating a LINESTRING table
 		stmt.execute("create table gdb_rdLine(pid integer primary key)");
 		stmt.execute("select addgeometrycolumn('gdb_rdLine','geometry',4326,'GEOMETRY','XY')");
 		stmt.execute("select createspatialindex('gdb_rdLine','geometry')");
 		stmt.execute("alter table gdb_rdLine add display_style text;");
 		stmt.execute("alter table gdb_rdLine add display_text text;");
+		stmt.execute("alter table gdb_rdLine add names Blob;");
 		stmt.execute("alter table gdb_rdLine add meshid text;");
 		stmt.execute("alter table gdb_rdLine add kind integer;");
 		stmt.execute("alter table gdb_rdLine add direct integer;");
@@ -314,17 +361,17 @@ public class ExportBaseData {
 		stmt.execute("alter table gdb_rdLine add op_lifecycle integer;");
 
 		String insertSql = "insert into gdb_rdLine values("
-				+ "?, GeomFromText(?, 4326), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+				+ "?, GeomFromText(?, 4326), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 		PreparedStatement prep = sqliteConn.prepareStatement(insertSql);
-
-		String operateDate = StringUtils.getCurrentTime();
 
 		Connection conn = oa.getConn();
 
 		Statement stmt2 = conn.createStatement();
-
-		ResultSet resultSet = stmt2.executeQuery(oraSql);
+		
+		String sql = "select a.*,        display_text.name,        styleFactors1.types,        styleFactors2.lane_types,        speedlimits.from_speed_limit,        speedlimits.to_speed_limit,        forms.forms   from rd_link a,        (select max(name1)||'/'||max(name2)||'/'||max(name3)||'/'||max(name4) name,link_pid from ( select a.link_pid, case when name_class=1 and seq_num=1 then b.name else null end as name1,  case when name_class=1 and seq_num=2 then b.name else null end as name2,  case when name_class=2 and seq_num=1 then b.name else null end as name3,    case when name_class=2 and seq_num=2 then b.name else null end as name4            from rd_link_name a, rd_name b          where a.name_groupid = b.name_groupid            and b.lang_code = 'CHI'            and a.u_record != 2) group by link_pid) display_text,        (select link_pid,                listagg(type, ',') within group(order by type) types           from (select a.link_pid, type                   from rd_link_limit a                  where type in (0, 4, 5, 6)                    and a.u_record != 2)          group by link_pid) styleFactors1,        (select link_pid,                listagg(lane_type, ',') within group(order by lane_type) lane_types           from rd_lane a          where a.u_record != 2          group by link_pid) styleFactors2,        (select link_pid, from_speed_limit, to_speed_limit           from rd_link_speedlimit a          where speed_type = 0            and a.u_record != 2) speedlimits,        (select link_pid,                listagg(form_of_way, ',') within group(order by form_of_way) forms           from rd_link_form          where u_record != 2          group by link_pid) forms  where a.link_pid = display_text.link_pid(+)    and a.link_pid = styleFactors1.link_pid(+)    and a.link_pid = styleFactors2.link_pid(+)    and a.link_pid = speedlimits.link_pid(+)    and a.link_pid = forms.link_pid(+)    and a.u_record != 2";
+		
+		ResultSet resultSet = stmt2.executeQuery(sql);
 
 		resultSet.setFetchSize(5000);
 
@@ -351,51 +398,56 @@ public class ExportBaseData {
 			prep.setString(3, "");
 
 			prep.setString(4, json.getString("display_text"));
+			
+			byte[] names = json.getString("names").getBytes();
 
-			prep.setString(5, json.getString("meshid"));
+			prep.setBinaryStream(5, new ByteArrayInputStream(names),
+					names.length);
+			
+			prep.setString(6, json.getString("meshid"));
 
-			prep.setInt(6, json.getInt("kind"));
+			prep.setInt(7, json.getInt("kind"));
 
-			prep.setInt(7, json.getInt("direct"));
+			prep.setInt(8, json.getInt("direct"));
 
-			prep.setInt(8, json.getInt("appInfo"));
+			prep.setInt(9, json.getInt("appInfo"));
 
-			prep.setInt(9, json.getInt("tollInfo"));
+			prep.setInt(10, json.getInt("tollInfo"));
 
-			prep.setInt(10, json.getInt("multiDigitized"));
+			prep.setInt(11, json.getInt("multiDigitized"));
 
-			prep.setInt(11, json.getInt("specialTraffic"));
+			prep.setInt(12, json.getInt("specialTraffic"));
 
-			prep.setInt(12, json.getInt("fc"));
+			prep.setInt(13, json.getInt("fc"));
 
-			prep.setInt(13, json.getInt("laneNum"));
+			prep.setInt(14, json.getInt("laneNum"));
 
-			prep.setInt(14, json.getInt("laneLeft"));
+			prep.setInt(15, json.getInt("laneLeft"));
 
-			prep.setInt(15, json.getInt("laneRight"));
+			prep.setInt(16, json.getInt("laneRight"));
 
-			prep.setInt(16, json.getInt("isViaduct"));
+			prep.setInt(17, json.getInt("isViaduct"));
 
-			prep.setInt(17, json.getInt("paveStatus"));
+			prep.setInt(18, json.getInt("paveStatus"));
 
 			byte[] forms = json.getString("forms").getBytes();
 
-			prep.setBinaryStream(18, new ByteArrayInputStream(forms),
+			prep.setBinaryStream(19, new ByteArrayInputStream(forms),
 					forms.length);
 
 			byte[] styleFactors = json.getString("styleFactors").getBytes();
 
-			prep.setBinaryStream(19, new ByteArrayInputStream(styleFactors),
+			prep.setBinaryStream(20, new ByteArrayInputStream(styleFactors),
 					styleFactors.length);
 
 			byte[] speedLimit = json.getString("speedLimit").getBytes();
 
-			prep.setBinaryStream(20, new ByteArrayInputStream(speedLimit),
+			prep.setBinaryStream(21, new ByteArrayInputStream(speedLimit),
 					speedLimit.length);
 
-			prep.setString(21, json.getString("op_date"));
+			prep.setString(22, json.getString("op_date"));
 
-			prep.setInt(22, json.getInt("op_lifecycle"));
+			prep.setInt(23, json.getInt("op_lifecycle"));
 
 			prep.executeUpdate();
 
@@ -407,26 +459,123 @@ public class ExportBaseData {
 		}
 
 		sqliteConn.commit();
+	}
+	
+	public static void exportRdNode2Sqlite(Connection sqliteConn, Statement stmt, OracleAddress oa, String operateDate) throws Exception{
+		// creating a LINESTRING table
+		stmt.execute("create table gdb_rdNode(pid integer primary key)");
+		stmt.execute("select addgeometrycolumn('gdb_rdNode','geometry',4326,'GEOMETRY','XY')");
+		stmt.execute("select createspatialindex('gdb_rdNode','geometry')");
+		stmt.execute("alter table gdb_rdNode add display_style text;");
+		stmt.execute("alter table gdb_rdNode add display_text text;");
+		stmt.execute("alter table gdb_rdNode add meshid text;");
+		stmt.execute("alter table gdb_rdNode add isMain integer;");
+		stmt.execute("alter table gdb_rdNode add op_date text;");
+		stmt.execute("alter table gdb_rdNode add op_lifecycle integer;");
 
-		sqliteConn.close();
+		String insertSql = "insert into gdb_rdNode values("
+				+ "?, GeomFromText(?, 4326), ?, ?, ?, ?, ?, ?)";
 
-		// 压缩文件
-		ZipUtils.zipFile(dir + "/tmp/", dir + "/" + operateDate + ".zip");
+		PreparedStatement prep = sqliteConn.prepareStatement(insertSql);
 
-		FileUtil.deleteDirectory(new File(dir + "/tmp"));
+		Connection conn = oa.getConn();
 
+		Statement stmt2 = conn.createStatement();
+		
+		String oraSql = "select b.node_pid,b.geometry,c.mesh_id,a.is_main from rd_cross_node a,rd_node b,rd_node_mesh c where c.node_pid=b.node_pid and a.node_pid=b.node_pid and a.u_record!=2 and b.u_record!=2";
+
+		ResultSet resultSet = stmt2.executeQuery(oraSql);
+
+		resultSet.setFetchSize(5000);
+
+//		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+		int count = 0;
+
+		while (resultSet.next()) {
+
+			JSONObject json = enclosingRdNode(resultSet, operateDate);
+
+			int pid = json.getInt("pid");
+
+//			if (map.containsKey(pid)) {
+//				continue;
+//			}
+
+//			map.put(pid, 0);
+
+			prep.setInt(1, pid);
+
+			prep.setString(2, json.getString("geometry"));
+
+			prep.setString(3, "");
+
+			prep.setString(4, "");
+
+			prep.setString(5, json.getString("meshid"));
+
+			prep.setInt(6, json.getInt("isMain"));
+
+			prep.setString(7, json.getString("op_date"));
+
+			prep.setInt(8, json.getInt("op_lifecycle"));
+
+			prep.executeUpdate();
+
+			count += 1;
+
+			if (count % 5000 == 0) {
+				sqliteConn.commit();
+			}
+		}
+
+		sqliteConn.commit();
+	}
+	
+	public static JSONObject enclosingRdNode(ResultSet rs, String operateDate)
+			throws Exception {
+
+		JSONObject json = new JSONObject();
+
+		int pid = rs.getInt("node_pid");
+
+		json.put("pid", pid);
+		
+		int isMain = rs.getInt("is_main");
+
+		json.put("isMain", isMain);
+
+		String meshid = rs.getString("mesh_id");
+
+		json.put("meshid", meshid);
+
+		STRUCT struct = (STRUCT) rs.getObject("geometry");
+
+		JGeometry geom = JGeometry.load(struct);
+
+		String geometry = new String(wkt.fromJGeometry(geom));
+
+		json.put("geometry", geometry);
+
+		json.put("op_date", operateDate);
+
+		json.put("op_lifecycle", 0);
+
+		return json;
 	}
 
 	public static void exportBaseData(OracleAddress oa, String dir)
 			throws Exception {
 
 		exportRdLine(oa, dir);
+		
+
 	}
 
 	public static void main(String[] args) throws Exception {
-		String username1 = "fmgdb14";
+		String username1 = "beijing11";
 
-		String password1 = "fmgdb14";
+		String password1 = "beijing11";
 
 		int port1 = 1521;
 
@@ -437,8 +586,8 @@ public class ExportBaseData {
 		OracleAddress oa1 = new OracleAddress(username1, password1, port1, ip1,
 				serviceName1);
 
-		 exportBaseData(oa1, "c:/1");
-		//exportBaseData2Sqlite(oa1, "./");
+		 //exportBaseData(oa1, "c:/1");
+		exportBaseData2Sqlite(oa1, "./");
 		 System.out.println("done");
 	}
 }

aac0eb17bc421db9a67507d01b25d3a75eff464a
lilei3774 <lilei3774@navinfo.com>
2016-01-26 18:47:46 +0800

    填充打断操作前检查

---
+++ fos-engine-edit/src/main/java/com/navinfo/dataservice/FosEngine/edit/operation/topo/breakpoint/Check.java
@@ -0,0 +1,50 @@
+package com.navinfo.dataservice.FosEngine.edit.operation.topo.breakpoint;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.HashSet;
+import java.util.Set;
+
+import net.sf.json.JSONObject;
+
+import com.navinfo.dataservice.commons.mercator.MercatorProjection;
+
+public class Check {
+
+	
+
+	// 路口组成link不允许打断
+	public void checkIsCrossLink(Connection conn, int linkPid) throws Exception {
+
+		String sql = "select node_pid from rd_cross_link where link_pid = :1 and rownum =1";
+
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+
+		pstmt.setInt(1, linkPid);
+
+		ResultSet resultSet = pstmt.executeQuery();
+
+		boolean flag = false;
+
+		if (resultSet.next()) {
+			flag = true;
+		}
+		
+		resultSet.close();
+
+		pstmt.close();
+
+		if (flag) {
+			
+			throwException("路口组成link不允许打断");
+		}
+	}
+	
+	
+
+	private void throwException(String msg) throws Exception {
+		throw new Exception(msg);
+	}
+
+}

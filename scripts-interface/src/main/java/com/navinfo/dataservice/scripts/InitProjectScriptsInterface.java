284b1488df5a3a80a6093e5f4f0edde222516f62
wangshishuai3966 <wangshishuai3966@navinfo.com>
2016-02-18 15:02:08 +0800

    建库脚本增加对project_grid表的赋值

--- scripts-interface/src/main/java/com/navinfo/dataservice/scripts/InitProjectScriptsInterface.java
+++ scripts-interface/src/main/java/com/navinfo/dataservice/scripts/InitProjectScriptsInterface.java
@@ -22,69 +22,72 @@ import com.navinfo.navicommons.database.QueryRunner;
 import com.navinfo.navicommons.database.sql.PackageExec;
 import com.navinfo.navicommons.database.sql.SqlExec;
 
-/** 
- * @ClassName: InitProjectScriptsInterface 
- * @author Xiao Xiaowen 
- * @date 2016-1-15 下午3:40:32 
+/**
+ * @ClassName: InitProjectScriptsInterface
+ * @author Xiao Xiaowen
+ * @date 2016-1-15 下午3:40:32
  * @Description: TODO
  */
 public class InitProjectScriptsInterface {
-	
-	public static JSONObject initProject(JSONObject request){
+
+	public static JSONObject initProject(JSONObject request) {
 		JSONObject response = new JSONObject();
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		Connection tarConn = null;
-		try{
-			String sourceDbId = (String)request.get("sourceDbId");
-			Assert.notNull(sourceDbId,"projectId不能为空");
-			String projectIdStr = (String)request.get("projectId");
-			Assert.notNull(projectIdStr,"projectId不能为空");
+		try {
+			String sourceDbId = (String) request.get("sourceDbId");
+			Assert.notNull(sourceDbId, "projectId不能为空");
+			String projectIdStr = (String) request.get("projectId");
+			Assert.notNull(projectIdStr, "projectId不能为空");
 			int projectId = Integer.valueOf(projectIdStr);
-			String dbName = (String)request.get("dbName");
-			Assert.notNull(dbName,"dbName不能为空");
-//			String gdbVersion = (String)request.get("gdbVersion");
-//			Assert.notNull(gdbVersion,"gdbVersion不能为空");
+			String dbName = (String) request.get("dbName");
+			Assert.notNull(dbName, "dbName不能为空");
+			// String gdbVersion = (String)request.get("gdbVersion");
+			// Assert.notNull(gdbVersion,"gdbVersion不能为空");
 			String gdbVersion = "240+";
-			String projectName = (String)request.get("projectName");
-			String meshes = (String)request.get("meshes");
-			meshes = com.navinfo.navicommons.utils.StringUtils.removeBlankChar(meshes);
-			Assert.notNull(meshes,"meshes不能为空");
+			String projectName = (String) request.get("projectName");
+			String meshes = (String) request.get("meshes");
+			meshes = com.navinfo.navicommons.utils.StringUtils
+					.removeBlankChar(meshes);
+			Assert.notNull(meshes, "meshes不能为空");
 
-			//create_db
+			// create_db
 			JSONObject createDbRequest = new JSONObject();
 			createDbRequest.put("name", dbName);
 			createDbRequest.put("type", "prjRoad");
 			createDbRequest.put("gdbVersion", gdbVersion);
-			JSONObject createDbResponse = ToolScriptsInterface.createDb(createDbRequest);
+			JSONObject createDbResponse = ToolScriptsInterface
+					.createDb(createDbRequest);
 			response.put("create_db", createDbResponse);
-			
-			//fm_man写记录
+
+			// fm_man写记录
 			String prjDbId = createDbResponse.getString("dbId");
 			QueryRunner runner = new QueryRunner();
-			conn = MultiDataSourceFactory.getInstance().getManDataSource().getConnection();
-			
-			
-			
-			//fm_man中写project记录
+			conn = MultiDataSourceFactory.getInstance().getManDataSource()
+					.getConnection();
+
+			// fm_man中写project记录
 			String sqlPrj = "INSERT INTO PROJECT_INFO(PROJECT_ID,PROJECT_NAME,DB_ID)VALUES(?,?,?)";
-			runner.update(conn, sqlPrj, projectId,projectName,Integer.valueOf(prjDbId));
-			//project_mesh
+			runner.update(conn, sqlPrj, projectId, projectName,
+					Integer.valueOf(prjDbId));
+			// project_mesh
 			String sqlMesh = "INSERT INTO PROJECT_MESH(PROJECT_ID,MESH_ID,MESH_TYPE,SYNC_TIME)VALUES(?,?,?,SYSDATE)";
 			stmt = conn.prepareStatement(sqlMesh);
 			Set<String> coreMeshes = new HashSet<String>();
 			CollectionUtils.addAll(coreMeshes, meshes.split(","));
-			for(String mesh:coreMeshes){
+			for (String mesh : coreMeshes) {
 				stmt.setInt(1, projectId);
 				stmt.setInt(2, Integer.valueOf(mesh));
 				stmt.setInt(3, 1);
 				stmt.addBatch();
 			}
+
 			Set<String> extendMeshes = MeshUtils.getNeighborMeshSet(coreMeshes);
-			//gei 导数据用
-			String allMeshesStr = StringUtils.join(extendMeshes,",");
+			// 给导数据用
+			String allMeshesStr = StringUtils.join(extendMeshes, ",");
 			extendMeshes.removeAll(coreMeshes);
-			for(String mesh:extendMeshes){
+			for (String mesh : extendMeshes) {
 				stmt.setInt(1, projectId);
 				stmt.setInt(2, Integer.valueOf(mesh));
 				stmt.setInt(3, 2);
@@ -92,12 +95,27 @@ public class InitProjectScriptsInterface {
 			}
 			stmt.executeBatch();
 			stmt.clearBatch();
-			//mesh表
+			stmt.close();
+			
+			// project_grid
+			String sqlGrid = "INSERT INTO PROJECT_GRID(PROJECT_ID,GRID_ID) VALUES(?,?)";
+			stmt = conn.prepareStatement(sqlGrid);
+			for (String mesh : coreMeshes) {
+				for (int i = 1; i < 5; i++) {
+					stmt.setInt(1, projectId);
+					stmt.setInt(2, Integer.valueOf(mesh+"0"+i));
+					stmt.addBatch();
+				}
+			}
+			stmt.executeBatch();
+			stmt.clearBatch();
+
+			// mesh表
 			String uSql = "UPDATE MESH SET PROJECT_ID=?,HANDLE_PROJECT_ID=? WHERE MESH_ID IN (SELECT MESH_ID FROM PROJECT_MESH WHERE PROJECT_ID=? AND MESH_TYPE=1)";
-			runner.update(conn, uSql, projectId,projectId,projectId);
+			runner.update(conn, uSql, projectId, projectId, projectId);
 			conn.commit();
 			response.put("init_fm_man", "success");
-			//export data
+			// export data
 			JSONObject expRequest = new JSONObject();
 			expRequest.put("exportMode", "copy");
 			expRequest.put("feature", "gdb");
@@ -107,11 +125,13 @@ public class InitProjectScriptsInterface {
 			expRequest.put("sourceDbId", sourceDbId);
 			expRequest.put("targetDbId", prjDbId);
 			expRequest.put("gdbVersion", gdbVersion);
-			JSONObject expResponse = ToolScriptsInterface.exportData(expRequest);
+			JSONObject expResponse = ToolScriptsInterface
+					.exportData(expRequest);
 			response.put("export_data", expResponse);
-			
-			//创建索引、包等等
-			OracleSchema schema = (OracleSchema)new DbManager().getDbById(Integer.valueOf(29));
+
+			// 创建索引、包等等
+			OracleSchema schema = (OracleSchema) new DbManager()
+					.getDbById(Integer.valueOf(29));
 			tarConn = schema.getDriverManagerDataSource().getConnection();
 			String sqlFile = "/com/navinfo/dataservice/scripts/resources/prj_utils.sql";
 			SqlExec sqlExec = new SqlExec(tarConn);
@@ -120,14 +140,14 @@ public class InitProjectScriptsInterface {
 			PackageExec packageExec = new PackageExec(tarConn);
 			packageExec.execute(pckFile);
 			response.put("prj_utils_install", "success");
-			
+
 			response.put("msg", "执行成功");
-		}catch(Exception e){
-			response.put("msg", "ERROR:"+e.getMessage());
+		} catch (Exception e) {
+			response.put("msg", "ERROR:" + e.getMessage());
 			DbUtils.closeQuietly(stmt);
 			DbUtils.rollbackAndCloseQuietly(conn);
 			DbUtils.rollbackAndCloseQuietly(tarConn);
-		}finally{
+		} finally {
 			DbUtils.closeQuietly(stmt);
 			DbUtils.commitAndCloseQuietly(conn);
 			DbUtils.commitAndCloseQuietly(tarConn);
@@ -139,18 +159,20 @@ public class InitProjectScriptsInterface {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		try{
-			JSONObject request=null;
+		try {
+			JSONObject request = null;
 			JSONObject response = null;
 			String dir = SystemConfig.getSystemConfig().getValue("scripts.dir");
-			request = ToolScriptsInterface.readJson(dir+"request"+File.separator+"init_project.json");
+			request = ToolScriptsInterface.readJson(dir + "request"
+					+ File.separator + "init_project.json");
 			response = initProject(request);
-			ToolScriptsInterface.writeJson(response,dir+"response"+File.separator+"init_project.json");
+			ToolScriptsInterface.writeJson(response, dir + "response"
+					+ File.separator + "init_project.json");
 
 			System.out.println(response);
 			System.out.println("Over.");
 			System.exit(0);
-		}catch(Exception e){
+		} catch (Exception e) {
 			System.out.println("Oops, something wrong...");
 			e.printStackTrace();
 		}

6e49ecdb87e205a86598a977b60dcd69f8e52b1e
wangshishuai3966 <wangshishuai3966@navinfo.com>
2016-03-17 18:02:51 +0800

    创建空间索引后执行analyze

--- scripts-interface/src/main/resources/com/navinfo/dataservice/scripts/resources/prj_utils.sql
+++ scripts-interface/src/main/resources/com/navinfo/dataservice/scripts/resources/prj_utils.sql
@@ -29,6 +29,8 @@ create index idx_rd_link_geometry
 on rd_link(geometry)
 indextype is mdsys.spatial_index;   
 
+analyze table rd_link compute statistics;
+
 insert into user_sdo_geom_metadata
 values
   ('RD_NODE',
@@ -40,6 +42,8 @@ values
 create index idx_rd_node_geometry
 on rd_node(geometry)
 indextype is mdsys.spatial_index;  
+
+analyze table rd_node compute statistics;
 
 insert into user_sdo_geom_metadata
   (table_name, COLUMN_NAME, DIMINFO, SRID)

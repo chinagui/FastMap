95dfc3e0f06617a40a364586f3b1e91f19a141fb
Xiaowen-Navinfo <xiaoxiaowen4127@navinfo.com>
2016-03-22 09:46:40 +0800

    添加api项目

---
+++ api/src/main/java/com/navinfo/dataservice/api/iface/datahub/IDatahubService.java
@@ -0,0 +1,11 @@
+package com.navinfo.dataservice.api.iface.datahub;
+
+/** 
+* @ClassName: IDbService 
+* @author Xiao Xiaowen 
+* @date 2016年3月21日 下午5:42:36 
+* @Description: TODO
+*/
+public interface IDatahubService {
+	
+}
---
+++ api/src/main/java/com/navinfo/dataservice/api/model/DbServer.java
@@ -0,0 +1,89 @@
+package com.navinfo.dataservice.api.model;
+
+import java.util.Set;
+
+/** 
+ * @ClassName: DbServer 
+ * @author Xiao Xiaowen 
+ * @Description: TODO
+ *  
+ */
+public class DbServer {
+	private int sid;
+	protected String type=DbServerType.TYPE_NONE;
+	private String ip;
+	private int port;
+	private String serviceName;
+	private Set<String> bizType;
+	private String descp;
+	
+	private String identity;//ip:port
+	
+	public DbServer(String type,String ip,int port,String serviceName){
+		this.type=type;
+		this.ip=ip;
+		this.port=port;
+		this.identity=ip+":"+port;
+		this.serviceName=serviceName;
+	}
+	
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	public String getServiceName() {
+		return serviceName;
+	}
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+	public Set<String> getBizType() {
+		return bizType;
+	}
+	public void setBizType(Set<String> bizType) {
+		this.bizType = bizType;
+	}
+	public String getDescp() {
+		return descp;
+	}
+	public void setDescp(String descp) {
+		this.descp = descp;
+	}
+	public String getIdentity(){
+		return this.identity;
+	}
+	public int hashCode(){
+		return getIdentity().hashCode();
+	}
+	public boolean equals(Object anObject){
+		if(anObject==null)return false;
+		if(anObject instanceof DbServer
+				&&getIdentity().equals(((DbServer) anObject).getIdentity())){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}

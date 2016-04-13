638ebf3b3f8ce21b0134cef72857c2661ca023c8
zhangxiaolong4943 <zhangxiaolong4943@navinfo.com>
2016-04-13 11:34:50 +0800

    新增AdAdmin的selector

---
+++ dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/selector/ad/zone/AdAdminNameSelector.java
@@ -0,0 +1,38 @@
+package com.navinfo.dataservice.dao.glm.selector.ad.zone;
+
+import java.sql.Connection;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.navinfo.dataservice.dao.glm.iface.IRow;
+import com.navinfo.dataservice.dao.glm.iface.ISelector;
+import com.navinfo.dataservice.dao.glm.selector.ad.geo.AdFaceSelector;
+
+public class AdAdminNameSelector implements ISelector{
+	
+	private static Logger logger = Logger.getLogger(AdFaceSelector.class);
+
+	private Connection conn;
+
+	public AdAdminNameSelector(Connection conn) {
+		super();
+		this.conn = conn;
+	}
+
+	@Override
+	public IRow loadById(int id, boolean isLock) throws Exception {
+		return null;
+	}
+
+	@Override
+	public IRow loadByRowId(String rowId, boolean isLock) throws Exception {
+		return null;
+	}
+
+	@Override
+	public List<IRow> loadRowsByParentId(int id, boolean isLock) throws Exception {
+		return null;
+	}
+
+}

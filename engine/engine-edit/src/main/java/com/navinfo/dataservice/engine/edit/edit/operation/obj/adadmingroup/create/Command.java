8601b7e0b7722256af02ae0218216ff74b852947
zhangxiaolong4943 <zhangxiaolong4943@navinfo.com>
2016-04-22 20:27:48 +0800

    删除注释

--- engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/edit/operation/obj/adadmingroup/create/Command.java
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/edit/operation/obj/adadmingroup/create/Command.java
@@ -10,21 +10,6 @@ import com.navinfo.dataservice.dao.glm.iface.OperType;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
-/**
- * json格式:
- * {
-	"projectId"："项目ID",
-	"command"："CREATE",
-	"type"："ADADMINGROUP",
-	"pid": "pid",
-	"data"：{
-		"parentType": "adadmin/adadmingroup",
-		"downAdAdmins"：[[1653],[1231]]
-	}
-	}
- * @author Administrator
- *
- */
 public class Command implements ICommand {
 
 	private String requester;

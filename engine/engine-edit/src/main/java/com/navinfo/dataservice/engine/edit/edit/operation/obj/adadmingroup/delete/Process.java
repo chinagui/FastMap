659c9fae6ef88da48336164508104244345dafbe
zhangxiaolong4943 <zhangxiaolong4943@navinfo.com>
2016-04-27 13:47:08 +0800

    行政区划层级制作代码提交

---
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/edit/operation/obj/adadmingroup/delete/Process.java
@@ -0,0 +1,126 @@
+package com.navinfo.dataservice.engine.edit.edit.operation.obj.adadmingroup.delete;
+
+import java.sql.Connection;
+
+import com.navinfo.dataservice.dao.glm.iface.ICommand;
+import com.navinfo.dataservice.dao.glm.iface.IOperation;
+import com.navinfo.dataservice.dao.glm.iface.IProcess;
+import com.navinfo.dataservice.dao.glm.iface.Result;
+import com.navinfo.dataservice.dao.log.LogWriter;
+import com.navinfo.dataservice.dao.pool.GlmDbPoolManager;
+import com.navinfo.dataservice.engine.edit.edit.operation.OperatorFactory;
+import com.navinfo.dataservice.engine.edit.edit.operation.obj.rdlink.create.Check;
+
+public class Process implements IProcess {
+
+	private Command command;
+
+	private Result result;
+
+	private Connection conn;
+	
+	private String postCheckMsg;
+	
+	private Check check = new Check();
+	
+	public Process(ICommand command) throws Exception {
+		this.command = (Command) command;
+
+		this.result = new Result();
+
+		this.conn = GlmDbPoolManager.getInstance().getConnection(this.command
+				.getProjectId());
+
+	}
+	
+	@Override
+	public ICommand getCommand() {
+		
+		return command;
+	}
+
+	@Override
+	public Result getResult() {
+		
+		return result;
+	}
+
+	@Override
+	public boolean prepareData() throws Exception {
+		
+		return true;
+	}
+
+	@Override
+	public String preCheck() throws Exception {
+		
+		return null;
+	}
+
+	@Override
+	public String run() throws Exception {
+		String msg;
+		try {
+			conn.setAutoCommit(false);
+
+			this.prepareData();
+
+			String preCheckMsg = this.preCheck();
+
+			if (preCheckMsg != null) {
+				throw new Exception(preCheckMsg);
+			}
+
+			IOperation operation = new Operation(command, conn);
+
+			msg = operation.run(result);
+
+			this.recordData();
+
+			this.postCheck();
+
+			conn.commit();
+
+		} catch (Exception e) {
+			
+			conn.rollback();
+
+			throw e;
+		} finally {
+			try {
+				conn.close();
+			} catch (Exception e) {
+				
+			}
+		}
+
+		return msg;
+	}
+
+	@Override
+	public void postCheck() throws Exception {
+		
+
+	}
+
+	@Override
+	public String getPostCheck() throws Exception {
+		
+		return postCheckMsg;
+	}
+
+	@Override
+	public boolean recordData() throws Exception {
+		
+		LogWriter lw = new LogWriter(conn, this.command.getProjectId());
+		
+		lw.generateLog(command, result);
+		
+		OperatorFactory.recordData(conn, result);
+
+		lw.recordLog(command, result);
+
+		return true;
+	}
+
+}

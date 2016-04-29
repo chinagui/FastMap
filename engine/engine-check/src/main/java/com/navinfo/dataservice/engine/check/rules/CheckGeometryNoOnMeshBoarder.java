724b1fc3fa0e8be6e707d53c7f73f2387c44ba40
xiaolvluo <zhangxiaoyi@navinfo.com>
2016-04-29 16:03:02 +0800

    CheckGeometryNoOnMeshBoarder

--- engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/CheckGeometryNoOnMeshBoarder.java
+++ engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/CheckGeometryNoOnMeshBoarder.java
@@ -1,29 +1,11 @@
 package com.navinfo.dataservice.engine.check.rules;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
 
-import net.sf.json.JSONObject;
-
-import com.navinfo.dataservice.commons.geom.GeoTranslator;
 import com.navinfo.dataservice.commons.util.MeshUtils;
 import com.navinfo.dataservice.dao.check.CheckCommand;
-import com.navinfo.dataservice.dao.check.NiValExceptionOperator;
 import com.navinfo.dataservice.dao.glm.iface.IRow;
-import com.navinfo.dataservice.dao.glm.iface.OperType;
-import com.navinfo.dataservice.dao.glm.model.rd.link.RdLink;
-import com.navinfo.dataservice.dao.glm.model.rd.link.RdLinkSpeedlimit;
 import com.navinfo.dataservice.dao.glm.model.rd.speedlimit.RdSpeedlimit;
-import com.navinfo.dataservice.dao.pool.GlmDbPoolManager;
-import com.navinfo.dataservice.engine.check.CheckEngine;
 import com.navinfo.dataservice.engine.check.core.baseRule;
-import com.navinfo.dataservice.engine.check.helper.DatabaseOperator;
-import com.navinfo.dataservice.engine.check.helper.GeoHelper;
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
 
 //点限速点位不能在图框线上
 

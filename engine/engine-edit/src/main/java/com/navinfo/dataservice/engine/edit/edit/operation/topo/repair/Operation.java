8999f649e36ef469d1842e84645a7ee1e19699a7
songdongyan <songdongyan@navinfo.com>
2016-05-03 19:03:47 +0800

    Process重构

--- engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/edit/operation/topo/repair/Operation.java
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/edit/operation/topo/repair/Operation.java
@@ -307,7 +307,7 @@ public class Operation implements IOperation {
 				data.put("latitude", lat);
 			}
 			breakJson.put("data", data);
-			ICommand breakCommand = new com.navinfo.dataservice.engine.edit.edit.operation.topo.breakpoint.Command(
+			com.navinfo.dataservice.engine.edit.edit.operation.topo.breakpoint.Command breakCommand = new com.navinfo.dataservice.engine.edit.edit.operation.topo.breakpoint.Command(
 					breakJson, breakJson.toString());
 			com.navinfo.dataservice.engine.edit.edit.operation.topo.breakpoint.Process breakProcess = new com.navinfo.dataservice.engine.edit.edit.operation.topo.breakpoint.Process(
 					breakCommand, conn);

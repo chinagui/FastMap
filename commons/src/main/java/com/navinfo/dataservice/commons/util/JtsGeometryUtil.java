8397dbdea50e3f30c2efa41b9e4318fa3eef8f78
Xiaowen-Navinfo <xiaoxiaowen4127@navinfo.com>
2016-05-05 10:23:02 +0800

    上线先分离相关计算几何算法等

--- commons/src/main/java/com/navinfo/dataservice/commons/util/JtsGeometryUtil.java
+++ commons/src/main/java/com/navinfo/dataservice/commons/util/JtsGeometryUtil.java
@@ -1,8 +1,11 @@
 package com.navinfo.dataservice.commons.util;
 
 import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKTReader;
 
 /** 
  * 添加一个jts Geometry 的工具类
@@ -14,7 +17,11 @@ import com.vividsolutions.jts.geom.LineString;
 */
 public class JtsGeometryUtil {
 	private static final GeometryFactory geometryFactory = new GeometryFactory();
+	private static final WKTReader reader = new WKTReader(geometryFactory);
 	public static LineString createLineString(Coordinate[] coordinates){
 		return geometryFactory.createLineString(coordinates);
+	}
+	public static Geometry read(String wkt) throws ParseException{
+		return reader.read(wkt);
 	}
 }

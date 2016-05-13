47ea162e4a7f489768f9014e1a6efdd58c7ba1df
xiaolvluo <zhangxiaoyi@navinfo.com>
2016-05-13 15:58:40 +0800

    glm08033 bug,及后检查参数添加

--- engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/core/VariablesFactory.java
+++ engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/core/VariablesFactory.java
@@ -2,11 +2,15 @@ package com.navinfo.dataservice.engine.check.core;
 
 import com.navinfo.dataservice.dao.glm.iface.IRow;
 import com.navinfo.dataservice.dao.glm.model.rd.link.RdLink;
+import com.navinfo.dataservice.dao.glm.model.rd.restrict.RdRestriction;
+import com.navinfo.dataservice.dao.glm.model.rd.restrict.RdRestrictionDetail;
 
 public class VariablesFactory {
 
 	public static String getRdLinkPid(IRow data){
 		if(data instanceof RdLink){return String.valueOf(((RdLink) data).getPid());}
+		if(data instanceof RdRestriction){return String.valueOf(((RdRestriction) data).getInLinkPid());}
+		if(data instanceof RdRestrictionDetail){return String.valueOf(((RdRestrictionDetail) data).getOutLinkPid());}
 		return null;
 	}
 

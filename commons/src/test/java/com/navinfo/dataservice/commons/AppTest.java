a4269e779c3fcbf22500c23a41e0d45781358152
zhaokaikai <zhaokaikai@navinfo.com>
2016-06-01 19:37:15 +0800

    去掉注释

--- commons/src/test/java/com/navinfo/dataservice/commons/AppTest.java
+++ commons/src/test/java/com/navinfo/dataservice/commons/AppTest.java
@@ -1,5 +1,8 @@
 package com.navinfo.dataservice.commons;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -35,4 +38,16 @@ public class AppTest
     {
         assertTrue( true );
     }
+    public static void main(String[] args) {
+		List<String> list1 =  new ArrayList<String>();
+		List<String> list2 =  new ArrayList<String>();
+		list1.add("AAA");
+		list1.add("BBB");
+		list1.add("CCC");
+		list2.add("CCC");
+		list2.add("DDDD");
+		list2.add("FFF");
+		list2.retainAll(list1);
+		System.out.println(list2);
+	}
 }

103a2e3c7799296a1961066d2ae7874817d05b21
zhangxiaolong4943 <zhangxiaolong4943@navinfo.com>
2016-06-06 21:25:22 +0800

    完善poi_name_selector根据rowid和parentid查询接口

--- dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/selector/poi/index/IxPoiNameSelector.java
+++ dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/selector/poi/index/IxPoiNameSelector.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.navinfo.dataservice.commons.exception.DataNotFoundException;
@@ -110,12 +111,110 @@ public class IxPoiNameSelector implements ISelector {
 
 	@Override
 	public IRow loadByRowId(String rowId, boolean isLock) throws Exception {
-		return null;
+		IxPoiName ixPoiName = new IxPoiName();
+
+		String sql = "select * from " + ixPoiName.tableName() + " where row_id=hextoraw(:1) and u_record!=2";
+
+		if (isLock) {
+			sql += " for update nowait";
+		}
+
+		PreparedStatement pstmt = null;
+
+		ResultSet resultSet = null;
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, rowId);
+
+			resultSet = pstmt.executeQuery();
+
+			if (resultSet.next()) {
+				setAttr(ixPoiName, resultSet);
+			} else {
+				throw new DataNotFoundException("数据不存在");
+			}
+		} catch (Exception e) {
+
+			throw e;
+
+		} finally {
+			try {
+				if (resultSet != null) {
+					resultSet.close();
+				}
+			} catch (Exception e) {
+
+			}
+
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+			} catch (Exception e) {
+
+			}
+
+		}
+		return ixPoiName;
 	}
 
 	@Override
 	public List<IRow> loadRowsByParentId(int id, boolean isLock) throws Exception {
-		return null;
+		List<IRow> rows = new ArrayList<IRow>();
+
+		String sql = "select * from ix_poi_name_flag where name_id=:1 and u_record!=:2";
+
+		if (isLock) {
+			sql += " for update nowait";
+		}
+
+		PreparedStatement pstmt = null;
+
+		ResultSet resultSet = null;
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setInt(1, id);
+
+			pstmt.setInt(2, 2);
+
+			resultSet = pstmt.executeQuery();
+
+			while (resultSet.next()) {
+
+				IxPoiName ixPoiName = new IxPoiName();
+
+				setAttr(ixPoiName, resultSet);
+
+				rows.add(ixPoiName);
+			}
+		} catch (Exception e) {
+
+			throw e;
+
+		} finally {
+			try {
+				if (resultSet != null) {
+					resultSet.close();
+				}
+			} catch (Exception e) {
+
+			}
+
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+			} catch (Exception e) {
+
+			}
+
+		}
+
+		return rows;
 	}
 	
 	/**

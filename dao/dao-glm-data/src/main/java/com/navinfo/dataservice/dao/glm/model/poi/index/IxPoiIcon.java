b9551733196ed4c58f160669679cfb9859f45474
zhangxiaolong4943 <zhangxiaolong4943@navinfo.com>
2016-06-06 14:48:24 +0800

    新增poi索引（基础信息）下的模型类

---
+++ dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/model/poi/index/IxPoiIcon.java
@@ -0,0 +1,286 @@
+package com.navinfo.dataservice.dao.glm.model.poi.index;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import com.navinfo.dataservice.commons.geom.GeoTranslator;
+import com.navinfo.dataservice.commons.geom.Geojson;
+import com.navinfo.dataservice.dao.glm.iface.IObj;
+import com.navinfo.dataservice.dao.glm.iface.IRow;
+import com.navinfo.dataservice.dao.glm.iface.ObjLevel;
+import com.navinfo.dataservice.dao.glm.iface.ObjStatus;
+import com.navinfo.dataservice.dao.glm.iface.ObjType;
+import com.vividsolutions.jts.geom.Geometry;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import net.sf.json.JsonConfig;
+
+/**
+ * POI图标(3DICON)表
+ * @author zhangxiaolong
+ *
+ */
+public class IxPoiIcon implements IObj {
+	
+	private int pid;
+	
+	private int poiPid;
+	
+	private String iconName;
+	
+	private Geometry geometry;
+	
+	private int manageCode;
+	
+	private String clientFlag;
+	
+	private String memo;
+	
+	private String rowId;
+	
+	private Map<String, Object> changedFields = new HashMap<String, Object>();
+	
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public int getPoiPid() {
+		return poiPid;
+	}
+
+	public void setPoiPid(int poiPid) {
+		this.poiPid = poiPid;
+	}
+
+	public String getIconName() {
+		return iconName;
+	}
+
+	public void setIconName(String iconName) {
+		this.iconName = iconName;
+	}
+
+	public Geometry getGeometry() {
+		return geometry;
+	}
+
+	public void setGeometry(Geometry geometry) {
+		this.geometry = geometry;
+	}
+
+	public int getManageCode() {
+		return manageCode;
+	}
+
+	public void setManageCode(int manageCode) {
+		this.manageCode = manageCode;
+	}
+
+	public String getClientFlag() {
+		return clientFlag;
+	}
+
+	public void setClientFlag(String clientFlag) {
+		this.clientFlag = clientFlag;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+	public String getRowId() {
+		return rowId;
+	}
+
+	@Override
+	public String rowId() {
+		return this.rowId;
+	}
+
+	@Override
+	public void setRowId(String rowId) {
+		this.rowId = rowId;
+	}
+
+	@Override
+	public String tableName() {
+		return "ix_poi_icon";
+	}
+
+	@Override
+	public ObjStatus status() {
+		return null;
+	}
+
+	@Override
+	public void setStatus(ObjStatus os) {
+	}
+
+	@Override
+	public ObjType objType() {
+		return ObjType.IXPOIICON;
+	}
+
+	@Override
+	public void copy(IRow row) {
+	}
+
+	@Override
+	public Map<String, Object> changedFields() {
+		return this.changedFields;
+	}
+
+	@Override
+	public String parentPKName() {
+		return "rel_id";
+	}
+
+	@Override
+	public int parentPKValue() {
+		return this.pid;
+	}
+
+	@Override
+	public String parentTableName() {
+		return "ix_poi";
+	}
+
+	@Override
+	public List<List<IRow>> children() {
+		return null;
+	}
+
+	@Override
+	public boolean fillChangeFields(JSONObject json) throws Exception {
+		@SuppressWarnings("rawtypes")
+		Iterator keys = json.keys();
+
+		while (keys.hasNext()) {
+			String key = (String) keys.next();
+
+			if (json.get(key) instanceof JSONArray) {
+				continue;
+			}  else if ("geometry".equals(key)) {
+				
+				JSONObject geojson = json.getJSONObject(key);
+				
+				String wkt = Geojson.geojson2Wkt(geojson.toString());
+				
+				String oldwkt = GeoTranslator.jts2Wkt(geometry, 0.00001, 5);
+				
+				if(!wkt.equals(oldwkt))
+				{
+					changedFields.put(key, json.getJSONObject(key));
+				}
+			}  else {
+				if ( !"objStatus".equals(key)) {
+					
+					Field field = this.getClass().getDeclaredField(key);
+					
+					field.setAccessible(true);
+					
+					Object objValue = field.get(this);
+					
+					String oldValue = null;
+					
+					if (objValue == null){
+						oldValue = "null";
+					}else{
+						oldValue = String.valueOf(objValue);
+					}
+					
+					String newValue = json.getString(key);
+					
+					if (!newValue.equals(oldValue)){
+						Object value = json.get(key);
+						
+						if(value instanceof String){
+							changedFields.put(key, newValue.replace("'", "''"));
+						}
+						else{
+							changedFields.put(key, value);
+						}
+
+					}
+
+					
+				}
+			}
+		}
+		
+		if (changedFields.size() >0){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	@Override
+	public int mesh() {
+		return 0;
+	}
+
+	@Override
+	public void setMesh(int mesh) {
+	}
+
+	@Override
+	public JSONObject Serialize(ObjLevel objLevel) throws Exception {
+
+		JsonConfig jsonConfig = Geojson.geoJsonConfig(0.00001, 5);
+		
+		JSONObject json = JSONObject.fromObject(this, jsonConfig);
+
+		return json;
+	}
+
+	@Override
+	public boolean Unserialize(JSONObject json) throws Exception {
+		@SuppressWarnings("rawtypes")
+		Iterator keys = json.keys();
+
+		while (keys.hasNext()) {
+
+			String key = (String) keys.next();
+
+			if (!"objStatus".equals(key)) {
+
+				Field f = this.getClass().getDeclaredField(key);
+
+				f.setAccessible(true);
+
+				f.set(this, json.get(key));
+			}
+
+		}
+		return true;
+	}
+
+	@Override
+	public List<IRow> relatedRows() {
+		return null;
+	}
+
+	@Override
+	public int pid() {
+		return this.pid;
+	}
+
+	@Override
+	public String primaryKey() {
+		return "rel_id";
+	}
+
+}
---
+++ dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/model/poi/index/IxPoiName.java
@@ -0,0 +1,330 @@
+package com.navinfo.dataservice.dao.glm.model.poi.index;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import com.navinfo.dataservice.commons.util.JsonUtils;
+import com.navinfo.dataservice.dao.glm.iface.IObj;
+import com.navinfo.dataservice.dao.glm.iface.IRow;
+import com.navinfo.dataservice.dao.glm.iface.ObjLevel;
+import com.navinfo.dataservice.dao.glm.iface.ObjStatus;
+import com.navinfo.dataservice.dao.glm.iface.ObjType;
+import com.navinfo.dataservice.dao.glm.model.ad.zone.AdAdminPart;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+/**
+ * POI名称表
+ * 
+ * @author zhangxiaolong
+ *
+ */
+public class IxPoiName implements IObj {
+
+	private int pid;
+
+	private int poiPid;
+
+	private int nameGroupid;
+
+	private String langCode;
+
+	private int nameClass;
+
+	private int nameType;
+
+	private String name;
+
+	private String namePhonetic;
+
+	private String keywords;
+
+	private String nidePid;
+
+	private String rowId;
+
+	private Map<String, Object> changedFields = new HashMap<String, Object>();
+
+	private List<IRow> nameToneList = new ArrayList<IRow>();
+
+	public Map<String, AdAdminPart> nameToneMap = new HashMap<String, AdAdminPart>();
+	
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public int getPoiPid() {
+		return poiPid;
+	}
+
+	public void setPoiPid(int poiPid) {
+		this.poiPid = poiPid;
+	}
+
+	public int getNameGroupid() {
+		return nameGroupid;
+	}
+
+	public void setNameGroupid(int nameGroupid) {
+		this.nameGroupid = nameGroupid;
+	}
+
+	public String getLangCode() {
+		return langCode;
+	}
+
+	public void setLangCode(String langCode) {
+		this.langCode = langCode;
+	}
+
+	public int getNameClass() {
+		return nameClass;
+	}
+
+	public void setNameClass(int nameClass) {
+		this.nameClass = nameClass;
+	}
+
+	public int getNameType() {
+		return nameType;
+	}
+
+	public void setNameType(int nameType) {
+		this.nameType = nameType;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNamePhonetic() {
+		return namePhonetic;
+	}
+
+	public void setNamePhonetic(String namePhonetic) {
+		this.namePhonetic = namePhonetic;
+	}
+
+	public String getKeywords() {
+		return keywords;
+	}
+
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+
+	public String getNidePid() {
+		return nidePid;
+	}
+
+	public void setNidePid(String nidePid) {
+		this.nidePid = nidePid;
+	}
+
+	public String getRowId() {
+		return rowId;
+	}
+
+	public List<IRow> getNameToneList() {
+		return nameToneList;
+	}
+
+	public void setNameToneList(List<IRow> nameToneList) {
+		this.nameToneList = nameToneList;
+	}
+
+	@Override
+	public String rowId() {
+		return this.rowId;
+	}
+
+	@Override
+	public void setRowId(String rowId) {
+		this.rowId = rowId;
+	}
+
+	@Override
+	public String tableName() {
+		return "IX_POI_NAME";
+	}
+
+	@Override
+	public ObjStatus status() {
+		return null;
+	}
+
+	@Override
+	public void setStatus(ObjStatus os) {
+	}
+
+	@Override
+	public ObjType objType() {
+		return ObjType.IXPOINAME;
+	}
+
+	@Override
+	public void copy(IRow row) {
+		IxPoiName nameSource = (IxPoiName) row;
+
+		this.setName(nameSource.getName());
+
+		this.setPoiPid(nameSource.getPoiPid());
+
+		this.setNameGroupid(nameSource.getNameGroupid());
+
+		this.setNameClass(nameSource.getNameClass());
+
+		this.setNameType(nameSource.getNameType());
+
+		this.setRowId(nameSource.getRowId());
+	}
+
+	@Override
+	public Map<String, Object> changedFields() {
+		return changedFields;
+	}
+
+	@Override
+	public String parentPKName() {
+		return "name_id";
+	}
+
+	@Override
+	public int parentPKValue() {
+		return this.getPid();
+	}
+
+	@Override
+	public String parentTableName() {
+		return "ix_poi_name";
+	}
+
+	@Override
+	public List<List<IRow>> children() {
+		List<List<IRow>> children = new ArrayList<List<IRow>>();
+		children.add(this.nameToneList);
+		return children;
+	}
+
+	@Override
+	public boolean fillChangeFields(JSONObject json) throws Exception {
+		@SuppressWarnings("rawtypes")
+		Iterator keys = json.keys();
+
+		while (keys.hasNext()) {
+			String key = (String) keys.next();
+
+			if (json.get(key) instanceof JSONArray) {
+				continue;
+			} else {
+				if (!"objStatus".equals(key)) {
+
+					Field field = this.getClass().getDeclaredField(key);
+
+					field.setAccessible(true);
+
+					Object objValue = field.get(this);
+
+					String oldValue = null;
+
+					if (objValue == null) {
+						oldValue = "null";
+					} else {
+						oldValue = String.valueOf(objValue);
+					}
+
+					String newValue = json.getString(key);
+
+					if (!newValue.equals(oldValue)) {
+						Object value = json.get(key);
+
+						if (value instanceof String) {
+							changedFields.put(key, newValue.replace("'", "''"));
+						} else {
+							changedFields.put(key, value);
+						}
+
+					}
+
+				}
+			}
+		}
+
+		if (changedFields.size() > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public int mesh() {
+		return this.mesh();
+	}
+
+	@Override
+	public void setMesh(int mesh) {
+	}
+
+	@Override
+	public JSONObject Serialize(ObjLevel objLevel) throws Exception {
+		JSONObject json = JSONObject.fromObject(this, JsonUtils.getStrConfig());
+
+		if (objLevel == ObjLevel.HISTORY) {
+			json.remove("name");
+		}
+
+		return json;
+	}
+
+	@Override
+	public boolean Unserialize(JSONObject json) throws Exception {
+		@SuppressWarnings("rawtypes")
+		Iterator keys = json.keys();
+
+		while (keys.hasNext()) {
+
+			String key = (String) keys.next();
+
+			if (!"objStatus".equals(key)) {
+
+				Field f = this.getClass().getDeclaredField(key);
+
+				f.setAccessible(true);
+
+				f.set(this, json.get(key));
+			}
+
+		}
+		return true;
+	}
+
+	@Override
+	public List<IRow> relatedRows() {
+		return null;
+	}
+
+	@Override
+	public int pid() {
+		return this.getPid();
+	}
+
+	@Override
+	public String primaryKey() {
+		return "name_id";
+	}
+
+}

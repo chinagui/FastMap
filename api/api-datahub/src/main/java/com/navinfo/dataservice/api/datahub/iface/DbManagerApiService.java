fb2f2c4608b094fc7b5e2508198d3be5e2f33aba
yunfma <mayunfei@navinfo.com>
2016-06-07 20:06:10 +0800

    api service defination

---
+++ api/api-datahub/src/main/java/com/navinfo/dataservice/api/datahub/iface/DbManagerApiService.java
@@ -0,0 +1,12 @@
+package com.navinfo.dataservice.api.datahub.iface;
+
+
+/*
+ * @author mayunfei
+ * 2016年6月7日
+ * 描述：apiDbManagerApiService.java
+ */
+public interface DbManagerApiService {
+	public Object getOnlyDbByName(String dbName)throws Exception;
+}
+

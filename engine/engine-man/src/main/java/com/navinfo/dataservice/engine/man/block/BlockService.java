1d46e1dfe692935a68cab252db8acbd4aa005f99
jicaihua <jicaihua@navinfo.com>
2016-06-08 15:50:57 +0800

    增加两个接口： /man/block/query/ /man/block/listByWkt/

--- engine/engine-man/src/main/java/com/navinfo/dataservice/engine/man/block/BlockService.java
+++ engine/engine-man/src/main/java/com/navinfo/dataservice/engine/man/block/BlockService.java
@@ -1,4 +1,4 @@
-package com.navinfo.dataservice.engine.man.city;
+package com.navinfo.dataservice.engine.man.block;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -11,27 +11,25 @@ import org.apache.commons.dbutils.DbUtils;
 import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
 
-import com.navinfo.dataservice.commons.geom.GeoTranslator;
 import com.navinfo.dataservice.commons.log.LoggerRepos;
 import com.navinfo.dataservice.engine.man.dao.DBConnector;
+import com.navinfo.dataservice.engine.man.task.TaskOperation;
 import com.navinfo.navicommons.database.Page;
 import com.navinfo.navicommons.database.QueryRunner;
 import com.navinfo.navicommons.exception.ServiceException;
-import com.navinfo.navicommons.geo.computation.GeometryUtils;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.io.ParseException;
 
 import net.sf.json.JSONObject;
-import oracle.sql.STRUCT;
 
 /** 
-* @ClassName:  CityService 
+* @ClassName:  BlockService 
 * @author code generator
-* @date 2016-06-06 08:19:11 
+* @date 2016-06-08 01:32:00 
 * @Description: TODO
 */
-public class CityService {
+@Service
+public class BlockService {
 	private Logger log = LoggerRepos.getLogger(this.getClass());
 
 	
@@ -40,13 +38,13 @@ public class CityService {
 		try{
 			//持久化
 			QueryRunner run = new QueryRunner();
-			conn = DBConnector.getInstance().getManConnection();
-			City  bean = (City)JSONObject.toBean(json, City.class);	
+			conn =  DBConnector.getInstance().getManConnection();	
+			Block  bean = (Block)JSONObject.toBean(json, Block.class);	
 			
-			String createSql = "insert into CITY (CITY_ID, CITY_NAME, PROVINCE_NAME, GEOMETRY, REGION_ID, PLAN_STATUS) values(?,?,?,?,?,?)";			
+			String createSql = "insert into BLOCK (BLOCK_ID, CITY_ID, BLOCK_NAME, GEOMETRY, PLAN_STATUS) values(?,?,?,?,?)";			
 			run.update(conn, 
 					   createSql, 
-					   bean.getCityId() , bean.getCityName(), bean.getProvinceName(), bean.getGeometry(), bean.getRegionId(), bean.getPlanStatus()
+					   bean.getBlockId() , bean.getCityId(), bean.getBlockName(), bean.getGeometry(), bean.getPlanStatus()
 					   );
 		}catch(Exception e){
 			DbUtils.rollbackAndCloseQuietly(conn);
@@ -61,31 +59,27 @@ public class CityService {
 		try{
 			//持久化
 			QueryRunner run = new QueryRunner();
-			conn = DBConnector.getInstance().getManConnection();	
+			conn =  DBConnector.getInstance().getManConnection();	
 			JSONObject obj = JSONObject.fromObject(json);	
-			City  bean = (City)JSONObject.toBean(obj, City.class);	
+			Block  bean = (Block)JSONObject.toBean(obj, Block.class);	
 			
-			String updateSql = "update CITY set CITY_ID=?, CITY_NAME=?, PROVINCE_NAME=?, GEOMETRY=?, REGION_ID=?, PLAN_STATUS=? where 1=1 CITY_ID=? and CITY_NAME=? and PROVINCE_NAME=? and GEOMETRY=? and REGION_ID=? and PLAN_STATUS=?";
-			List<Object> values=new ArrayList();
+			String updateSql = "update BLOCK set BLOCK_ID=?, CITY_ID=?, BLOCK_NAME=?, GEOMETRY=?, PLAN_STATUS=? where 1=1 BLOCK_ID=? and CITY_ID=? and BLOCK_NAME=? and GEOMETRY=? and PLAN_STATUS=?";
+			List<Object> values=new ArrayList<Object>();
+			if (bean!=null&&bean.getBlockId()!=null && StringUtils.isNotEmpty(bean.getBlockId().toString())){
+				updateSql+=" and BLOCK_ID=? ";
+				values.add(bean.getBlockId());
+			};
 			if (bean!=null&&bean.getCityId()!=null && StringUtils.isNotEmpty(bean.getCityId().toString())){
 				updateSql+=" and CITY_ID=? ";
 				values.add(bean.getCityId());
 			};
-			if (bean!=null&&bean.getCityName()!=null && StringUtils.isNotEmpty(bean.getCityName().toString())){
-				updateSql+=" and CITY_NAME=? ";
-				values.add(bean.getCityName());
-			};
-			if (bean!=null&&bean.getProvinceName()!=null && StringUtils.isNotEmpty(bean.getProvinceName().toString())){
-				updateSql+=" and PROVINCE_NAME=? ";
-				values.add(bean.getProvinceName());
+			if (bean!=null&&bean.getBlockName()!=null && StringUtils.isNotEmpty(bean.getBlockName().toString())){
+				updateSql+=" and BLOCK_NAME=? ";
+				values.add(bean.getBlockName());
 			};
 			if (bean!=null&&bean.getGeometry()!=null && StringUtils.isNotEmpty(bean.getGeometry().toString())){
 				updateSql+=" and GEOMETRY=? ";
 				values.add(bean.getGeometry());
-			};
-			if (bean!=null&&bean.getRegionId()!=null && StringUtils.isNotEmpty(bean.getRegionId().toString())){
-				updateSql+=" and REGION_ID=? ";
-				values.add(bean.getRegionId());
 			};
 			if (bean!=null&&bean.getPlanStatus()!=null && StringUtils.isNotEmpty(bean.getPlanStatus().toString())){
 				updateSql+=" and PLAN_STATUS=? ";
@@ -93,7 +87,7 @@ public class CityService {
 			};
 			run.update(conn, 
 					   updateSql, 
-					   bean.getCityId() ,bean.getCityName(),bean.getProvinceName(),bean.getGeometry(),bean.getRegionId(),bean.getPlanStatus(),
+					   bean.getBlockId() ,bean.getCityId(),bean.getBlockName(),bean.getGeometry(),bean.getPlanStatus(),
 					   values.toArray()
 					   );
 		}catch(Exception e){
@@ -109,31 +103,27 @@ public class CityService {
 		try{
 			//持久化
 			QueryRunner run = new QueryRunner();
-			conn = DBConnector.getInstance().getManConnection();	
+			conn =  DBConnector.getInstance().getManConnection();	
 			JSONObject obj = JSONObject.fromObject(json);	
-			City  bean = (City)JSONObject.toBean(obj, City.class);	
+			Block  bean = (Block)JSONObject.toBean(obj, Block.class);	
 			
-			String deleteSql = "delete from  CITY where 1=1 ";
-			List<Object> values=new ArrayList();
+			String deleteSql = "delete from  BLOCK where 1=1 ";
+			List<Object> values=new ArrayList<Object>();
+			if (bean!=null&&bean.getBlockId()!=null && StringUtils.isNotEmpty(bean.getBlockId().toString())){
+				deleteSql+=" and BLOCK_ID=? ";
+				values.add(bean.getBlockId());
+			};
 			if (bean!=null&&bean.getCityId()!=null && StringUtils.isNotEmpty(bean.getCityId().toString())){
 				deleteSql+=" and CITY_ID=? ";
 				values.add(bean.getCityId());
 			};
-			if (bean!=null&&bean.getCityName()!=null && StringUtils.isNotEmpty(bean.getCityName().toString())){
-				deleteSql+=" and CITY_NAME=? ";
-				values.add(bean.getCityName());
-			};
-			if (bean!=null&&bean.getProvinceName()!=null && StringUtils.isNotEmpty(bean.getProvinceName().toString())){
-				deleteSql+=" and PROVINCE_NAME=? ";
-				values.add(bean.getProvinceName());
+			if (bean!=null&&bean.getBlockName()!=null && StringUtils.isNotEmpty(bean.getBlockName().toString())){
+				deleteSql+=" and BLOCK_NAME=? ";
+				values.add(bean.getBlockName());
 			};
 			if (bean!=null&&bean.getGeometry()!=null && StringUtils.isNotEmpty(bean.getGeometry().toString())){
 				deleteSql+=" and GEOMETRY=? ";
 				values.add(bean.getGeometry());
-			};
-			if (bean!=null&&bean.getRegionId()!=null && StringUtils.isNotEmpty(bean.getRegionId().toString())){
-				deleteSql+=" and REGION_ID=? ";
-				values.add(bean.getRegionId());
 			};
 			if (bean!=null&&bean.getPlanStatus()!=null && StringUtils.isNotEmpty(bean.getPlanStatus().toString())){
 				deleteSql+=" and PLAN_STATUS=? ";
@@ -157,31 +147,27 @@ public class CityService {
 		Connection conn = null;
 		try{
 			QueryRunner run = new QueryRunner();
-			conn = DBConnector.getInstance().getManConnection();	
+			conn =  DBConnector.getInstance().getManConnection();		
 			JSONObject obj = JSONObject.fromObject(json);	
-			City  bean = (City)JSONObject.toBean(obj, City.class);
+			Block  bean = (Block)JSONObject.toBean(obj, Block.class);
 			
-			String selectSql = "select * from CITY where 1=1 ";
-			List<Object> values=new ArrayList();
+			String selectSql = "select * from BLOCK where 1=1 ";
+			List<Object> values=new ArrayList<Object>();
+			if (bean!=null&&bean.getBlockId()!=null && StringUtils.isNotEmpty(bean.getBlockId().toString())){
+				selectSql+=" and BLOCK_ID=? ";
+				values.add(bean.getBlockId());
+			};
 			if (bean!=null&&bean.getCityId()!=null && StringUtils.isNotEmpty(bean.getCityId().toString())){
 				selectSql+=" and CITY_ID=? ";
 				values.add(bean.getCityId());
 			};
-			if (bean!=null&&bean.getCityName()!=null && StringUtils.isNotEmpty(bean.getCityName().toString())){
-				selectSql+=" and CITY_NAME=? ";
-				values.add(bean.getCityName());
-			};
-			if (bean!=null&&bean.getProvinceName()!=null && StringUtils.isNotEmpty(bean.getProvinceName().toString())){
-				selectSql+=" and PROVINCE_NAME=? ";
-				values.add(bean.getProvinceName());
+			if (bean!=null&&bean.getBlockName()!=null && StringUtils.isNotEmpty(bean.getBlockName().toString())){
+				selectSql+=" and BLOCK_NAME=? ";
+				values.add(bean.getBlockName());
 			};
 			if (bean!=null&&bean.getGeometry()!=null && StringUtils.isNotEmpty(bean.getGeometry().toString())){
 				selectSql+=" and GEOMETRY=? ";
 				values.add(bean.getGeometry());
-			};
-			if (bean!=null&&bean.getRegionId()!=null && StringUtils.isNotEmpty(bean.getRegionId().toString())){
-				selectSql+=" and REGION_ID=? ";
-				values.add(bean.getRegionId());
 			};
 			if (bean!=null&&bean.getPlanStatus()!=null && StringUtils.isNotEmpty(bean.getPlanStatus().toString())){
 				selectSql+=" and PLAN_STATUS=? ";
@@ -189,16 +175,15 @@ public class CityService {
 			};
 			ResultSetHandler<Page> rsHandler = new ResultSetHandler<Page>(){
 				public Page handle(ResultSet rs) throws SQLException {
-					List list = new ArrayList();
+					List<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
 		            Page page = new Page(currentPageNum);
 					while(rs.next()){
-						HashMap map = new HashMap();
+						HashMap<String, Object> map = new HashMap<String, Object>();
 						page.setTotalCount(rs.getInt(QueryRunner.TOTAL_RECORD_NUM));
+						map.put("blockId", rs.getInt("BLOCK_ID"));
 						map.put("cityId", rs.getInt("CITY_ID"));
-						map.put("cityName", rs.getString("CITY_NAME"));
-						map.put("provinceName", rs.getString("PROVINCE_NAME"));
+						map.put("blockName", rs.getString("BLOCK_NAME"));
 						map.put("geometry", rs.getObject("GEOMETRY"));
-						map.put("regionId", rs.getInt("REGION_ID"));
 						map.put("planStatus", rs.getInt("PLAN_STATUS"));
 						list.add(map);
 					}
@@ -222,43 +207,29 @@ public class CityService {
 		}
 		
 	}
-	public List<HashMap> queryListByWkt(JSONObject json)throws ServiceException{
+	public List<HashMap> listByWkt(JSONObject json)throws ServiceException{
 		Connection conn = null;
 		try{
-			QueryRunner run = new QueryRunner();
-			conn = DBConnector.getInstance().getManConnection();
-					
-			final String wkt= json.getString("wkt");
-			int planningStatus = json.getInt("planningStatus");
 			
-			String selectSql = "select t.CITY_ID,t.CITY_NAME, t.geometry.get_wkt() as geometry from CITY t where t.PLAN_STATUS="+planningStatus;
-		
-			ResultSetHandler<List<HashMap>> rsHandler = new ResultSetHandler<List<HashMap>>(){
-				public List<HashMap> handle(ResultSet rs) throws SQLException {
-					List<HashMap> list = new ArrayList<HashMap>();
-					while(rs.next()){
-						try {
-							if (GeometryUtils.IsIntersectPolygon(wkt,rs.getObject("geometry"))){
-								HashMap<String,Object> map = new HashMap<String,Object>();
-								map.put("cityId", rs.getInt("CITY_ID"));
-								map.put("cityName", rs.getString("CITY_NAME"));
-								map.put("geometry", rs.getObject("geometry"));
-								list.add(map);
-							}
-						} catch (ParseException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						} catch (Exception e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						
-					}
-					return list;
+			conn =  DBConnector.getInstance().getManConnection();	
+			
+			String selectSql ="select t.BLOCK_ID,t.BLOCK_NAME,t.GEOMETRY.get_wkt() as GEOMETRY, from BLOCK t where PLAN_STATUS="+json.getInt("planningStatus");
+					
+			if (StringUtils.isNotEmpty(json.getString("snapshot"))){
+				if ("1".equals(json.getString("snapshot"))){
+					selectSql="select t.BLOCK_ID,t.BLOCK_NAME, from BLOCK t where PLAN_STATUS="+json.getInt("planningStatus");
+				}	
+			};
+			if (!json.containsKey("relation") || ("intersect".equals(json.getString("relation")))){
+				selectSql+=" and SDO_ANYINTERACT(t.geometry,sdo_geometry(?,8307))='TRUE'";
+			}else{
+				if ("within".equals(json.getString("relation"))){
+					selectSql+=" and sdo_within_distance(t.geometry,  sdo_geom.sdo_mbr(sdo_geometry(?, 8307)), 'DISTANCE=0') = 'TRUE'";
 				}
-	    		
-	    	}		;
-	    	return run.query(conn, selectSql, rsHandler);
+			}
+			List<Object> list=new ArrayList<Object>();
+			list.add(json.getString("wkt"));
+			return BlockOperation.queryBlockBySql(conn, selectSql,list);
 		}catch(Exception e){
 			DbUtils.rollbackAndCloseQuietly(conn);
 			log.error(e.getMessage(), e);
@@ -268,26 +239,24 @@ public class CityService {
 		}
 	}
 	
-	
-	public HashMap query(JSONObject json)throws ServiceException{
+	public HashMap<?, ?> query(JSONObject json)throws ServiceException{
 		Connection conn = null;
 		try{
 			//持久化
 			QueryRunner run = new QueryRunner();
-			conn = DBConnector.getInstance().getManConnection();
+			conn = DBConnector.getInstance().getManConnection();	
 			JSONObject obj = JSONObject.fromObject(json);	
-			City  bean = (City)JSONObject.toBean(obj, City.class);	
+			Block  bean = (Block)JSONObject.toBean(obj, Block.class);	
 			
-			String selectSql = "select * from CITY where CITY_ID=? and CITY_NAME=? and PROVINCE_NAME=? and GEOMETRY=? and REGION_ID=? and PLAN_STATUS=?";
+			String selectSql = "select t.BLOCK_ID,t.CITY_ID,t.BLOCK_NAME,t.GEOMETRY.get_wkt() as GEOMETRY,t.PLAN_STATUS from BLOCK t where t.BLOCK_ID=?";
 			ResultSetHandler<HashMap> rsHandler = new ResultSetHandler<HashMap>(){
-				public HashMap handle(ResultSet rs) throws SQLException {
+				public HashMap<String, Object> handle(ResultSet rs) throws SQLException {
 					while(rs.next()){
-						HashMap map = new HashMap();
+						HashMap<String, Object> map = new HashMap<String, Object>();
+						map.put("blockId", rs.getInt("BLOCK_ID"));
 						map.put("cityId", rs.getInt("CITY_ID"));
-						map.put("cityName", rs.getString("CITY_NAME"));
-						map.put("provinceName", rs.getString("PROVINCE_NAME"));
+						map.put("blockName", rs.getString("BLOCK_NAME"));
 						map.put("geometry", rs.getObject("GEOMETRY"));
-						map.put("regionId", rs.getInt("REGION_ID"));
 						map.put("planStatus", rs.getInt("PLAN_STATUS"));
 						return map;
 					}
@@ -298,7 +267,7 @@ public class CityService {
 			return run.query(conn, 
 					   selectSql,
 					   rsHandler, 
-					   bean.getCityId(), bean.getCityName(), bean.getProvinceName(), bean.getGeometry(), bean.getRegionId(), bean.getPlanStatus());
+					   bean.getBlockId());
 		}catch(Exception e){
 			DbUtils.rollbackAndCloseQuietly(conn);
 			log.error(e.getMessage(), e);

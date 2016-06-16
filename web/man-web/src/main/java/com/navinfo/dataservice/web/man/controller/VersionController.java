94a494ef57215574f9f6eb29d391d4eec790bb9c
wangshishuai3966 <wangshishuai3966@navinfo.com>
2016-06-16 20:50:24 +0800

    添加version查询接口

---
+++ web/man-web/src/main/java/com/navinfo/dataservice/web/man/controller/VersionController.java
@@ -0,0 +1,42 @@
+package com.navinfo.dataservice.web.man.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import net.sf.json.JSONObject;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.navinfo.dataservice.commons.log.LoggerRepos;
+import com.navinfo.dataservice.commons.springmvc.BaseController;
+import com.navinfo.dataservice.engine.man.version.VersionService;
+
+@Controller
+public class VersionController extends BaseController {
+	private Logger log = LoggerRepos.getLogger(this.getClass());
+	@Autowired
+	private VersionService service;
+
+	@RequestMapping(value = "/version/get")
+	public ModelAndView getVersion(HttpServletRequest request) {
+		try {
+			String parameter = request.getParameter("parameter");
+			if (StringUtils.isEmpty(parameter)) {
+				throw new IllegalArgumentException("parameter参数不能为空。");
+			}
+			JSONObject dataJson = JSONObject.fromObject(URLDecode(parameter));
+			if (dataJson == null) {
+				throw new IllegalArgumentException("parameter参数不能为空。");
+			}
+			String version = service.query(dataJson.getInt("type"));
+			return new ModelAndView("jsonView", success("success",version));
+		} catch (Exception e) {
+			log.error("创建失败，原因：" + e.getMessage(), e);
+			return new ModelAndView("jsonView", exception(e));
+		}
+	}
+}

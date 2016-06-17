e4ced8593e8ee3e45afbe24c1d77d471ac82417f
zhangxiaolong4943 <zhangxiaolong4943@navinfo.com>
2016-06-17 21:10:33 +0800

    修改铁路模型和添加必要的查询方法

--- dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/selector/rd/rw/RwLinkSelector.java
+++ dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/selector/rd/rw/RwLinkSelector.java
@@ -3,6 +3,7 @@ package com.navinfo.dataservice.dao.glm.selector.rd.rw;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.navinfo.dataservice.commons.geom.GeoTranslator;
@@ -10,19 +11,18 @@ import com.navinfo.dataservice.dao.glm.iface.IRow;
 import com.navinfo.dataservice.dao.glm.iface.ISelector;
 import com.navinfo.dataservice.dao.glm.model.rd.rw.RwLink;
 import com.navinfo.dataservice.dao.glm.model.rd.rw.RwLinkName;
-import com.navinfo.dataservice.dao.glm.model.rd.rw.RwNode;
 import com.vividsolutions.jts.geom.Geometry;
 
 import oracle.sql.STRUCT;
 
 public class RwLinkSelector implements ISelector {
-	
+
 	private Connection conn;
 
 	public RwLinkSelector(Connection conn) {
 		this.conn = conn;
 	}
-	
+
 	@Override
 	public IRow loadById(int id, boolean isLock) throws Exception {
 		RwLink rwLink = new RwLink();
@@ -46,54 +46,12 @@ public class RwLinkSelector implements ISelector {
 			resultSet = pstmt.executeQuery();
 
 			if (resultSet.next()) {
-				
-				rwLink.setPid(id);
-				
-				rwLink.setFeaturePid(resultSet.getInt("feature_pid"));
-				
-				rwLink.setsNodePid(resultSet.getInt("s_node_pid"));
-				
-				rwLink.seteNodePid(resultSet.getInt("e_node_pid"));
-				
-				rwLink.setKind(resultSet.getInt("kind"));
-				
-				rwLink.setForm(resultSet.getInt("form"));
-				
-				rwLink.setLength(resultSet.getDouble("length"));
-				
-				STRUCT struct = (STRUCT) resultSet.getObject("geometry");
 
-				Geometry geometry = GeoTranslator.struct2Jts(struct, 100000, 0);
-				
-				rwLink.setGeometry(geometry);
-				
-				rwLink.setMeshId(resultSet.getInt("mesh_id"));
-				
-				rwLink.setScale(resultSet.getInt("scale"));
-				
-				rwLink.setDetailFlag(resultSet.getInt("detail_flag"));
-				
-				rwLink.setEditFlag(resultSet.getInt("edit_flag"));
-				
-				rwLink.setColor(resultSet.getString("color"));
-				
-				rwLink.setRowId(resultSet.getString("row_id"));
-				
-				List<IRow> nodes = new RwNodeSelector(conn).loadRowsByParentId(id, isLock);
-
-				rwLink.setRwNodes(nodes);
-
-				for (IRow row : rwLink.getRwNodes()) {
-					RwNode obj = (RwNode) row;
-
-					rwLink.nodeMap.put(obj.rowId(), obj);
-				}
-				
 				List<IRow> names = new RwLinkNameSelector(conn).loadRowsByParentId(id, isLock);
 
-				rwLink.setRwLinkNames(names);
+				rwLink.setNames(names);
 
-				for (IRow row : rwLink.getRwLinkNames()) {
+				for (IRow row : rwLink.getNames()) {
 					RwLinkName obj = (RwLinkName) row;
 
 					rwLink.linkNameMap.put(obj.rowId(), obj);
@@ -135,4 +93,100 @@ public class RwLinkSelector implements ISelector {
 		return null;
 	}
 
+	public List<RwLink> loadByNodePid(int nodePid, boolean isLock) throws Exception {
+
+		List<RwLink> links = new ArrayList<RwLink>();
+
+		StringBuilder sb = new StringBuilder(
+				"select * from rw_link where (s_node_pid = :1 or e_node_pid = :2) and u_record!=2");
+
+		if (isLock) {
+			sb.append(" for update nowait");
+		}
+
+		PreparedStatement pstmt = null;
+
+		ResultSet resultSet = null;
+
+		try {
+			pstmt = conn.prepareStatement(sb.toString());
+
+			pstmt.setInt(1, nodePid);
+
+			pstmt.setInt(2, nodePid);
+
+			resultSet = pstmt.executeQuery();
+
+			while (resultSet.next()) {
+				RwLink rwLink = new RwLink();
+
+				setAttr(rwLink, resultSet);
+
+				// 获取LINK对应的关联数据 rd_link_name
+				List<IRow> names = new RwLinkNameSelector(conn).loadRowsByParentId(rwLink.getPid(), isLock);
+
+				rwLink.setNames(names);
+
+				links.add(rwLink);
+			}
+		} catch (Exception e) {
+
+			throw e;
+
+		} finally {
+			try {
+				if (resultSet != null) {
+					resultSet.close();
+				}
+			} catch (Exception e) {
+
+			}
+
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+			} catch (Exception e) {
+
+			}
+
+		}
+
+		return links;
+
+	}
+
+	private void setAttr(RwLink rwLink, ResultSet resultSet) throws Exception {
+		rwLink.setPid(resultSet.getInt("link_pid"));
+
+		rwLink.setFeaturePid(resultSet.getInt("feature_pid"));
+
+		rwLink.setsNodePid(resultSet.getInt("s_node_pid"));
+
+		rwLink.seteNodePid(resultSet.getInt("e_node_pid"));
+
+		rwLink.setKind(resultSet.getInt("kind"));
+
+		rwLink.setForm(resultSet.getInt("form"));
+
+		rwLink.setLength(resultSet.getDouble("length"));
+
+		STRUCT struct = (STRUCT) resultSet.getObject("geometry");
+
+		Geometry geometry = GeoTranslator.struct2Jts(struct, 100000, 0);
+
+		rwLink.setGeometry(geometry);
+
+		rwLink.setMeshId(resultSet.getInt("mesh_id"));
+
+		rwLink.setScale(resultSet.getInt("scale"));
+
+		rwLink.setDetailFlag(resultSet.getInt("detail_flag"));
+
+		rwLink.setEditFlag(resultSet.getInt("edit_flag"));
+
+		rwLink.setColor(resultSet.getString("color"));
+
+		rwLink.setRowId(resultSet.getString("row_id"));
+	}
 }

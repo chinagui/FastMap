2affed26ae7b400196da003cf1a12d59574ca416
wangshishuai3966 <wangshishuai3966@navinfo.com>
2016-06-25 15:35:56 +0800

    修改变迁图统计接口

--- api/api-statics/src/main/java/com/navinfo/dataservice/api/statics/iface/StaticsApi.java
+++ api/api-statics/src/main/java/com/navinfo/dataservice/api/statics/iface/StaticsApi.java
@@ -1,25 +1,28 @@
 package com.navinfo.dataservice.api.statics.iface;
 
 import java.util.List;
+import java.util.Set;
 
 import com.navinfo.dataservice.api.statics.model.GridChangeStatInfo;
 import com.navinfo.dataservice.api.statics.model.GridStatInfo;
 
-
-/** 
-* @ClassName: DatahubApiService 
-* @author Xiao Xiaowen 
-* @date 2016年6月6日 下午8:30:06 
-* @Description: TODO
-*  
-*/
+/**
+ * @ClassName: DatahubApiService
+ * @author Xiao Xiaowen
+ * @date 2016年6月6日 下午8:30:06
+ * @Description: TODO
+ * 
+ */
 public interface StaticsApi {
-	List<GridStatInfo> getLatestCollectStatByGrids(List<String> grids)throws Exception;
-	
-	List<GridStatInfo> getLatestDailyEditStatByGrids(List<String> grids)throws Exception;
-	
-	List<GridStatInfo> getLatestMonthlyEditStatByGrids(List<String> grids)throws Exception;
-	
+	List<GridStatInfo> getLatestCollectStatByGrids(List<String> grids)
+			throws Exception;
+
+	List<GridStatInfo> getLatestDailyEditStatByGrids(List<String> grids)
+			throws Exception;
+
+	List<GridStatInfo> getLatestMonthlyEditStatByGrids(List<String> grids)
+			throws Exception;
+
 	/**
 	 * 
 	 * 获取变迁统计信息
@@ -31,7 +34,7 @@ public interface StaticsApi {
 	 * @return
 	 * @throws Exception
 	 */
-	List<GridChangeStatInfo> getChangeStatByGrids(List<String> grids, int type, int stage, String date)throws Exception;
-	
-}
+	List<GridChangeStatInfo> getChangeStatByGrids(Set<String> grids, int type,
+			int stage, String date) throws Exception;
 
+}

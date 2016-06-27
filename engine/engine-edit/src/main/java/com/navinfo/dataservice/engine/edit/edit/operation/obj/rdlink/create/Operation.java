b9771f3fd10ea9b4b62df68b629015e925a6e159
zhaokaikai <zhaokaikai@navinfo.com>
2016-06-27 20:27:23 +0800

    修改rdlink创建

--- engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/edit/operation/obj/rdlink/create/Operation.java
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/edit/operation/obj/rdlink/create/Operation.java
@@ -189,7 +189,7 @@ public class Operation implements IOperation {
 						breakJson, breakJson.toString());
 				com.navinfo.dataservice.engine.edit.edit.operation.topo.breakin.breakrdpoint.Process breakProcess = new com.navinfo.dataservice.engine.edit.edit.operation.topo.breakin.breakrdpoint.Process(
 						breakCommand, conn);
-				breakProcess.run();
+				breakProcess.innerRun();
 			}
 		}
 	}

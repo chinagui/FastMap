6a09b7a009d867dc0715487ccad63001c3d1606e
zhangxiaolong4943 <zhangxiaolong4943@navinfo.com>
2016-07-02 17:10:14 +0800

    完善修行代码

--- engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/edit/operation/topo/repair/repairrdlink/Process.java
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/edit/operation/topo/repair/repairrdlink/Process.java
@@ -5,10 +5,8 @@ import java.util.List;
 import com.navinfo.dataservice.commons.geom.GeoTranslator;
 import com.navinfo.dataservice.dao.glm.model.rd.gsc.RdGsc;
 import com.navinfo.dataservice.dao.glm.model.rd.link.RdLink;
-import com.navinfo.dataservice.dao.glm.model.rd.node.RdNode;
 import com.navinfo.dataservice.dao.glm.selector.rd.gsc.RdGscSelector;
 import com.navinfo.dataservice.dao.glm.selector.rd.link.RdLinkSelector;
-import com.navinfo.dataservice.dao.glm.selector.rd.node.RdNodeSelector;
 import com.navinfo.dataservice.engine.edit.edit.operation.AbstractCommand;
 import com.navinfo.dataservice.engine.edit.edit.operation.AbstractProcess;
 import com.vividsolutions.jts.geom.Geometry;
@@ -17,14 +15,7 @@ public class Process extends AbstractProcess<Command> {
 
 	public Process(AbstractCommand command) throws Exception {
 		super(command);
-		// TODO Auto-generated constructor stub
 	}
-
-	private RdLink updateLink;
-
-	private RdNode snode;
-
-	private RdNode enode;
 
 	private Check check = new Check();
 
@@ -33,18 +24,12 @@ public class Process extends AbstractProcess<Command> {
 
 		int linkPid = this.getCommand().getLinkPid();
 
-		this.updateLink = (RdLink) new RdLinkSelector(this.getConn()).loadById(linkPid, true);
-
-		RdNodeSelector nodeSelector = new RdNodeSelector(this.getConn());
-
-		this.snode = (RdNode) nodeSelector.loadById(updateLink.getsNodePid(), true);
-
-		this.enode = (RdNode) nodeSelector.loadById(updateLink.geteNodePid(), true);
+		this.getCommand().setUpdateLink((RdLink) new RdLinkSelector(this.getConn()).loadById(linkPid, true));
 
 		// 查询需要修行的线上是否存在立交
 		RdGscSelector gscSelector = new RdGscSelector(this.getConn());
 
-		List<RdGsc> gscList = gscSelector.onlyLoadRdGscLinkByLinkPid(linkPid, "RW_LINK", true);
+		List<RdGsc> gscList = gscSelector.onlyLoadRdGscLinkByLinkPid(linkPid, "RD_LINK", true);
 
 		this.getCommand().setGscList(gscList);
 
@@ -71,7 +56,7 @@ public class Process extends AbstractProcess<Command> {
 
 	@Override
 	public String exeOperation() throws Exception {
-		return new Operation(this.getConn(), this.getCommand(), updateLink, snode, enode, check).run(this.getResult());
+		return new Operation(this.getConn(), this.getCommand()).run(this.getResult());
 	}
 
 }

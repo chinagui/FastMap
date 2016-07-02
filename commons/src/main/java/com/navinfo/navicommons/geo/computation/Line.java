e3501f81160d30bda12a6f963e7a4585d8f627fb
Xiaowen-Navinfo <xiaoxiaowen4127@navinfo.com>
2016-07-02 11:27:57 +0800

    fix bug

---
+++ commons/src/main/java/com/navinfo/navicommons/geo/computation/Line.java
@@ -0,0 +1,12 @@
+package com.navinfo.navicommons.geo.computation;
+
+/** 
+* @ClassName: Line 
+* @author Xiao Xiaowen 
+* @date 2016年7月2日 上午9:56:56 
+* @Description: TODO
+*/
+public abstract class Line {
+	
+	public abstract <T> getX();
+}

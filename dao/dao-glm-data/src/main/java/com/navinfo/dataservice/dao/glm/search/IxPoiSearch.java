7a89599a3b8291bc2683396308d662fbc10ecce3
deeryao <luyao1101@163.com>
2016-07-05 16:19:44 +0800

    修复 poi渲染查询poi父子关系bug

--- dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/search/IxPoiSearch.java
+++ dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/search/IxPoiSearch.java
@@ -57,16 +57,37 @@ public class IxPoiSearch implements ISearch {
 			int gap) throws Exception {
 		List<SearchSnapshot> list = new ArrayList<SearchSnapshot>();
 
-		String sql = "with tmp1 as  (select pid, kind_code,x_guide, y_guide, geometry, row_id     from ix_poi    where sdo_relate(geometry, sdo_geometry(    :1    , 8307), 'mask=anyinteract') =          'TRUE'      and u_record != 2),  tmp2 as  (SELECT COUNT(1) PARENTCOUNT     FROM IX_POI_PARENT P, tmp1 a    WHERE p.parent_poi_pid = a.PID      and p.u_record != 2),  tmp3 as  (SELECT COUNT(1) CHILDCOUNT     FROM IX_POI_CHILDREN P, tmp1 a    WHERE p.child_poi_pid = a.PID      and p.u_record != 2) select  a.*,  b.status,  c.PARENTCOUNT,  d.CHILDCOUNT,  (SELECT NAME     FROM ix_poi_name p    WHERE p.POI_PID = a.PID      AND p.LANG_CODE = 'CHI'      AND p.NAME_CLASS = 1      AND p.NAME_TYPE = 2      AND p.u_record != 2) NAME   from tmp1 a, poi_edit_status b, tmp2 c, tmp3 d  where a.row_id = b.row_id";
+		//String sql = "with tmp1 as  (select pid, kind_code,x_guide, y_guide, geometry, row_id     from ix_poi    where sdo_relate(geometry, sdo_geometry(    :1    , 8307), 'mask=anyinteract') =          'TRUE'      and u_record != 2),  tmp2 as  (SELECT COUNT(1) PARENTCOUNT     FROM IX_POI_PARENT P, tmp1 a    WHERE p.parent_poi_pid = a.PID      and p.u_record != 2),  tmp3 as  (SELECT COUNT(1) CHILDCOUNT     FROM IX_POI_CHILDREN P, tmp1 a    WHERE p.child_poi_pid = a.PID      and p.u_record != 2) select  a.*,  b.status,  c.PARENTCOUNT,  d.CHILDCOUNT,  (SELECT NAME     FROM ix_poi_name p    WHERE p.POI_PID = a.PID      AND p.LANG_CODE = 'CHI'      AND p.NAME_CLASS = 1      AND p.NAME_TYPE = 2      AND p.u_record != 2) NAME   from tmp1 a, poi_edit_status b, tmp2 c, tmp3 d  where a.row_id = b.row_id";
+		
+		StringBuilder sb=new StringBuilder();
+		
+		sb.append("WITH TMP1 AS ( ");
+		sb.append("SELECT PID, KIND_CODE, X_GUIDE, Y_GUIDE, GEOMETRY, ROW_ID ");
+		sb.append("FROM IX_POI WHERE ");
+		sb.append("SDO_RELATE(GEOMETRY, SDO_GEOMETRY(:1, 8307), 'mask=anyinteract') = 'TRUE' ");
+		sb.append("AND U_RECORD != 2) ");
+		sb.append("SELECT A.*, B.STATUS, ");
+		sb.append("(SELECT /*+ leading(P,A) use hash(P,A)*/ ");		
+		sb.append("COUNT(1) FROM IX_POI_PARENT P WHERE ");
+		sb.append("P.PARENT_POI_PID = A.PID AND P.U_RECORD != 2) PARENTCOUNT, ");
+		sb.append("(SELECT /*+ leading(P,A) use hash(P,A)*/ ");
+		sb.append("COUNT(1) FROM IX_POI_CHILDREN C WHERE C.CHILD_POI_PID = A.PID AND C.U_RECORD != 2) CHILDCOUNT, ");
+		sb.append("(SELECT /*+ leading(P,A) use hash(P,A)*/ NAME FROM IX_POI_NAME P ");
+		sb.append("WHERE P.POI_PID = A.PID AND P.LANG_CODE = 'CHI' AND P.NAME_CLASS = 1 AND P.NAME_TYPE = 2 AND P.U_RECORD != 2) NAME ");
+		sb.append("FROM TMP1 A, POI_EDIT_STATUS B WHERE A.ROW_ID = B.ROW_ID ");
 		
 		PreparedStatement pstmt = null;
 
 		ResultSet resultSet = null;
 
 		try {
-			pstmt = conn.prepareStatement(sql);
+			//pstmt = conn.prepareStatement(sql);
 
-			System.out.println(sql);
+			//System.out.println(sql);
+			
+			pstmt = conn.prepareStatement(sb.toString());
+
+			System.out.println(sb.toString());
 
 			String wkt = MercatorProjection.getWktWithGap(x, y, z, gap);
 
--- engine/engine-edit/src/test/java/com/navinfo/dataservice/engine/edit/luyao/commonTest.java
+++ engine/engine-edit/src/test/java/com/navinfo/dataservice/engine/edit/luyao/commonTest.java
@@ -1,6 +1,8 @@
 package com.navinfo.dataservice.engine.edit.luyao;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
 import net.sf.json.JSONObject;
 
@@ -10,8 +12,11 @@ import org.junit.Test;
 import com.navinfo.dataservice.bizcommons.datasource.DBConnector;
 import com.navinfo.dataservice.dao.glm.iface.IRow;
 import com.navinfo.dataservice.dao.glm.iface.ObjLevel;
+import com.navinfo.dataservice.dao.glm.iface.ObjType;
 import com.navinfo.dataservice.dao.glm.selector.rd.branch.RdBranchSelector;
 import com.navinfo.dataservice.engine.edit.InitApplication;
+import com.navinfo.dataservice.engine.edit.edit.operation.Transaction;
+import com.navinfo.dataservice.engine.edit.edit.search.SearchProcess;
 
 public class commonTest  extends InitApplication{
 
@@ -46,6 +51,42 @@ public class commonTest  extends InitApplication{
 					System.out.println(row.Serialize(ObjLevel.FULL));
 				}
 			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	@Test
+	public void createZoneFace() throws Exception {
+		String parameter = "{\"command\":\"CREATE\",\"type\":\"ZONEFACE\",\"dbId\":42,\"data\":{\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[116.50073736906052,39.999875527018744],[116.50084733963013,40.00010770716541],[116.50126039981842,40.000105652477295],[116.50119334459303,39.9999412772289],[116.50108337402342,40.00005223056498],[116.50091439485549,39.99993716784262],[116.50090634822845,40.00005633994432],[116.50073736906052,39.999875527018744]]}}}";
+		Transaction t = new Transaction(parameter);
+		String msg = t.run();
+	}
+
+	@Test
+	public void createRwNode() throws Exception {
+		String parameter = "{\"command\":\"BREAK\",\"dbId\":42,\"objId\":100006675,\"data\":{\"longitude\":116.4776915626939,\"latitude\":40.013441046805035},\"type\":\"RWNODE\"}";
+		Transaction t = new Transaction(parameter);
+		String msg = t.run();
+	}
+	
+	
+	@Test
+	public void getTitleWithGap()
+	{
+		Connection conn = null;
+		try{
+			conn = DBConnector.getInstance().getConnectionById(8);
+			
+			SearchProcess p = new SearchProcess(conn);
+			
+			List<ObjType> objType = new ArrayList<>();
+			
+			objType.add(ObjType.IXPOI);
+
+			System.out.println(p.searchDataByTileWithGap(objType, 107937, 49616, 17, 80));
 
 		} catch (Exception e) {
 			e.printStackTrace();

442647ed3675cfb27adc2b0d3638595edf392ad6
Xiaowen-Navinfo <xiaoxiaowen4127@navinfo.com>
2016-07-08 10:12:47 +0800

    cop联调

---
+++ scripts-interface/src/test/java/com/navinfo/dataservice/bizcommons/CkResultToolTest.java
@@ -0,0 +1,36 @@
+package com.navinfo.dataservice.bizcommons;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.navinfo.dataservice.api.datahub.iface.DatahubApi;
+import com.navinfo.dataservice.api.datahub.model.DbInfo;
+import com.navinfo.dataservice.bizcommons.datarow.CkResultTool;
+import com.navinfo.dataservice.commons.database.DbConnectConfig;
+import com.navinfo.dataservice.commons.database.OracleSchema;
+import com.navinfo.dataservice.commons.springmvc.ApplicationContextUtil;
+import com.navinfo.dataservice.commons.springmvc.ClassPathXmlAppContextInit;
+
+/** 
+ * @ClassName: CkResultToolTest
+ * @author xiaoxiaowen4127
+ * @date 2016年7月7日
+ * @Description: CkResultToolTest.java
+ */
+public class CkResultToolTest extends ClassPathXmlAppContextInit {
+	@Before
+	public void before(){
+		initContext(new String[]{"dubbo-app-scripts.xml","dubbo-scripts.xml"});
+	}
+	@Test
+	public void generateCkResultGrid_01(){
+		try{
+			DatahubApi datahub = (DatahubApi)ApplicationContextUtil.getBean("datahubApi");
+			DbInfo db = datahub.getDbById(192);
+			OracleSchema schema = new OracleSchema(DbConnectConfig.createConnectConfig(db.getConnectParam()));
+			CkResultTool.generateCkResultGrid(schema);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}

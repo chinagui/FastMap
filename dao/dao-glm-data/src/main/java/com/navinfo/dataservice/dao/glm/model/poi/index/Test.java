fc6ba90feb9b682a96ed0d8427ba7e3400387878
zhangxiaolong4943 <zhangxiaolong4943@navinfo.com>
2016-07-13 14:09:55 +0800

    删除无用的Test.java类

--- dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/model/poi/index/Test.java
+++
@@ -1,5 +0,0 @@
-package com.navinfo.dataservice.dao.glm.model.poi.index;
-
-public class Test {
-
-}
--- dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/operator/poi/deep/Test.java
+++
@@ -1,5 +0,0 @@
-package com.navinfo.dataservice.dao.glm.operator.poi.deep;
-
-public class Test {
-
-}

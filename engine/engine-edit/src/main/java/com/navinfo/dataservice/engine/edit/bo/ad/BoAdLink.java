dc8d78ecd08f43ab8b469b56f33ea3f636b945d9
Xiaowen-Navinfo <xiaoxiaowen4127@navinfo.com>
2016-07-15 16:04:09 +0800

    test

---
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/bo/ad/BoAdLink.java
@@ -0,0 +1,11 @@
+package com.navinfo.dataservice.engine.edit.bo.ad;
+
+/** 
+ * @ClassName: BoAdLink
+ * @author xiaoxiaowen4127
+ * @date 2016年7月15日
+ * @Description: BoAdLink.java
+ */
+public class BoAdLink {
+
+}

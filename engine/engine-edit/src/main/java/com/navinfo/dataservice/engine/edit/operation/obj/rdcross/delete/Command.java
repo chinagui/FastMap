8079adf55b99fda12e4d2d2229546d49d3adbb8e
zhangxiaolong4943 <zhangxiaolong4943@navinfo.com>
2016-07-21 16:20:07 +0800

    新增删除路口对交通信号灯的关系影响

--- engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/obj/rdcross/delete/Command.java
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/obj/rdcross/delete/Command.java
@@ -1,17 +1,19 @@
 package com.navinfo.dataservice.engine.edit.operation.obj.rdcross.delete;
 
-import net.sf.json.JSONObject;
+import java.util.List;
 
-import com.navinfo.dataservice.dao.glm.iface.ICommand;
+import com.navinfo.dataservice.dao.glm.iface.IRow;
 import com.navinfo.dataservice.dao.glm.iface.ObjType;
 import com.navinfo.dataservice.dao.glm.iface.OperType;
 import com.navinfo.dataservice.engine.edit.operation.AbstractCommand;
 
+import net.sf.json.JSONObject;
+
 public class Command extends AbstractCommand {
 
 	private String requester;
-
-//	private int projectId;
+	
+	private List<IRow> trafficsignals;
 
 	private int pid;
 	
@@ -23,13 +25,13 @@ public class Command extends AbstractCommand {
 		this.pid = pid;
 	}
 
-//	public int getProjectId() {
-//		return projectId;
-//	}
-//
-//	public void setProjectId(int projectId) {
-//		this.projectId = projectId;
-//	}
+	public List<IRow> getTrafficsignals() {
+		return trafficsignals;
+	}
+
+	public void setTrafficsignals(List<IRow> trafficsignals) {
+		this.trafficsignals = trafficsignals;
+	}
 
 	@Override
 	public OperType getOperType() {
@@ -49,7 +51,6 @@ public class Command extends AbstractCommand {
 	public Command(JSONObject json, String requester) {
 		this.requester = requester;
 
-//		this.projectId = json.getInt("projectId");
 		this.setDbId(json.getInt("dbId"));
 
 		this.pid = json.getInt("objId");

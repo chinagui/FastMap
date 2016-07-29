648308ffee919c6ac98643f12660007e98fa498e
yunfma <mayunfei@navinfo.com>
2016-07-29 18:32:02 +0800

    LogWriter bug fixed

--- toolbox/import-core/src/main/java/com/navinfo/dataservice/impcore/flushbylog/LogWriter.java
+++ toolbox/import-core/src/main/java/com/navinfo/dataservice/impcore/flushbylog/LogWriter.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
@@ -14,6 +17,7 @@ import oracle.spatial.geometry.JGeometry;
 import oracle.spatial.util.WKT;
 import oracle.sql.STRUCT;
 
+import com.alibaba.dubbo.common.utils.CollectionUtils;
 import com.navinfo.dataservice.commons.database.oracle.MyPoolGuardConnectionWrapper;
 import com.navinfo.dataservice.commons.log.LoggerRepos;
 import com.navinfo.dataservice.commons.util.DateUtils;
@@ -57,6 +61,65 @@ public class LogWriter {
 			}
 		}
 	}
+	static final Set includeFiledsSet=new HashSet();
+	static{
+		String[] includeFiledsArr = new String[]{"pid",
+				"kind_code",
+				"geometry",
+				"x_guide",
+				"y_guide",
+				"link_pid",
+				"side",
+				"name_groupid",
+				"road_flag",
+				"pmesh_id",
+				"admin_real",
+				"importance",
+				"chain",
+				"airport_code",
+				"access_flag",
+				"open_24h",
+				"mesh_id_5k",
+				"mesh_id",
+				"region_id",
+				"post_code",
+				"edit_flag",
+				"dif_groupid",
+				"reserved",
+				"state",
+				"field_state",
+				"label",
+				"type",
+				"address_flag",
+				"ex_priority",
+				"edition_flag",
+				"poi_memo",
+				"old_blockcode",
+				"old_name",
+				"old_address",
+				"old_kind",
+				"poi_num",
+				"log",
+				"task_id",
+				"data_version",
+				"field_task_id",
+				"verified_flag",
+				"collect_time",
+				"geo_adjust_flag",
+				"full_attr_flag",
+				"old_x_guide",
+				"old_y_guide",
+				"u_record",
+				"u_fields",
+				"u_date",
+				"row_id",
+				"level",
+				"sports_venue",
+				"indoor",
+				"vip_flag"};
+		includeFiledsSet.addAll(Arrays.asList(includeFiledsArr));
+	}
+	
 	private int insertData(EditLog editLog) {
 
 		StringBuilder sb = new StringBuilder("insert into ");
@@ -78,14 +141,23 @@ public class LogWriter {
 			int keySize = json.keySet().size();
 
 			int tmpPos = 0;
-
+			//"access_flag":0,"address_flag":0,"admin_real":0,"airport_code":"","chain":"","changed_fields":{},"collect_time":"20160729165352","data_version":"250+","dif_groupid":"","edit_flag":1,"edition_flag":"","ex_priority":"","field_state":"改种别代码","field_task_id":0,"full_attr_flag":9,"geo_adjust_flag":9,"geometry":"POINT (116.47389 40.01167)","importance":0,"indoor":0,"kind_code":"110101","label":"","\"LEVEL\"":"B2","link_pid":574126,"log":"改名称|改分类|改POI_LEVEL|改RELATION","mesh_id":605603,"mesh_id_5k":"","name_groupid":0,"old_address":"","old_blockcode":"","old_kind":"110101","old_name":"测试餐饮企业集团","old_x_guide":0,"old_y_guide":0,"open_24h":0,"pid":1152117135,"pmesh_id":0,"poi_memo":"","poi_num":"00166420160729165352","post_code":"","region_id":0,"reserved":"","road_flag":0,"row_id":"00B0AD28E8A74984AC6C94C749BC653E","side":0,"sports_venue":"","state":0,"status":0,"task_id":0,"type":0,"u_date":"","u_record":1,"verified_flag":9,"vip_flag":"","x_guide":116.47389,"y_guide":40.01162}
+			
+			
 			while (it.hasNext()) {
+				String filed = it.next();
+				if("IX_POI".equalsIgnoreCase(tableName)&& !includeFiledsSet.contains(filed) ){
+					continue;
+				}
+				if ("changed_fields".equalsIgnoreCase(filed)||"status".equalsIgnoreCase(filed)){
+					continue;
+				}
 				if (++tmpPos < keySize) {
-					sb.append(it.next());
+					sb.append(filed);
 
 					sb.append(",");
 				} else {
-					sb.append(it.next());
+					sb.append(filed);
 				}
 			}
 			boolean hasUrecord = false;
@@ -106,6 +178,12 @@ public class LogWriter {
 			this.log.debug(json.keys());
 			int i =0;
 			for (Object key : json.keySet()){
+				if("IX_POI".equalsIgnoreCase(tableName)&& !includeFiledsSet.contains(key.toString()) ){
+					continue;
+				}
+				if ("changed_fields".equalsIgnoreCase(key.toString())||"status".equalsIgnoreCase(key.toString())){
+					continue;
+				}
 				if (i==0){
 					sb.append("?");
 				}else{
@@ -127,9 +205,14 @@ public class LogWriter {
 			pstmt = this.targetDbConn.prepareStatement(sb.toString());
 
 			while (it.hasNext()) {
-				tmpPos++;
-
 				String keyName = it.next();
+				if("IX_POI".equalsIgnoreCase(tableName)&& !includeFiledsSet.contains(keyName) ){
+					continue;
+				}
+				if ("changed_fields".equalsIgnoreCase(keyName.toString())||"status".equalsIgnoreCase(keyName.toString())){
+					continue;
+				}
+				tmpPos++;
 
 				Object valObj = json.get(keyName);
 

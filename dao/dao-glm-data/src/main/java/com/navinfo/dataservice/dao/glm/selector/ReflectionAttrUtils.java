f59fb91a76c14a10170de2c94ac19d4776e737d7
zhangyuntao <zhangyuntao@navinfo.com>
2016-08-19 10:58:39 +0800

    修改属性赋值

--- dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/selector/ReflectionAttrUtils.java
+++ dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/selector/ReflectionAttrUtils.java
@@ -14,7 +14,10 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
-import java.sql.Types;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.CharUtils;
 
 import com.navinfo.dataservice.commons.geom.GeoTranslator;
 import com.navinfo.dataservice.commons.util.StringUtils;
@@ -37,53 +40,58 @@ public class ReflectionAttrUtils {
 	 * @throws Exception
 	 */
 	public static void executeResultSet(IRow row, ResultSet rs) throws Exception {
+		Class<?> clazz = row.getClass();
 		ResultSetMetaData rsm = rs.getMetaData();
 		int columnCount = rsm.getColumnCount();
-		Field[] fields = row.getClass().getDeclaredFields();
-		for (int i = 0; i < fields.length; i++) {
-			Field field = fields[i];
-			String fieldName = field.getName();
+		Map<String, Object> map = new HashMap<String, Object>();
+		for (int i = 1; i <= columnCount; i++) {
+			map.put(fieldToProperty(rsm.getColumnName(i), row), rs.getObject(i));
+		}
+		for (String fieldName : map.keySet()) {
+			if ("uRecord".equals(fieldName) || "uDate".equals(fieldName) || "uFields".equals(fieldName))
+				continue;
+			Field field = null;
 			if (fieldName.equals("pid") && row instanceof IObj) {
-				String pkName = ((IObj) row).primaryKey();
-				Object value = rs.getInt(pkName);
+				field = clazz.getDeclaredField("pid");
 				field.setAccessible(true);
-				field.set(row, value);
+				field.set(row, Integer.valueOf(map.get("pid").toString()));
 				continue;
 			}
-			for (int j = 1; j <= columnCount; j++) {
-				String columnName = rsm.getColumnName(j);
-				if (columnName.equalsIgnoreCase(StringUtils.toColumnName(fieldName))) {
-					int columnType = rsm.getColumnType(j);
-					Object value = rs.getObject(j);
-					if (value != null) {
-						if (Types.VARBINARY == columnType || Types.VARCHAR == columnType) {
-							value = rs.getString(columnName);
-						}
-						if (Types.NUMERIC == columnType) {
-							if (value.toString().contains(".")) {
-								value = ((BigDecimal) value).doubleValue();
-							} else {
-								BigDecimal tempValue = (BigDecimal) value;
-								if (tempValue.doubleValue() > Integer.MAX_VALUE) {
-									value = BigInteger.valueOf(tempValue.longValue());
-								} else {
-									value = tempValue.intValue();
-								}
-							}
-						}
-						if (Types.STRUCT == columnType) {
-							value = GeoTranslator.struct2Jts((STRUCT) value, 100000, 0);
-						}
-						if (Types.TIMESTAMP == columnType) {
-							value = rs.getTimestamp(columnName);
-						}
-						field.setAccessible(true);
-						field.set(row, value);
-					}
+			field = clazz.getDeclaredField(fieldName);
+			Object value = map.get(fieldName);
+			if (null != value) {
+				switch (field.getType().getName()) {
+				case "java.lang.String":
+					if (fieldName.equalsIgnoreCase("rowid"))
+						value = rs.getString("row_id");
+					else
+						value = String.valueOf(value);
+					break;
+				case "int":
+					value = Integer.valueOf(value.toString());
+					break;
+				case "double":
+					value = Double.valueOf(value.toString());
+					break;
+				case "java.math.BigInteger":
+					value = new BigInteger(value.toString());
+					break;
+				case "java.math.BigDecimal":
+					value = new BigDecimal(value.toString());
+				case "java.sql.Date":
+				case "java.util.Date":
+					value = rs.getTimestamp(StringUtils.toColumnName(fieldName));
+					break;
+				case "com.vividsolutions.jts.geom.Geometry":
+					value = GeoTranslator.struct2Jts((STRUCT) value, 100000, 0);
+				default:
 					break;
 				}
 			}
+			field.setAccessible(true);
+			field.set(row, value);
 		}
+
 	}
 
 	/**
@@ -142,6 +150,38 @@ public class ReflectionAttrUtils {
 		default:
 			throw new Exception("不支持的表名转对象名称:" + tableName);
 		}
+	}
+
+	/**
+	 * 字段转换成对象属性 例如：user_name to userName
+	 * 
+	 * @param field
+	 * @return
+	 */
+	public static String fieldToProperty(String field, IRow row) {
+		if (null == field) {
+			return "";
+		}
+		if (row instanceof IObj) {
+			if (((IObj) row).primaryKey().equalsIgnoreCase(field))
+				return "pid";
+		}
+		char[] chars = field.toLowerCase().toCharArray();
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < chars.length; i++) {
+			char c = chars[i];
+			if (c == '_') {
+				int j = i + 1;
+				if (j < chars.length) {
+					sb.append(org.apache.commons.lang.StringUtils.upperCase(CharUtils.toString(chars[j])));
+					i++;
+				}
+			} else {
+				sb.append(c);
+			}
+		}
+		return sb.toString();
+
 	}
 
 }

207876b673a7b97b993143bfe05228dfe9157db1
zhangxiaolong4943 <zhangxiaolong4943@navinfo.com>
2016-08-22 19:33:33 +0800

    CRF对象添加关系维护调用的方法

--- dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/selector/rd/crf/RdObjectSelector.java
+++ dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/selector/rd/crf/RdObjectSelector.java
@@ -7,39 +7,45 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.navinfo.dataservice.dao.glm.iface.IRow;
+import com.navinfo.dataservice.dao.glm.iface.ObjType;
 import com.navinfo.dataservice.dao.glm.model.rd.crf.RdObject;
+import com.navinfo.dataservice.dao.glm.model.rd.crf.RdObjectInter;
+import com.navinfo.dataservice.dao.glm.model.rd.crf.RdObjectLink;
+import com.navinfo.dataservice.dao.glm.model.rd.crf.RdObjectRoad;
 import com.navinfo.dataservice.dao.glm.selector.AbstractSelector;
 import com.navinfo.navicommons.database.sql.DBUtils;
 
-/** 
-* @ClassName: RdObjectSelector 
-* @author Zhang Xiaolong
-* @date 2016年8月20日 下午5:36:33 
-* @Description: TODO
-*/
+/**
+ * @ClassName: RdObjectSelector
+ * @author Zhang Xiaolong
+ * @date 2016年8月20日 下午5:36:33
+ * @Description: TODO
+ */
 public class RdObjectSelector extends AbstractSelector {
-	
-	public RdObjectSelector(Connection conn)
-	{
+
+	public RdObjectSelector(Connection conn) {
 		super(RdObject.class, conn);
 	}
-	
+
 	/**
 	 * 通过crf道路pid查询可以选择的道路名
 	 * 如果选择的CRFO中，存在名称类型为立交桥名（连接路）并且属性为“匝道”或者名称类型为立交桥名（主路）的link，
 	 * 则此字段将CRFO中名称类型为立交桥名（连接路）并且属性为“匝道”或者名称类型为“立交桥名（主路）”的link上的道路名称显示出来，
 	 * 供用户下拉选择，并且可以手工编辑
+	 * 
 	 * @param pid
 	 * @param isLock
 	 * @return
 	 * @throws Exception
 	 */
-	public List<String> getRdObjectName(int pid,boolean isLock) throws Exception
-	{
+	public List<String> getRdObjectName(int pid, boolean isLock) throws Exception {
 		List<String> nameList = new ArrayList<>();
-		
+
 		PreparedStatement pstmt = null;
 		ResultSet resultSet = null;
 		try {
@@ -49,7 +55,7 @@ public class RdObjectSelector extends AbstractSelector {
 				sb.append(" for update nowait");
 			}
 			pstmt = getConn().prepareStatement(sb.toString());
-			
+
 			pstmt.setInt(1, pid);
 
 			resultSet = pstmt.executeQuery();
@@ -65,4 +71,59 @@ public class RdObjectSelector extends AbstractSelector {
 		}
 		return nameList;
 	}
-}	
+
+	/**
+	 * 根据crf对象的组成要素类型和要素类型的pid查询crf对象的map
+	 * @param pids 要素pids的string字符串
+	 * @param type 要素类型type
+	 * @param isLock 是否加锁
+	 * @return crf对象的map key:同一个crf对象组成要素的pid合并的字符串，value对应的pid
+	 * @throws Exception
+	 */
+	public Map<String,RdObject> loadRdObjectByPidAndType(String pids, ObjType type, boolean isLock) throws Exception {
+		Map<String,RdObject> objectMap = new HashMap<>();
+		PreparedStatement pstmt = null;
+		ResultSet resultSet = null;
+		String sql = "";
+		switch (type) {
+		case RDROAD:
+			sql = "select a.pid,listagg(b.road_pid,',')WITHIN GROUP(order by b.pid)as mapkey from rd_object a,rd_object_road b where a.PID = b.PID and b.ROAD_PID in(" + pids
+					+ ") and a.U_RECORD !=2 and b.U_RECORD !=2 group by a.pid";
+			break;
+		case RDINTER:
+			sql = "select a.pid,listagg(b.inter_pid,',')WITHIN GROUP(order by b.pid) as mapkey from rd_object a,rd_object_inter b where a.PID = b.PID and b.inter_PID in(" + pids
+					+ ") and a.U_RECORD !=2 and b.U_RECORD !=2 group by a.pid";
+			break;
+		case RDLINK:
+			sql = "select a.pid,listagg(b.link_pid,',')WITHIN GROUP(order by b.pid) as mapkey from rd_object a,rd_object_link b where a.PID = b.PID and b.link_pid in(" + pids
+					+ ") and a.U_RECORD !=2 and b.U_RECORD !=2 group by a.pid";
+			break;
+		default:
+			break;
+		}
+		try {
+			StringBuilder sb = new StringBuilder(sql);
+			pstmt = getConn().prepareStatement(sb.toString());
+			resultSet = pstmt.executeQuery();
+
+			while (resultSet.next()) {
+				String mapKey = resultSet.getString("mapkey");
+				RdObject obj = new RdObject();
+				obj.setPid(resultSet.getInt("pid"));
+				List<IRow> roads = loadRowsByClassParentId(RdObjectRoad.class, obj.getPid(), isLock, null);
+				obj.setRoads(roads);
+				List<IRow> inters = loadRowsByClassParentId(RdObjectInter.class, obj.getPid(), isLock, null);
+				obj.setInters(inters);
+				List<IRow> links = loadRowsByClassParentId(RdObjectLink.class, obj.getPid(), isLock, null);
+				obj.setLinks(links);
+				objectMap.put(mapKey, obj);
+			}
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			DBUtils.closeResultSet(resultSet);
+			DBUtils.closeStatement(pstmt);
+		}
+		return objectMap;
+	}
+}

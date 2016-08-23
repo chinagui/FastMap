610eda6f78c3adc2886f681fef496e7f881a0a88
xiaolvluo <zhangxiaoyi@navinfo.com>
2016-08-23 11:10:52 +0800

    GLM26017 GLM08031 GLM08049 RDCROSS001 RDCROSS002

---
+++ engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/GLM08031.java
@@ -0,0 +1,48 @@
+package com.navinfo.dataservice.engine.check.rules;
+
+import java.util.List;
+
+import com.navinfo.dataservice.dao.check.CheckCommand;
+import com.navinfo.dataservice.dao.glm.iface.IRow;
+import com.navinfo.dataservice.dao.glm.model.rd.restrict.RdRestriction;
+import com.navinfo.dataservice.dao.glm.model.rd.restrict.RdRestrictionDetail;
+import com.navinfo.dataservice.engine.check.core.baseRule;
+/**
+ * 交限	html	GLM08031	后台	路口交限里不允许有经过线信息
+ * @author zhangxiaoyi
+ *
+ */
+public class GLM08031 extends baseRule {
+
+	public GLM08031() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void preCheck(CheckCommand checkCommand) throws Exception {
+		for(IRow obj:checkCommand.getGlmList()){
+			//新增交限检查
+			if(obj instanceof RdRestriction ){
+				RdRestriction restriObj=(RdRestriction) obj;
+				List<IRow> details=restriObj.getDetails();
+				for(IRow objTmp:details){
+					RdRestrictionDetail detailObj=(RdRestrictionDetail) objTmp;
+					if(detailObj.getRelationshipType()==1){
+						List<IRow> vias=detailObj.getVias();
+						if(vias!=null&&vias.size()>0){
+							this.setCheckResult("", "", 0);
+							return;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public void postCheck(CheckCommand checkCommand) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/GLM08049.java
@@ -0,0 +1,28 @@
+package com.navinfo.dataservice.engine.check.rules;
+
+import com.navinfo.dataservice.dao.check.CheckCommand;
+import com.navinfo.dataservice.engine.check.core.baseRule;
+/**
+ * 交限	html	GLM08049	后台	路口交限的进入线到退出线无通路可行，即报LOG
+ * @author zhangxiaoyi
+ *
+ */
+public class GLM08049 extends baseRule {
+
+	public GLM08049() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void preCheck(CheckCommand checkCommand) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postCheck(CheckCommand checkCommand) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

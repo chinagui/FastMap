57fd167299428774703a9bef3e5fcc4caae0f755
zhaokaikai <zhaokaikai@navinfo.com>
2016-08-24 14:51:12 +0800

    测试

--- engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/obj/adlink/create/Command.java
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/obj/adlink/create/Command.java
@@ -4,7 +4,6 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 import com.navinfo.dataservice.commons.geom.GeoTranslator;
-import com.navinfo.dataservice.dao.glm.iface.ICommand;
 import com.navinfo.dataservice.dao.glm.iface.ObjType;
 import com.navinfo.dataservice.dao.glm.iface.OperType;
 import com.navinfo.dataservice.engine.edit.operation.AbstractCommand;

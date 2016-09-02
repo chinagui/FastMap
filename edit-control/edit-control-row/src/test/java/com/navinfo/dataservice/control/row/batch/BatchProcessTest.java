b88a0cb1cb853c84aba0b51a8f8da02eebb35128
wangdongbin <wangdongbin@navinfo.com>
2016-09-02 15:39:11 +0800

    行编批处理

--- edit-control/edit-control-row/src/test/java/com/navinfo/dataservice/control/row/batch/BatchProcessTest.java
+++ edit-control/edit-control-row/src/test/java/com/navinfo/dataservice/control/row/batch/BatchProcessTest.java
@@ -9,6 +9,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.navinfo.dataservice.bizcommons.datasource.DBConnector;
 import com.navinfo.dataservice.commons.springmvc.ApplicationContextUtil;
+import com.navinfo.dataservice.engine.edit.service.EditApiImpl;
 
 import net.sf.json.JSONObject;
 
@@ -28,8 +29,9 @@ public class BatchProcessTest {
 		Connection conn=null;
 		try {
 			conn = DBConnector.getInstance().getConnectionById(42);
+			EditApiImpl editApiImpl = new EditApiImpl(conn);
 			BatchProcess batchProcess = new BatchProcess();
-//			batchProcess.execute(json,conn);
+			batchProcess.execute(json,conn,editApiImpl);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();

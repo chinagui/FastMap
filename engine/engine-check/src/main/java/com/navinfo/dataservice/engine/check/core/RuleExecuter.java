ffc58b3034cd56793417fb50cbe0d3534845301b
deeryao <luyao1101@163.com>
2016-09-08 10:35:51 +0800

    在线检查

--- engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/core/RuleExecuter.java
+++ engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/core/RuleExecuter.java
@@ -80,6 +80,8 @@ public class RuleExecuter {
 			{variablevalue=VariablesFactory.getRdGatePid(data);break;}
 			case RDELECTRONICEYE_PID:
 			{variablevalue=VariablesFactory.getRdElectroniceye(data);break;}
+			case RDLANE_PID:
+			{variablevalue=VariablesFactory.getRdElectroniceye(data);break;}
 		}
 		if(!variablesValueMap.containsKey(variable)){
 			variablesValueMap.put(variable, new HashSet<String>());}

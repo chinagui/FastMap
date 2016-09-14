84cdae4a047b2be9a6181c3a69f37ebf2295e308
xiaolvluo <zhangxiaoyi@navinfo.com>
2016-09-14 10:10:35 +0800

    layer消息推送

--- engine/engine-man/src/main/java/com/navinfo/dataservice/engine/man/layer/LayerService.java
+++ engine/engine-man/src/main/java/com/navinfo/dataservice/engine/man/layer/LayerService.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import net.sf.json.JSONObject;
 import oracle.sql.CLOB;
@@ -20,8 +21,10 @@ import org.springframework.stereotype.Service;
 
 import com.navinfo.dataservice.api.man.model.Task;
 import com.navinfo.dataservice.bizcommons.datasource.DBConnector;
+import com.navinfo.dataservice.engin.man.message.MessageOperation;
 import com.navinfo.dataservice.engine.man.common.DbOperation;
 import com.navinfo.dataservice.engine.man.task.TaskOperation;
+import com.navinfo.dataservice.engine.man.userInfo.UserInfoOperation;
 import com.navinfo.dataservice.commons.geom.GeoTranslator;
 import com.navinfo.dataservice.commons.geom.Geojson;
 import com.navinfo.dataservice.commons.log.LoggerRepos;
@@ -54,6 +57,12 @@ public class LayerService {
 			String createSql = "insert into customised_layer (LAYER_ID, LAYER_NAME,GEOMETRY, CREATE_USER_ID, CREATE_DATE,STATUS) "
 					+ "values(customised_layer_seq.nextval,'"+layerName+"',sdo_geometry('"+wkt+"',8307),"+userId+",sysdate,1)";			
 			DbOperation.exeUpdateOrInsertBySql(conn, createSql);
+			
+			String msgTitle="重点区块创建";
+			List<String> msgContentList=new ArrayList<String>();
+			msgContentList.add("重点区块:"+layerName+"内容发生变更，请关注");
+			layerPushMsg(conn,msgTitle,msgContentList);
+			
 		}catch(Exception e){
 			DbUtils.rollbackAndCloseQuietly(conn);
 			log.error(e.getMessage(), e);
@@ -62,6 +71,29 @@ public class LayerService {
 			DbUtils.commitAndCloseQuietly(conn);
 		}
 	}
+	
+	/*重点区块几何变更
+	 * 1.所有生管角色
+	 * 2.所有采集角色
+	 * 3.所有日编角色
+	 * 4.所有月编角色
+	 * 重点区块:XXX(重点区块名称)内容发生变更，请关注*/
+	public void layerPushMsg(Connection conn,String msgTile,List<String> msgContentList) throws Exception{
+		String userSql="SELECT DISTINCT M.USER_ID FROM ROLE_USER_MAPPING M WHERE M.ROLE_ID IN (3, 4,5,6)";
+		List<Integer> userIdList=UserInfoOperation.getUserListBySql(conn, userSql);
+		Object[][] msgList=new Object[userIdList.size()*msgContentList.size()][3];
+		int num=0;
+		for(int userId:userIdList){
+			for(String msgContent:msgContentList){
+				msgList[num][0]=userId;
+				msgList[num][1]=msgTile;
+				msgList[num][2]=msgContent;
+				num+=1;
+			}
+		}
+		MessageOperation.batchInsert(conn,msgList);
+	}
+	
 	public void update(String layerId,String wkt,String layerName)throws Exception{
 		Connection conn = null;
 		try{
@@ -82,6 +114,18 @@ public class LayerService {
 			};
 			updateSql+=" where LAYER_ID="+layerId;
 			run.update(conn, baseSql+updateSql);
+			if (wkt!=null && StringUtils.isNotEmpty(wkt)){
+				String msgTitle="重点区块修改";
+				if (layerName==null|| StringUtils.isEmpty(layerName)){
+					String selectSql ="SELECT LAYER_ID,LAYER_NAME,T.GEOMETRY,CREATE_USER_ID,CREATE_DATE FROM CUSTOMISED_LAYER t"
+							+ " where LAYER_ID="+layerId;
+					List<HashMap> layerMap = query(selectSql, conn);
+					layerName=(String) layerMap.get(0).get("layerName");
+				};
+				List<String> msgContentList=new ArrayList<String>();
+				msgContentList.add("重点区块:"+layerName+"内容发生变更，请关注");
+				layerPushMsg(conn,msgTitle,msgContentList);
+			};
 		}catch(Exception e){
 			DbUtils.rollbackAndCloseQuietly(conn);
 			log.error(e.getMessage(), e);
@@ -96,7 +140,15 @@ public class LayerService {
 		try{
 			conn = DBConnector.getInstance().getManConnection();			
 			String updateSql = "UPDATE customised_layer SET STATUS=0 where LAYER_ID="+layerId;			
-			DbOperation.exeUpdateOrInsertBySql(conn, updateSql);	    	
+			DbOperation.exeUpdateOrInsertBySql(conn, updateSql);
+			String msgTitle="重点区块删除";
+			String selectSql ="SELECT LAYER_ID,LAYER_NAME,T.GEOMETRY,CREATE_USER_ID,CREATE_DATE FROM CUSTOMISED_LAYER t"
+						+ " where LAYER_ID="+layerId;
+			List<HashMap> layerMap = query(selectSql, conn);
+			String layerName=(String) layerMap.get(0).get("layerName");
+			List<String> msgContentList=new ArrayList<String>();
+			msgContentList.add("重点区块:"+layerName+"内容发生变更，请关注");
+			layerPushMsg(conn,msgTitle,msgContentList);
 		}catch(Exception e){
 			DbUtils.rollbackAndCloseQuietly(conn);
 			log.error(e.getMessage(), e);

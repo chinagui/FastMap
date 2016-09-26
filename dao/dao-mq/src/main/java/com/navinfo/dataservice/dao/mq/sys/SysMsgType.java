769611581a46c3927750c788db89d76f46849a31
hhmhsm <531476471@qq.com>
2016-09-26 08:40:27 +0800

    添加系统消息订阅和发布

---
+++ dao/dao-mq/src/main/java/com/navinfo/dataservice/dao/mq/sys/SysMsgType.java
@@ -0,0 +1,25 @@
+package com.navinfo.dataservice.dao.mq.sys;
+
+/**
+ * 
+ * @ClassName SysMsgType
+ * @author Han Shaoming
+ * @date 2016年9月21日 下午6:47:34
+ * @Description TODO
+ */
+public class SysMsgType {
+	/**
+	 * 发给所有人
+	 */
+	public static final String MSG_ALL_JOB="all_msg";
+	/**
+	 * 发给个人
+	 */
+	public static final String MSG_PERSONAL_JOB="personal_msg";
+	/**
+	 * 发给一组人
+	 */
+	public static final String MSG_GROUP_JOB="group_msg";
+	
+
+}
--- engine/engine-sys/src/main/java/com/navinfo/dataservice/engine/sys/msg/SysMsgService.java
+++ engine/engine-sys/src/main/java/com/navinfo/dataservice/engine/sys/msg/SysMsgService.java
@@ -16,11 +16,12 @@ import com.navinfo.navicommons.database.Page;
 import com.navinfo.navicommons.database.QueryRunner;
 import com.navinfo.navicommons.exception.ServiceException;
 
-/** 
- * @ClassName: MsgService
- * @author xiaoxiaowen4127
- * @date 2016年9月8日
- * @Description: MsgService.java
+/**
+ * 
+ * @ClassName SysMsgService
+ * @author Han Shaoming
+ * @date 2016年9月22日 下午4:44:23
+ * @Description TODO
  */
 public class SysMsgService {
 	protected Logger log = LoggerRepos.getLogger(this.getClass());
@@ -37,45 +38,126 @@ public class SysMsgService {
 	}
 	private SysMsgService(){}
 	
+	/**
+	 * 查询未读消息
+	 * @param userId
+	 * @return
+	 * @throws ServiceException
+	 */
 	public List<SysMsg> getUnread(long userId)throws ServiceException{
 		List<SysMsg> msgs = null;
-		Connection conn = null;
+		Connection sysConn = null;
 		try{
-			QueryRunner run = new QueryRunner();
-			conn = MultiDataSourceFactory.getInstance().getSysDataSource()
-					.getConnection();
+			QueryRunner queryRunner = new QueryRunner();
+			sysConn = MultiDataSourceFactory.getInstance().getSysDataSource().getConnection();
 			String sql = "SELECT T.* FROM SYS_MESSAGE T WHERE NOT EXISTS(SELECT 1 FROM SYS_MESSAGE_READ_LOG L WHERE T.MSG_ID=L.MSG_ID AND L.USER_ID=?) AND T.TARGET_USER_ID IN (0,?)";
-			msgs = run.query(conn, sql, new MultiRowHandler(), userId,userId);
+			msgs = queryRunner.query(sysConn, sql, new MultiRowHandler(), userId,userId);
 			//设置为已读
-			sql = "INSERT INTO SYS_MESSAGE_READ_LOG (MSG_ID,USER_ID) SELECT T.MSG_ID,? USER_ID FROM SYS_MESSAGE T WHERE NOT EXISTS(SELECT 1 FROM SYS_MESSAGE_READ_LOG L WHERE T.MSG_ID=L.MSG_ID AND L.USER_ID=?) AND T.TARGET_USER_ID IN (0,?)";
-			run.update(conn, sql, userId,userId,userId);
+			//sql = "INSERT INTO SYS_MESSAGE_READ_LOG (MSG_ID,USER_ID) SELECT T.MSG_ID,? USER_ID FROM SYS_MESSAGE T WHERE NOT EXISTS(SELECT 1 FROM SYS_MESSAGE_READ_LOG L WHERE T.MSG_ID=L.MSG_ID AND L.USER_ID=?) AND T.TARGET_USER_ID IN (0,?)";
+			//queryRunner.update(sysConn, sql, userId,userId,userId);
 			return msgs;
 		}catch(Exception e){
-			DbUtils.rollbackAndCloseQuietly(conn);
+			DbUtils.rollbackAndCloseQuietly(sysConn);
 			log.error(e.getMessage(), e);
 			throw new ServiceException("查询失败，原因为:"+e.getMessage(),e);
 		}finally{
-			DbUtils.commitAndCloseQuietly(conn);
-		}
-	}
-
-	public Page getRead(long userId,int pageNum,int pageSize)throws ServiceException{
-		Connection conn = null;
-		try{
-			QueryRunner run = new QueryRunner();
-			conn = MultiDataSourceFactory.getInstance().getSysDataSource()
-					.getConnection();
-			String sql = "SELECT T.* FROM SYS_MESSAGE T WHERE EXISTS(SELECT 1 FROM SYS_MESSAGE_READ_LOG L WHERE T.MSG_ID=L.MSG_ID AND L.USER_ID=?)";
-			return run.query(pageNum,pageSize,conn, sql, new MultiRowWithPageHandler(pageNum,pageSize), userId);
-		}catch(Exception e){
-			DbUtils.closeQuietly(conn);
-			log.error(e.getMessage(), e);
-			throw new ServiceException("查询失败，原因为:"+e.getMessage(),e);
-		}finally{
-			DbUtils.closeQuietly(conn);
+			DbUtils.commitAndCloseQuietly(sysConn);
 		}
 	}
 	
+	
+	/**
+	 * 更改系统消息状态
+	 * @param userId
+	 * @throws ServiceException
+	 */
+	public void updateMsgStatusToRead(long msgId,long userId)throws ServiceException{
+		Connection sysConn = null;
+		try{
+			QueryRunner queryRunner = new QueryRunner();
+			sysConn = MultiDataSourceFactory.getInstance().getSysDataSource().getConnection();
+			//设置为已读
+			String sql = "INSERT INTO SYS_MESSAGE_READ_LOG (MSG_ID,USER_ID) VALUES(?,?)";
+			Object[] params={msgId,userId};
+			queryRunner.update(sysConn, sql, params);
+		}catch(Exception e){
+			DbUtils.rollbackAndCloseQuietly(sysConn);
+			log.error(e.getMessage(), e);
+			throw new ServiceException("更改失败，原因为:"+e.getMessage(),e);
+		}finally{
+			DbUtils.commitAndCloseQuietly(sysConn);
+		}
+	}
+	
+	/**
+	 * 查询已读系统消息
+	 * @param userId
+	 * @param pageNum
+	 * @param pageSize
+	 * @return
+	 * @throws ServiceException
+	 */
+	public Page getReadMsg(long userId,int pageNum,int pageSize)throws ServiceException{
+		Connection sysConn = null;
+		try{
+			QueryRunner queryRunner = new QueryRunner();
+			sysConn = MultiDataSourceFactory.getInstance().getSysDataSource().getConnection();
+			String sql = "SELECT T.* FROM SYS_MESSAGE T WHERE EXISTS(SELECT 1 FROM SYS_MESSAGE_READ_LOG L WHERE T.MSG_ID=L.MSG_ID AND L.USER_ID=? AND L.MSG_STATUS=1)";
+			Object[] params = {userId};
+			Page page = queryRunner.query(pageNum,pageSize,sysConn, sql, new MultiRowWithPageHandler(pageNum,pageSize), params);
+			return page;
+		}catch(Exception e){
+			DbUtils.rollbackAndCloseQuietly(sysConn);
+			log.error(e.getMessage(), e);
+			throw new ServiceException("查询失败，原因为:"+e.getMessage(),e);
+		}finally{
+			DbUtils.commitAndCloseQuietly(sysConn);
+		}
+	}
+	
+	/**
+	 * 删除消息
+	 * @param userId
+	 * @param pageNum
+	 * @param pageSize
+	 * @return
+	 * @throws ServiceException
+	 */
+	public void deleteMsg(long msgId,long userId)throws ServiceException{
+		System.out.println("msgId==================="+msgId);
+		Connection sysConn = null;
+		try{
+			QueryRunner queryRunner = new QueryRunner();
+			sysConn = MultiDataSourceFactory.getInstance().getSysDataSource().getConnection();
+			//判断消息是否已读
+			String sql = "SELECT COUNT(1) FROM SYS_MESSAGE_READ_LOG WHERE MSG_ID=? AND USER_ID=?";
+			Object[] params={msgId,userId};
+			long count = queryRunner.queryForLong(sysConn,sql,params);
+			System.out.println("count=========="+count);
+			//是否为已读
+			if(count > 0){
+				//已读消息
+				System.out.println("================已读消息");
+				//修改删除日志的状态为2
+				String updateDeleteLogSql = "UPDATE SYS_MESSAGE_READ_LOG SET MSG_STATUS= 2 WHERE MSG_ID=? AND USER_ID=?";
+				Object[] updateDeleteLogParams={msgId,userId};
+				queryRunner.update(sysConn, updateDeleteLogSql, updateDeleteLogParams);
+			}else{
+				//未读消息
+				//添加删除日志并且状态为2
+				System.out.println("==================添加删除日志并且状态为2");
+				String insertDeleteLogSql = "INSERT INTO SYS_MESSAGE_READ_LOG(MSG_ID,USER_ID,MSG_STATUS) VALUES(?,?,2)";
+				Object[] insertDeleteLogParams={msgId,userId};
+				queryRunner.update(sysConn, insertDeleteLogSql, insertDeleteLogParams);
+			}
+		}catch(Exception e){
+			DbUtils.rollbackAndCloseQuietly(sysConn);
+			log.error(e.getMessage(), e);
+			throw new ServiceException("删除失败，原因为:"+e.getMessage(),e);
+		}finally{
+			DbUtils.commitAndCloseQuietly(sysConn);
+		}
+	}
 	
 	/* resultset handler */
 	class MultiRowHandler implements ResultSetHandler<List<SysMsg>>{
@@ -89,6 +171,8 @@ public class SysMsgService {
 				msg.setMsgContent(rs.getString("MSG_CONTENT"));
 				msg.setCreateTime(rs.getTimestamp("CREATE_TIME"));
 				msg.setTargetUserId(rs.getLong("TARGET_USER_ID"));
+				msg.setMsgTitle(rs.getString("MSG_TITLE"));
+				msg.setPushUserId(rs.getLong("PUSH_USER_ID"));
 				msgs.add(msg);
 			}
 			return msgs;
@@ -115,6 +199,8 @@ public class SysMsgService {
 				msg.setMsgContent(rs.getString("MSG_CONTENT"));
 				msg.setCreateTime(rs.getTimestamp("CREATE_TIME"));
 				msg.setTargetUserId(rs.getLong("TARGET_USER_ID"));
+				msg.setMsgTitle(rs.getString("MSG_TITLE"));
+				msg.setPushUserId(rs.getLong("PUSH_USER_ID"));
 				msgs.add(msg);
 				if(total==0){
 					total=rs.getInt("TOTAL_RECORD_NUM_");

6af1c5647d4d97c88974bfd883bb0885334a1715
zhangyuntao <zhangyuntao@navinfo.com>
2016-09-28 18:38:12 +0800

    同一POI添加检查

--- engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/obj/samepoi/create/Process.java
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/obj/samepoi/create/Process.java
@@ -6,37 +6,37 @@ import com.navinfo.dataservice.engine.edit.operation.AbstractCommand;
 import com.navinfo.dataservice.engine.edit.operation.AbstractProcess;
 
 /**
+ * @author zhangyt
  * @Title: Process.java
  * @Description: TODO
- * @author zhangyt
  * @date: 2016年8月29日 上午10:28:31
  * @version: v1.0
  */
 public class Process extends AbstractProcess<Command> {
 
-	private Check check = new Check();
+    private Check check = new Check();
 
-	public Process() {
-		super();
-	}
+    public Process() {
+        super();
+    }
 
-	public Process(AbstractCommand command) throws Exception {
-		super(command);
-	}
+    public Process(AbstractCommand command) throws Exception {
+        super(command);
+    }
 
-	@Override
-	public String preCheck() throws Exception {
-//		Command command = this.getCommand();
-//		IxPoiSelector selector = new IxPoiSelector(this.getConn());
-//		IxPoi poi = (IxPoi) selector.loadById(command.getPidArray().getInt(0), false, false);
-//		IxPoi otherPoi = (IxPoi) selector.loadById(command.getPidArray().getInt(1), false, false);
-//		check.checkKindOfPOI(poi, otherPoi);
-		return null;
-	}
+    @Override
+    public String preCheck() throws Exception {
+        Command command = this.getCommand();
+        IxPoiSelector selector = new IxPoiSelector(this.getConn());
+        IxPoi poi = (IxPoi) selector.loadById(command.getPidArray().getInt(0), false, false);
+        IxPoi otherPoi = (IxPoi) selector.loadById(command.getPidArray().getInt(1), false, false);
+        return check.checkKindOfPOI(poi, otherPoi);
+        //return null;
+    }
 
-	@Override
-	public String exeOperation() throws Exception {
-		return new Operation(this.getCommand()).run(this.getResult());
-	}
+    @Override
+    public String exeOperation() throws Exception {
+        return new Operation(this.getCommand()).run(this.getResult());
+    }
 
 }

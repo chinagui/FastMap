9099cd5c64edbd1ef714174ce30272ed3fb248eb
zhangxiaolong4943 <zhangxiaolong4943@navinfo.com>
2016-09-30 11:50:25 +0800

    修改多线立交维护层级关系

--- dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/selector/rd/gsc/RdGscSelector.java
+++ dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/selector/rd/gsc/RdGscSelector.java
@@ -279,7 +279,7 @@ public class RdGscSelector extends AbstractSelector {
 	 */
 	private void setChild(RdGsc rdGsc,boolean isLock) throws Exception
 	{
-		List<IRow> links = new RdGscLinkSelector(conn).loadRowsByParentId(rdGsc.getPid(), isLock);
+		List<IRow> links = new RdGscLinkSelector(conn).loadRowsByClassParentId(RdGscLink.class, rdGsc.getPid(), isLock, "shp_seq_num");
 
 		rdGsc.setLinks(links);
 

6e274943a36a6c52c73f006a6525d760d8bf75ae
zhaokaikai <zhaokaikai@navinfo.com>
2016-10-09 21:47:11 +0800

    bug修改

--- engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/topo/depart/updowndepartlink/Operation.java
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/topo/depart/updowndepartlink/Operation.java
@@ -158,7 +158,7 @@ public class Operation implements IOperation {
 		    	flagBooleans.add(CompPolylineUtil.isRightSide(JtsGeometryFactory.createLineString(currentLink.getGeometry().getCoordinates()), JtsGeometryFactory.createLineString(nextLink.getGeometry().getCoordinates()), JtsGeometryFactory.createLineString(link.getGeometry().getCoordinates())));
 		    }
 		   if (flagBooleans.size() >1 ){
-			   this.createInnerLine(lines[i],lines[lines.length-i],map,result);
+			   this.createInnerLine(lines[i],lines[lines.length-i-1],map,result);
 		   }
 		}
 		
@@ -177,9 +177,13 @@ public class Operation implements IOperation {
 		Coordinate[] coordinates = new Coordinate[2];
 		Coordinate sCoordinate = lineDownString.getCoordinates()[lineDownString.getCoordinates().length-1];
 		Coordinate eCoordinate = lineUpString.getCoordinates()[0];
+		coordinates[0]= sCoordinate;
+		coordinates[1]= eCoordinate;
 		RdNode sNode = map.get(JtsGeometryFactory.createPoint(sCoordinate));
 		RdNode eNode = map.get(JtsGeometryFactory.createPoint(eCoordinate));
 		RdLink innerLink =  new RdLink();
+		innerLink.setsNodePid(sNode.getPid());
+		innerLink.setsNodePid(eNode.getPid());
 		innerLink.setGeometry(JtsGeometryFactory.createLineString(coordinates));
 		RdLinkOperateUtils.addRdLink(sNode, eNode, innerLink,
 				innerLink, result);
@@ -213,7 +217,7 @@ public class Operation implements IOperation {
 					
 					targetLine = CompPolylineUtil.cut(lines[i], lines[i+1],JtsGeometryFactory.createLineString(GeoTranslator.transform(link.getGeometry(), 0.00001, 5).getCoordinates()),currentPoint,true );
 				}else{
-					targetLine =CompPolylineUtil.cut(lines[lines.length-i], lines[lines.length-i-1],JtsGeometryFactory.createLineString(GeoTranslator.transform(link.getGeometry(), 0.00001, 5).getCoordinates()),currentPoint,false );
+					targetLine =CompPolylineUtil.cut(lines[lines.length-i-1], lines[lines.length-i-2],JtsGeometryFactory.createLineString(GeoTranslator.transform(link.getGeometry(), 0.00001, 5).getCoordinates()),currentPoint,false );
 				}
 				if (targetLine.getCoordinate() != null){
 					

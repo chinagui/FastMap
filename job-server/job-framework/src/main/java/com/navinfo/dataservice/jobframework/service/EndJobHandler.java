d4c8a8a55891892c2016f85e6693f8a8cb1d7906
Xiaowen-Navinfo <xiaoxiaowen4127@navinfo.com>
2016-10-14 22:10:10 +0800

    gdbBatch,end_job handler

--- job-server/job-framework/src/main/java/com/navinfo/dataservice/jobframework/service/EndJobHandler.java
+++ job-server/job-framework/src/main/java/com/navinfo/dataservice/jobframework/service/EndJobHandler.java
@@ -10,6 +10,7 @@ import com.navinfo.dataservice.api.job.model.JobInfo;
 import com.navinfo.dataservice.commons.database.MultiDataSourceFactory;
 import com.navinfo.dataservice.commons.log.LoggerRepos;
 import com.navinfo.dataservice.dao.mq.MsgHandler;
+import com.navinfo.dataservice.dao.mq.sys.SysMsgPublisher;
 import com.navinfo.navicommons.database.QueryRunner;
 
 import net.sf.json.JSONException;
@@ -28,6 +29,25 @@ public class EndJobHandler implements MsgHandler {
 	 */
 	@Override
 	public void handle(String message) {
+		sendMsg(message);
+	}
+	
+	public void sendMsg(String message){
+		try{
+			log.debug("Handle end job message:"+message);
+			JSONObject jobMsg = JSONObject.fromObject(message);
+			long userId = jobMsg.getLong("userId");
+			long jobId = jobMsg.getLong("jobId");
+			int status = jobMsg.getInt("status");
+			String resultMsg = jobMsg.getString("resultMsg");
+			JSONObject resp = jobMsg.getJSONObject("response");
+			SysMsgPublisher.publishMsg("job("+jobId+")执行完成", resultMsg, 0, new long[]{userId});
+		}catch(Exception e){
+			log.warn("接收到end_job消息,但处理过程中出错，消息已消费。message："+message);
+			log.error(e.getMessage(),e);
+		}
+	}
+	public void persMsg(String message){
 		Connection conn = null;
 		try{
 			//持久化

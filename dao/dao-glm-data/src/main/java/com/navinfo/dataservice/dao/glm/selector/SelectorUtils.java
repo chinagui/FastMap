bd0a2bf843b6e8633426c44e55134086447bf953
zhangxiaolong4943 <zhangxiaolong4943@navinfo.com>
2016-10-17 15:08:21 +0800

    优化道路名搜索查询效率

--- dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/selector/SelectorUtils.java
+++ dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/selector/SelectorUtils.java
@@ -27,18 +27,19 @@ public class SelectorUtils {
 		int total = 0;
 		int startRow = (pageNum - 1) * pageSize + 1;
 		int endRow = pageNum * pageSize;
-		StringBuilder buffer = new StringBuilder();
+		String sql = "";
 		StringBuilder bufferCondition = new StringBuilder();
 		if (tableName.equals(ObjType.RDLINK.toString())) {
 			if (object.containsKey("name")) {
-				bufferCondition.append(
-						" select  /*+ leading(iln,rn) use_hash(iln,rn)*/  COUNT (1) OVER (PARTITION BY 1) total,rln.link_pid pid,rn.name from rd_link_name rln,rd_name rn where rln.name_class=1 and rn.lang_code = 'CHI' and   rn.name_groupid = rln.name_groupId");
-				bufferCondition.append(" and rn.name like '%" + object.getString("name") + "%' ");
+				bufferCondition.append("with tmp1 as ( select lang_code,name_groupid,name from rd_name where name like '%路%' ) ,tmp2 AS (	SELECT /*+ leading(iln,tmp1) use_hash(iln,tmp1)*/ rln.link_pid pid,tmp1.name FROM rd_link_name rln,tmp1 WHERE rln.name_class=1 AND tmp1.name_groupid = rln.name_groupId ) ,tmp3 AS (	SELECT count(*) over () total,tmp2.*, ROWNUM rn FROM tmp2 ) ,tmp4 as ( select * from tmp3 where ROWNUM <=:1 ) SELECT * FROM tmp4 WHERE rn >=:2");
+				sql = bufferCondition.toString();
 			} else {
 				bufferCondition
 						.append("SELECT  COUNT(1) OVER(PARTITION BY 1) TOTAL, tmp.pid,rn.name FROM( SELECT /*index(tmpLink)*/ tmpLink.LINK_PID PID ,rln.name_groupid  FROM rd_link tmpLink LEFT JOIN RD_LINK_NAME RLN ON tmpLink.Link_Pid = rln.link_pid AND RLN.NAME_CLASS = 1 where tmpLink.LINK_PID = "
 								+ object.getString("linkPid")
 								+ " GROUP BY tmpLink.LINK_PID,rln.name_groupid )tmp LEFT JOIN RD_NAME rn ON tmp.name_groupid = rn.name_groupid AND  RN.LANG_CODE = 'CHI'");
+				
+				sql = getSqlFromBufferCondition(bufferCondition,isLock);
 			}
 		}
 		if (tableName.equals(ObjType.IXPOI.toString())) {
@@ -46,28 +47,21 @@ public class SelectorUtils {
 				bufferCondition.append(
 						" select COUNT (1) OVER (PARTITION BY 1) total,ipn.poi_pid pid,ipn.name from ix_poi_name ipn where ipn.name_class=1 and ipn.name_type =2 and ipn.lang_code = 'CHI' ");
 				bufferCondition.append(" and ipn.name like '%" + object.getString("name") + "%' ");
+				sql = getSqlFromBufferCondition(bufferCondition,isLock);
 			} else {
 				bufferCondition
 						.append("SELECT COUNT (1) OVER (PARTITION BY 1) total,tmp.pid,tmp.name from( select ix.pid,ipn.name from ix_poi ix left join ix_poi_name ipn on ix.pid = ipn.poi_pid and ipn.name_class=1 AND ipn.name_type =2 AND ipn.lang_code = 'CHI' where ix.pid = "
 								+ object.getString("pid") + " )tmp");
+				sql = getSqlFromBufferCondition(bufferCondition,isLock);
+				
 			}
 		}
-
-		buffer.append(" SELECT * ");
-		buffer.append(" FROM (SELECT c.*, ROWNUM rn ");
-		buffer.append(" FROM ( " + bufferCondition.toString() + "");
-
-		buffer.append(" ) c");
-		buffer.append(" WHERE ROWNUM <= :1) ");
-		buffer.append("  WHERE rn >= :2 ");
-		if (isLock) {
-			buffer.append(" for update nowait");
-		}
+		
 		PreparedStatement pstmt = null;
 
 		ResultSet resultSet = null;
 		try {
-			pstmt = conn.prepareStatement(buffer.toString());
+			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, endRow);
 			pstmt.setInt(2, startRow);
 			resultSet = pstmt.executeQuery();
@@ -99,7 +93,23 @@ public class SelectorUtils {
 
 		}
 	}
+	
+	private String getSqlFromBufferCondition(StringBuilder bufferCondition,boolean isLock)
+	{
+		StringBuilder buffer = new StringBuilder();
+		buffer.append(" SELECT * ");
+		buffer.append(" FROM (SELECT c.*, ROWNUM rn ");
+		buffer.append(" FROM ( " + bufferCondition.toString() + "");
 
+		buffer.append(" ) c");
+		buffer.append(" WHERE ROWNUM <= :1) ");
+		buffer.append("  WHERE rn >= :2 ");
+		if (isLock) {
+			buffer.append(" for update nowait");
+		}
+		
+		return buffer.toString();
+	}
 	/**
 	 * 获取顶级主表表名（履历中的obNm），针对对象冲突的特殊处理
 	 * 

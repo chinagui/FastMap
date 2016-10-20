015a57587068e2d43aa0a68c2f4e1130ce97bdd7
zhangyuntao <zhangyuntao@navinfo.com>
2016-10-20 16:07:25 +0800

    上下线分离对RTIC、警示信息维护

--- engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/topo/depart/updowndepartlink/OpRefRelationObj.java
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/topo/depart/updowndepartlink/OpRefRelationObj.java
@@ -11,233 +11,233 @@ import com.navinfo.dataservice.engine.edit.operation.obj.rdcross.depart.Operatio
 
 public class OpRefRelationObj {
 
-	private Connection conn = null;
+    private Connection conn = null;
 
-	public OpRefRelationObj(Connection conn) {
-		this.conn = conn;
-	}
+    public OpRefRelationObj(Connection conn) {
+        this.conn = conn;
+    }
 
-	// 同一线
-	public String handleSameLink(Command command, Result result)
-			throws Exception {
-		com.navinfo.dataservice.engine.edit.operation.obj.rdsamelink.delete.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdsamelink.delete.Operation(
-				conn);
-		operation.deleteByUpDownPartLink(command.getsNode().getPid(), command
-				.geteNode().getPid(), command.getLinks(), result);
-		return "";
-	}
+    // 同一线
+    public String handleSameLink(Command command, Result result)
+            throws Exception {
+        com.navinfo.dataservice.engine.edit.operation.obj.rdsamelink.delete.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdsamelink.delete.Operation(
+                conn);
+        operation.deleteByUpDownPartLink(command.getsNode().getPid(), command
+                .geteNode().getPid(), command.getLinks(), result);
+        return "";
+    }
 
-	// 同一点
-	public String handleSameNode(Command command, Result result)
-			throws Exception {
-		com.navinfo.dataservice.engine.edit.operation.obj.rdsamenode.delete.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdsamenode.delete.Operation(
-				conn);
-		operation.deleteByUpDownPartLink(command.getsNode().getPid(), command
-				.geteNode().getPid(), command.getLinks(), result);
-		return "";
-	}
+    // 同一点
+    public String handleSameNode(Command command, Result result)
+            throws Exception {
+        com.navinfo.dataservice.engine.edit.operation.obj.rdsamenode.delete.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdsamenode.delete.Operation(
+                conn);
+        operation.deleteByUpDownPartLink(command.getsNode().getPid(), command
+                .geteNode().getPid(), command.getLinks(), result);
+        return "";
+    }
 
-	// 车信
-	public String handleLaneConnexity(Command command, Result result)
-			throws Exception {
+    // 车信
+    public String handleLaneConnexity(Command command, Result result)
+            throws Exception {
 
-		int preNodePid = command.getsNode().getPid();
+        int preNodePid = command.getsNode().getPid();
 
-		int lastNodePid = command.geteNode().getPid();
+        int lastNodePid = command.geteNode().getPid();
 
-		com.navinfo.dataservice.engine.edit.operation.obj.rdlaneconnexity.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdlaneconnexity.depart.Operation(
-				conn, preNodePid, lastNodePid, command.getNoTargetLinks(),
-				command.getLinks());
+        com.navinfo.dataservice.engine.edit.operation.obj.rdlaneconnexity.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdlaneconnexity.depart.Operation(
+                conn, preNodePid, lastNodePid, command.getNoTargetLinks(),
+                command.getLinks());
 
-		operation.upDownPart(command.getLeftLinkMapping(),
-				command.getRightLinkMapping(), result);
-		return "";
-	}
+        operation.upDownPart(command.getLeftLinkMapping(),
+                command.getRightLinkMapping(), result);
+        return "";
+    }
 
-	// 交限
-	public String handleRestriction(Command command, Result result)
-			throws Exception {
+    // 交限
+    public String handleRestriction(Command command, Result result)
+            throws Exception {
 
-		int preNodePid = command.getsNode().getPid();
+        int preNodePid = command.getsNode().getPid();
 
-		int lastNodePid = command.geteNode().getPid();
+        int lastNodePid = command.geteNode().getPid();
 
-		com.navinfo.dataservice.engine.edit.operation.obj.rdrestriction.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdrestriction.depart.Operation(
-				conn, preNodePid, lastNodePid, command.getNoTargetLinks(),
-				command.getLinks());
+        com.navinfo.dataservice.engine.edit.operation.obj.rdrestriction.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdrestriction.depart.Operation(
+                conn, preNodePid, lastNodePid, command.getNoTargetLinks(),
+                command.getLinks());
 
-		operation.upDownPart(command.getLeftLinkMapping(),
-				command.getRightLinkMapping(), result);
+        operation.upDownPart(command.getLeftLinkMapping(),
+                command.getRightLinkMapping(), result);
 
-		return "";
-	}
+        return "";
+    }
 
-	// 语音引导
-	public String handleVoiceguide(Command command, Result result)
-			throws Exception {
+    // 语音引导
+    public String handleVoiceguide(Command command, Result result)
+            throws Exception {
 
-		int preNodePid = command.getsNode().getPid();
+        int preNodePid = command.getsNode().getPid();
 
-		int lastNodePid = command.geteNode().getPid();
+        int lastNodePid = command.geteNode().getPid();
 
-		com.navinfo.dataservice.engine.edit.operation.obj.rdvoiceguide.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdvoiceguide.depart.Operation(
-				conn, preNodePid, lastNodePid, command.getNoTargetLinks(),
-				command.getLinks());
+        com.navinfo.dataservice.engine.edit.operation.obj.rdvoiceguide.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdvoiceguide.depart.Operation(
+                conn, preNodePid, lastNodePid, command.getNoTargetLinks(),
+                command.getLinks());
 
-		operation.upDownPart(command.getLeftLinkMapping(),
-				command.getRightLinkMapping(), result);
+        operation.upDownPart(command.getLeftLinkMapping(),
+                command.getRightLinkMapping(), result);
 
-		return "";
-	}
+        return "";
+    }
 
-	// 分歧
-	public String handleBranch(Command command, Result result)
-			throws Exception {
+    // 分歧
+    public String handleBranch(Command command, Result result)
+            throws Exception {
 
-		int preNodePid = command.getsNode().getPid();
+        int preNodePid = command.getsNode().getPid();
 
-		int lastNodePid = command.geteNode().getPid();
+        int lastNodePid = command.geteNode().getPid();
 
-		com.navinfo.dataservice.engine.edit.operation.obj.rdbranch.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdbranch.depart.Operation(
-				conn, preNodePid, lastNodePid, command.getNoTargetLinks(),
-				command.getLinks());
+        com.navinfo.dataservice.engine.edit.operation.obj.rdbranch.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdbranch.depart.Operation(
+                conn, preNodePid, lastNodePid, command.getNoTargetLinks(),
+                command.getLinks());
 
-		operation.upDownPart(command.getLeftLinkMapping(),
-				command.getRightLinkMapping(), result);
+        operation.upDownPart(command.getLeftLinkMapping(),
+                command.getRightLinkMapping(), result);
 
-		return "";
-	}
+        return "";
+    }
 
-	// 顺行
-	public String handleDirectroute(Command command, Result result)
-			throws Exception {
+    // 顺行
+    public String handleDirectroute(Command command, Result result)
+            throws Exception {
 
-		int preNodePid = command.getsNode().getPid();
+        int preNodePid = command.getsNode().getPid();
 
-		int lastNodePid = command.geteNode().getPid();
+        int lastNodePid = command.geteNode().getPid();
 
-		com.navinfo.dataservice.engine.edit.operation.obj.rddirectroute.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rddirectroute.depart.Operation(
-				conn, preNodePid, lastNodePid, command.getNoTargetLinks(),
-				command.getLinks());
+        com.navinfo.dataservice.engine.edit.operation.obj.rddirectroute.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rddirectroute.depart.Operation(
+                conn, preNodePid, lastNodePid, command.getNoTargetLinks(),
+                command.getLinks());
 
-		operation.upDownPart(command.getLeftLinkMapping(),
-				command.getRightLinkMapping(), result);
+        operation.upDownPart(command.getLeftLinkMapping(),
+                command.getRightLinkMapping(), result);
 
-		return "";
-	}
+        return "";
+    }
 
-	// 警示信息
-	public String handlerdWarninginfo(Command command, Result result)
-			throws Exception {
+    // 警示信息
+    public String handlerdWarninginfo(Command command, Result result)
+            throws Exception {
 
-		// com.navinfo.dataservice.engine.edit.operation.obj.rdwarninginfo.delete.Operation
-		// warninginfoOperation = new
-		// com.navinfo.dataservice.engine.edit.operation.obj.rdwarninginfo.delete.Operation(conn);
-		// warninginfoOperation.batchDeleteByLink(command.getLinks(), result);
-		// return null;
-		// 维护警示信息
-		com.navinfo.dataservice.engine.edit.operation.obj.rdwarninginfo.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdwarninginfo.depart.Operation(
-				conn);
-		return operation.updownDepart(command.getLinks(),
-				command.getLeftLinkMapping(), command.getRightLinkMapping(),
-				result);
-	}
+        // com.navinfo.dataservice.engine.edit.operation.obj.rdwarninginfo.delete.Operation
+        // warninginfoOperation = new
+        // com.navinfo.dataservice.engine.edit.operation.obj.rdwarninginfo.delete.Operation(conn);
+        // warninginfoOperation.batchDeleteByLink(command.getLinks(), result);
+        // return null;
+        // 维护警示信息
+        com.navinfo.dataservice.engine.edit.operation.obj.rdwarninginfo.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdwarninginfo.depart.Operation(
+                conn);
+        return operation.updownDepart(command.getLinks(),
+                command.getLeftLinkMapping(), command.getRightLinkMapping(), command.getNoTargetLinks(),
+                result);
+    }
 
-	// 点限速
-	public String handlerdSpeedlimit(Command command, Result result)
-			throws Exception {
-		// com.navinfo.dataservice.engine.edit.operation.obj.rdspeedlimit.update.Operation
-		// warninginfoOperation = new
-		// com.navinfo.dataservice.engine.edit.operation.obj.rdspeedlimit.update.Operation(conn);
-		// warninginfoOperation.upDownLink(command.getsNode(),
-		// command.getLinks(), command.getLeftLinkMapping(),
-		// command.getRightLinkMapping(), result);
-		// return null;
-		// 维护点限速
-		com.navinfo.dataservice.engine.edit.operation.obj.rdspeedlimit.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdspeedlimit.depart.Operation(
-				conn);
-		return operation.updownDepart(command.getsNode(), command.getLinks(),
-				command.getLeftLinkMapping(), command.getRightLinkMapping(),
-				result);
-	}
+    // 点限速
+    public String handlerdSpeedlimit(Command command, Result result)
+            throws Exception {
+        // com.navinfo.dataservice.engine.edit.operation.obj.rdspeedlimit.update.Operation
+        // warninginfoOperation = new
+        // com.navinfo.dataservice.engine.edit.operation.obj.rdspeedlimit.update.Operation(conn);
+        // warninginfoOperation.upDownLink(command.getsNode(),
+        // command.getLinks(), command.getLeftLinkMapping(),
+        // command.getRightLinkMapping(), result);
+        // return null;
+        // 维护点限速
+        com.navinfo.dataservice.engine.edit.operation.obj.rdspeedlimit.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdspeedlimit.depart.Operation(
+                conn);
+        return operation.updownDepart(command.getsNode(), command.getLinks(),
+                command.getLeftLinkMapping(), command.getRightLinkMapping(),
+                result);
+    }
 
-	// 维护电子眼信息
-	public String handlerdRdElectroniceye(Command command, Result result)
-			throws Exception {
-		com.navinfo.dataservice.engine.edit.operation.obj.rdeleceye.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdeleceye.depart.Operation(
-				conn);
-		return operation.updownDepart(command.getsNode(), command.getLinks(),
-				command.getLeftLinkMapping(), command.getRightLinkMapping(),
-				result);
-	}
+    // 维护电子眼信息
+    public String handlerdRdElectroniceye(Command command, Result result)
+            throws Exception {
+        com.navinfo.dataservice.engine.edit.operation.obj.rdeleceye.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdeleceye.depart.Operation(
+                conn);
+        return operation.updownDepart(command.getsNode(), command.getLinks(),
+                command.getLeftLinkMapping(), command.getRightLinkMapping(),
+                result);
+    }
 
-	// 维护RTIC信息
-	public String handlerRdLinkRtic(Command command, Result result)
-			throws Exception {
-		com.navinfo.dataservice.engine.edit.operation.obj.rdlink.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdlink.depart.Operation(
-				conn);
-		return operation.updownDepart(command.getLinks(),
-				command.getLeftLinkMapping(), command.getRightLinkMapping(),
-				result);
-	}
+    // 维护RTIC信息
+    public String handlerRdLinkRtic(Command command, Result result)
+            throws Exception {
+        com.navinfo.dataservice.engine.edit.operation.obj.rdlink.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdlink.depart.Operation(
+                conn);
+        return operation.updownDepart(command.getLinks(),
+                command.getLeftLinkMapping(), command.getRightLinkMapping(),
+                result);
+    }
 
-	// 维护IxPoi信息
-	public String handlerIxPoi(Command command, Result result) throws Exception {
-		com.navinfo.dataservice.engine.edit.operation.obj.poi.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.poi.depart.Operation(
-				conn);
-		return operation.updownDepart(command.getLinks(),
-				command.getLeftLinkMapping(), command.getRightLinkMapping(),
-				result);
-	}
+    // 维护IxPoi信息
+    public String handlerIxPoi(Command command, Result result) throws Exception {
+        com.navinfo.dataservice.engine.edit.operation.obj.poi.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.poi.depart.Operation(
+                conn);
+        return operation.updownDepart(command.getLinks(),
+                command.getLeftLinkMapping(), command.getRightLinkMapping(),
+                result);
+    }
 
-	// 维护减速带信息
-	public String handlerRdSpeedbump(Command command, Result result)
-			throws Exception {
-		com.navinfo.dataservice.engine.edit.operation.obj.rdspeedbump.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdspeedbump.depart.Operation(
-				conn);
-		return operation.updownDepart(command.getLinks(),
-				command.getLeftLinkMapping(), command.getRightLinkMapping(),
-				result);
-	}
+    // 维护减速带信息
+    public String handlerRdSpeedbump(Command command, Result result)
+            throws Exception {
+        com.navinfo.dataservice.engine.edit.operation.obj.rdspeedbump.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdspeedbump.depart.Operation(
+                conn);
+        return operation.updownDepart(command.getLinks(),
+                command.getLeftLinkMapping(), command.getRightLinkMapping(),
+                result);
+    }
 
-	// 维护坡度信息
-	public String handlerRdSlope(Command command, Result result)
-			throws Exception {
-		com.navinfo.dataservice.engine.edit.operation.obj.rdslope.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdslope.depart.Operation(
-				conn);
-		return operation.updownDepart(command.getLinks(),
-				command.getLeftLinkMapping(), command.getRightLinkMapping(),
-				result);
-	}
+    // 维护坡度信息
+    public String handlerRdSlope(Command command, Result result)
+            throws Exception {
+        com.navinfo.dataservice.engine.edit.operation.obj.rdslope.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdslope.depart.Operation(
+                conn);
+        return operation.updownDepart(command.getLinks(),
+                command.getLeftLinkMapping(), command.getRightLinkMapping(),
+                result);
+    }
 
-	// 维护可变限速
-	public String handlerRdVariableSpeed(Command command, Result result)
-			throws Exception {
-		com.navinfo.dataservice.engine.edit.operation.obj.rdvariablespeed.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdvariablespeed.depart.Operation(
-				conn);
-		return operation.updownDepart(command.getLinks(),
-				command.getLeftLinkMapping(), command.getRightLinkMapping(),
-				result);
-	}
+    // 维护可变限速
+    public String handlerRdVariableSpeed(Command command, Result result)
+            throws Exception {
+        com.navinfo.dataservice.engine.edit.operation.obj.rdvariablespeed.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdvariablespeed.depart.Operation(
+                conn);
+        return operation.updownDepart(command.getLinks(),
+                command.getLeftLinkMapping(), command.getRightLinkMapping(),
+                result);
+    }
 
-	// 维护信号灯
-	public String handlerRdTrafficsignal(Command command, Result result)
-			throws Exception {
-		com.navinfo.dataservice.engine.edit.operation.obj.trafficsignal.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.trafficsignal.depart.Operation(
-				conn);
-		return operation.updownDepart(command.getLinks(),
-				command.getLeftLinkMapping(), command.getRightLinkMapping(),
-				result);
-	}
+    // 维护信号灯
+    public String handlerRdTrafficsignal(Command command, Result result)
+            throws Exception {
+        com.navinfo.dataservice.engine.edit.operation.obj.trafficsignal.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.trafficsignal.depart.Operation(
+                conn);
+        return operation.updownDepart(command.getLinks(),
+                command.getLeftLinkMapping(), command.getRightLinkMapping(),
+                result);
+    }
 
-	// 维护路口
-	public String handlerRdCross(Command command, Result result)
-			throws Exception {
-		com.navinfo.dataservice.engine.edit.operation.obj.rdcross.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdcross.depart.Operation(
-				conn);
-		return operation.updownDepart(command.getLinks(),
-				command.getLeftLinkMapping(), command.getRightLinkMapping(),
-				result);
-	}
+    // 维护路口
+    public String handlerRdCross(Command command, Result result)
+            throws Exception {
+        com.navinfo.dataservice.engine.edit.operation.obj.rdcross.depart.Operation operation = new com.navinfo.dataservice.engine.edit.operation.obj.rdcross.depart.Operation(
+                conn);
+        return operation.updownDepart(command.getLinks(),
+                command.getLeftLinkMapping(), command.getRightLinkMapping(),
+                result);
+    }
 
 }

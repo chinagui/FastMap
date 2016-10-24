d11cbb7769cea7be440516406844a70d80be3bbe
zhangli5174 <zhangli5174@navinfo.com>
2016-10-24 10:53:11 +0800

    提交 group 脚本代码

--- engine/engine-statics/src/main/java/com/navinfo/dataservice/engine/statics/overview/OverviewBlockStat.java
+++ engine/engine-statics/src/main/java/com/navinfo/dataservice/engine/statics/overview/OverviewBlockStat.java
@@ -8,31 +8,19 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.bson.Document;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import com.mongodb.BasicDBObject;
-import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Sorts;
 import com.navinfo.dataservice.api.man.iface.ManApi;
-import com.navinfo.dataservice.api.man.model.Subtask;
 import com.navinfo.dataservice.commons.springmvc.ApplicationContextUtil;
-import com.navinfo.dataservice.engine.statics.StatMain;
 import com.navinfo.dataservice.engine.statics.poicollect.PoiCollectMain;
-import com.navinfo.dataservice.engine.statics.poidaily.PoiDailyMain;
-import com.navinfo.dataservice.engine.statics.roadcollect.RoadCollectMain;
-import com.navinfo.dataservice.engine.statics.roaddaily.RoadDailyMain;
 import com.navinfo.dataservice.engine.statics.tools.MongoDao;
 import com.navinfo.dataservice.engine.statics.tools.OracleDao;
 import com.navinfo.dataservice.engine.statics.tools.StatUtil;
-
-import net.sf.json.JSONObject;
 
 /** 
  * @ClassName: OverviewBlockStat
@@ -276,7 +264,7 @@ public class OverviewBlockStat {
 				new String[] { "dubbo-consumer-datahub-test.xml"});
 		context.start();
 		new ApplicationContextUtil().setApplicationContext(context);
-		OverviewBlockStat overviewSubtaskStat = new OverviewBlockStat("fm_stat", "201610221933");
+		OverviewBlockStat overviewSubtaskStat = new OverviewBlockStat("fm_stat", "201610240956");
 		overviewSubtaskStat.runStat();
 		
 	}

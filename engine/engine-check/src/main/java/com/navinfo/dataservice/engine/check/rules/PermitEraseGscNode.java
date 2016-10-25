d59c49777a0a9323db4b5d765aeb280a97927fb0
xiaolvluo <zhangxiaoyi@navinfo.com>
2016-10-25 14:08:40 +0800

    ProduceService,PermitEraseGscNode

--- engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/PermitEraseGscNode.java
+++ engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/PermitEraseGscNode.java
@@ -3,6 +3,9 @@ package com.navinfo.dataservice.engine.check.rules;
 import java.util.List;
 import java.util.Map;
 
+import net.sf.json.JSONObject;
+
+import com.navinfo.dataservice.commons.geom.GeoTranslator;
 import com.navinfo.dataservice.dao.check.CheckCommand;
 import com.navinfo.dataservice.dao.glm.iface.IRow;
 import com.navinfo.dataservice.dao.glm.model.rd.gsc.RdGsc;
@@ -11,6 +14,7 @@ import com.navinfo.dataservice.dao.glm.selector.rd.gsc.RdGscSelector;
 import com.navinfo.dataservice.dao.glm.selector.rd.link.RdLinkSelector;
 import com.navinfo.dataservice.engine.check.core.baseRule;
 import com.navinfo.dataservice.engine.check.helper.GeoHelper;
+import com.vividsolutions.jts.awt.PointShapeFactory.X;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.LineString;
@@ -37,15 +41,8 @@ public class PermitEraseGscNode extends baseRule {
 				Map<String, Object> changedFields = rdLink.changedFields();
 				if(changedFields==null || !changedFields.containsKey("geometry")){continue;}
 				
-				int sPid=rdLink.getsNodePid();
-				int ePid=rdLink.geteNodePid();
-				LineString rdLine=(LineString) rdLink.getGeometry();
-				Point rdS=rdLine.getStartPoint();
-				Point rdE=rdLine.getEndPoint();
-				Geometry geo=rdLink.getGeometry();
 				//link上是否有立交
 				RdGscSelector gscSelector=new RdGscSelector(getConn());
-				boolean isError=false;	
 				String sqltmp="SELECT G.*"
 						+ "  FROM RD_GSC_LINK L1, RD_GSC G"
 						+ " WHERE L1.TABLE_NAME = 'RD_LINK' AND L1.U_RECORD != 2 "
@@ -53,9 +50,11 @@ public class PermitEraseGscNode extends baseRule {
 						+ "   AND L1.LINK_PID = "+rdLink.getPid()
 						+ "   AND L1.PID = G.PID";
 				List<RdGsc> gscList=gscSelector.loadBySql(sqltmp, false);
-				if(gscList.size()==0){isError=true;break;}
-				//立交点是否还在link上
-				Coordinate[] coords = geo.getCoordinates();	
+				if(gscList.size()==0){break;}
+				JSONObject geojson=(JSONObject) changedFields.get("geometry");
+				Geometry geoNew=GeoTranslator.geojson2Jts(geojson,100000, 0);
+				Coordinate[] coords = geoNew.getCoordinates();
+				//立交点是否还在link上	
 				for(int i=0;i<gscList.size();i++){
 					boolean hasPoints=false;
 					Geometry gscPoint=gscList.get(i).getGeometry();

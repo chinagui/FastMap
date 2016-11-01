1c577d40fa467ed9eac1123a57e891fef0a9cdfb
zhangxiaolong4943 <zhangxiaolong4943@navinfo.com>
2016-11-01 11:06:26 +0800

    临时注释调车信检查

--- engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/RdLane003.java
+++ engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/RdLane003.java
@@ -2,17 +2,13 @@ package com.navinfo.dataservice.engine.check.rules;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import com.navinfo.dataservice.dao.check.CheckCommand;
 import com.navinfo.dataservice.dao.glm.iface.IRow;
 import com.navinfo.dataservice.dao.glm.model.rd.cross.RdCross;
-import com.navinfo.dataservice.dao.glm.model.rd.lane.RdLaneTopoVia;
-import com.navinfo.dataservice.dao.glm.model.rd.laneconnexity.RdLaneConnexity;
 import com.navinfo.dataservice.dao.glm.model.rd.laneconnexity.RdLaneTopology;
 import com.navinfo.dataservice.dao.glm.model.rd.laneconnexity.RdLaneVia;
 import com.navinfo.dataservice.dao.glm.model.rd.link.RdLink;
-import com.navinfo.dataservice.dao.glm.selector.rd.cross.RdCrossSelector;
 import com.navinfo.dataservice.dao.glm.selector.rd.link.RdLinkSelector;
 import com.navinfo.dataservice.engine.check.core.baseRule;
 import com.navinfo.dataservice.engine.check.graph.TwoNodeConnected;
@@ -32,73 +28,73 @@ public class RdLane003 extends baseRule {
 
 	@Override
 	public void preCheck(CheckCommand checkCommand) throws Exception {
-		for(IRow obj : checkCommand.getGlmList()){
-			//create的时候只有主表对象，其中包含的内容涵盖子表内容，可直接用
-			if (obj instanceof RdLaneConnexity){//交限
-				RdLaneConnexity laneObj=(RdLaneConnexity) obj;
-				Map<String, Object> changedFields=laneObj.changedFields();
-				//新增执行该检查
-				if(changedFields!=null && changedFields.size()>0){continue;}
-				RdLinkSelector rdLinkSelector=new RdLinkSelector(this.getConn());
-				RdLink inLink = (RdLink) rdLinkSelector.loadByIdOnlyRdLink(laneObj.getInLinkPid(), false);
-				//进入link非单向
-				//RdCrossSelector crossSelector = new RdCrossSelector(this.getConn());
-				//RdCross  inCross  = crossSelector.loadCrossByNodePid(laneObj.getNodePid(), false);
-				/*
-				 * 11.进入Link为单方向且通行方向不能离开路口；
-				 * 12.退出Link为单方向且通行方向不能进入路口；
-				 * 需求现在还有出入 先不做检查判断 等需求明确再加上
-				 * */
-				
-				//进入link的退出点
-				int inLinkExitNode=0;
-				if(inLink.getDirect()==2){inLinkExitNode=inLink.geteNodePid();}
-				if(inLink.getDirect()==3){inLinkExitNode=inLink.getsNodePid();}
-				if(inLinkExitNode!=laneObj.getNodePid()){
-					this.setCheckResult("", "", 0,"进入Link通行方向应与车信通行方向一致");
-					return;
-				}
-				/*if((inLink.getDirect() == 2 || inLink.getDirect()==3) && inCross.getPid() == 0){
-					this.setCheckResult("", "", 0,"如果进入Link为单方向且通行方向不能离开路口");
-					return;
-				}*/
-				for(IRow topo:laneObj.getTopos()){
-					RdLaneTopology topoObj=(RdLaneTopology) topo;
-					//退出link
-					RdLink outLink = (RdLink) rdLinkSelector.loadByIdOnlyRdLink(topoObj.getOutLinkPid(), false);
-					
-					//退出link的进入点
-					int outLinkEnterNode=0;
-					//int outLinkOutNode = 0;
-					if(outLink.getDirect()==2){
-						outLinkEnterNode=outLink.getsNodePid();
-						//outLinkOutNode = outLink.geteNodePid();
-					}
-					if(outLink.getDirect()==3){
-						outLinkEnterNode=outLink.geteNodePid();
-						//outLinkOutNode = outLink.getsNodePid();
-						}
-					
-					
-					//退出link非单向
-					//RdCross outCross  = crossSelector.loadCrossByNodePid(outLinkOutNode, false);
-					/*if((outLink.getDirect() ==2 || outLink.getDirect() ==3)&& outCross.getPid() == 0){
-						this.setCheckResult("", "", 0,"退出Link为单方向且通行方向不能进入路口");
-						return;
-					}*/
-					boolean isRight=false;
-					//线线关系
-					if(topoObj.getRelationshipType()==2){
-						if(this.isValidLineLane(topoObj,inLinkExitNode,outLinkEnterNode)){isRight=true;}
-					}else if(topoObj.getRelationshipType()==1){
-						if(this.isValidCrossLane(inLinkExitNode,outLinkEnterNode)){isRight=true;}
-					}
-					if(!isRight){
-						this.setCheckResult("", "", 0,"退出Link通行方向应与车信通行方向一致");
-						return;
-					}
-				}
-			}}
+//		for(IRow obj : checkCommand.getGlmList()){
+//			//create的时候只有主表对象，其中包含的内容涵盖子表内容，可直接用
+//			if (obj instanceof RdLaneConnexity){//交限
+//				RdLaneConnexity laneObj=(RdLaneConnexity) obj;
+//				Map<String, Object> changedFields=laneObj.changedFields();
+//				//新增执行该检查
+//				if(changedFields!=null && changedFields.size()>0){continue;}
+//				RdLinkSelector rdLinkSelector=new RdLinkSelector(this.getConn());
+//				RdLink inLink = (RdLink) rdLinkSelector.loadByIdOnlyRdLink(laneObj.getInLinkPid(), false);
+//				//进入link非单向
+//				//RdCrossSelector crossSelector = new RdCrossSelector(this.getConn());
+//				//RdCross  inCross  = crossSelector.loadCrossByNodePid(laneObj.getNodePid(), false);
+//				/*
+//				 * 11.进入Link为单方向且通行方向不能离开路口；
+//				 * 12.退出Link为单方向且通行方向不能进入路口；
+//				 * 需求现在还有出入 先不做检查判断 等需求明确再加上
+//				 * */
+//				
+//				//进入link的退出点
+//				int inLinkExitNode=0;
+//				if(inLink.getDirect()==2){inLinkExitNode=inLink.geteNodePid();}
+//				if(inLink.getDirect()==3){inLinkExitNode=inLink.getsNodePid();}
+//				if(inLinkExitNode!=laneObj.getNodePid()){
+//					this.setCheckResult("", "", 0,"进入Link通行方向应与车信通行方向一致");
+//					return;
+//				}
+//				/*if((inLink.getDirect() == 2 || inLink.getDirect()==3) && inCross.getPid() == 0){
+//					this.setCheckResult("", "", 0,"如果进入Link为单方向且通行方向不能离开路口");
+//					return;
+//				}*/
+//				for(IRow topo:laneObj.getTopos()){
+//					RdLaneTopology topoObj=(RdLaneTopology) topo;
+//					//退出link
+//					RdLink outLink = (RdLink) rdLinkSelector.loadByIdOnlyRdLink(topoObj.getOutLinkPid(), false);
+//					
+//					//退出link的进入点
+//					int outLinkEnterNode=0;
+//					//int outLinkOutNode = 0;
+//					if(outLink.getDirect()==2){
+//						outLinkEnterNode=outLink.getsNodePid();
+//						//outLinkOutNode = outLink.geteNodePid();
+//					}
+//					if(outLink.getDirect()==3){
+//						outLinkEnterNode=outLink.geteNodePid();
+//						//outLinkOutNode = outLink.getsNodePid();
+//						}
+//					
+//					
+//					//退出link非单向
+//					//RdCross outCross  = crossSelector.loadCrossByNodePid(outLinkOutNode, false);
+//					/*if((outLink.getDirect() ==2 || outLink.getDirect() ==3)&& outCross.getPid() == 0){
+//						this.setCheckResult("", "", 0,"退出Link为单方向且通行方向不能进入路口");
+//						return;
+//					}*/
+//					boolean isRight=false;
+//					//线线关系
+//					if(topoObj.getRelationshipType()==2){
+//						if(this.isValidLineLane(topoObj,inLinkExitNode,outLinkEnterNode)){isRight=true;}
+//					}else if(topoObj.getRelationshipType()==1){
+//						if(this.isValidCrossLane(inLinkExitNode,outLinkEnterNode)){isRight=true;}
+//					}
+//					if(!isRight){
+//						this.setCheckResult("", "", 0,"退出Link通行方向应与车信通行方向一致");
+//						return;
+//					}
+//				}
+//			}}
 	}
 	
 	private boolean isValidLineLane(RdLaneTopology topoObj,int inLinkExitNode,int outLinkEnterNode) throws Exception{

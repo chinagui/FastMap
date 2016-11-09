4339deb6b3172d3be92a5a72fa824471574a2e08
zhaokaikai <zhaokaikai@navinfo.com>
2016-11-09 17:15:34 +0800

    第七迭代任务：节点分离实现挂接功能

--- engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/topo/depart/departrdnode/Operation.java
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/topo/depart/departrdnode/Operation.java
@@ -23,6 +23,13 @@ import org.json.JSONException;
 import java.sql.Connection;
 import java.util.*;
 
+/***
+ * 节点分离具体实现类
+ * 
+ * @author zhaokk
+ * 
+ * 
+ */
 public class Operation implements IOperation {
 
 	private Command command;
@@ -40,15 +47,24 @@ public class Operation implements IOperation {
 	@Override
 	public String run(Result result) throws Exception {
 		this.departNode(result);
-
 		return null;
 	}
 
+	/***
+	 * 分离节点
+	 * 
+	 * @param result
+	 * @throws Exception
+	 */
 	private void departNode(Result result) throws Exception {
-		if (this.command.getCatchNodePid() == 0) {
+
+		if (this.command.getCatchNodePid() == 0
+				&& this.command.getCatchLinkPid() == 0) {
+			// 如果分离节点没有挂接link和node 并且分离的node没有挂接其它的link按照node移动功能处理
 			if (this.command.getLinks().size() == 1) {
 				this.removeNode(result);
 			}
+			// 如果分离节点没有挂接link和node 并且分离的node有挂接其它的link按照原则此node要新增
 			if (this.command.getLinks().size() > 1) {
 				RdNode node = NodeOperateUtils.createRdNode(this.command
 						.getPoint().getX(), this.command.getPoint().getY());
@@ -57,35 +73,87 @@ public class Operation implements IOperation {
 			}
 
 		} else {
-			RdNodeSelector nodeSelector = new RdNodeSelector(conn);
-			IRow row = nodeSelector.loadById(this.command.getCatchNodePid(),
-					true, true);
-			RdNode node = (RdNode) row;
-			Geometry geom = GeoTranslator.transform(node.getGeometry(),
-					0.00001, 5);
-			this.command.setPoint(((Point) GeoTranslator.point2Jts(
-					geom.getCoordinate().x, geom.getCoordinate().y)));
-			if (this.command.getLinks().size() <= 1) {
-				result.insertObject(this.command.getNode(), ObjStatus.DELETE,
-						this.command.getNodePid());
+			// 节点挂接功能
+			if (this.command.getCatchNodePid() != 0) {
+				this.caleCatchNode(result);
 			}
-			this.updateLinkGeomtry(result, this.command.getCatchNodePid());
+			// 打断功能
+			if (this.command.getLinkPid() != 0) {
+				this.caleCatchBreakLink(result);
+			}
 
 		}
 	}
 
+	/***
+	 * 挂接打断功能
+	 * 
+	 * @param result
+	 * @throws Exception
+	 */
+	private void caleCatchBreakLink(Result result) throws Exception {
+		JSONObject breakJson = new JSONObject();
+		breakJson.put("objId", this.command.getCatchLinkPid());
+		breakJson.put("dbId", command.getDbId());
+		JSONObject data = new JSONObject();
+		if (this.command.getLinks().size() == 1) {
+			data.put("breakNodePid", this.command.getNodePid());
+		}
+		data.put("longitude", this.command.getPoint().getX());
+		data.put("latitude", this.command.getPoint().getY());
+		breakJson.put("data", data);
+		com.navinfo.dataservice.engine.edit.operation.topo.breakin.breakrdpoint.Command breakCommand = new com.navinfo.dataservice.engine.edit.operation.topo.breakin.breakrdpoint.Command(
+				breakJson, breakJson.toString());
+		com.navinfo.dataservice.engine.edit.operation.topo.breakin.breakrdpoint.Process breakProcess = new com.navinfo.dataservice.engine.edit.operation.topo.breakin.breakrdpoint.Process(
+				breakCommand, conn, result);
+		breakProcess.innerRun();
+
+	}
+
+	/***
+	 * 节点挂接功能
+	 * 
+	 * @param result
+	 * @throws Exception
+	 */
+	private void caleCatchNode(Result result) throws Exception {
+		// 加载挂接的点求几何
+		RdNodeSelector nodeSelector = new RdNodeSelector(conn);
+		IRow row = nodeSelector.loadById(this.command.getCatchNodePid(), true,
+				true);
+		RdNode node = (RdNode) row;
+		Geometry geom = GeoTranslator.transform(node.getGeometry(), 0.00001, 5);
+		this.command.setPoint(((Point) GeoTranslator.point2Jts(
+				geom.getCoordinate().x, geom.getCoordinate().y)));
+		// 如果原有node挂接的LINK<=1 原来的node需要删除更新link的几何为新的node
+		if (this.command.getLinks().size() <= 1) {
+			result.insertObject(this.command.getNode(), ObjStatus.DELETE,
+					this.command.getNodePid());
+		}
+		// 更新link的几何为新的node点
+		this.updateLinkGeomtry(result, this.command.getCatchNodePid());
+
+	}
+
+	/***
+	 * 调用点的移动功能
+	 * 
+	 * @param result
+	 * @throws Exception
+	 */
 	private void removeNode(Result result) throws Exception {
 		JSONObject updateContent = new JSONObject();
-
+		// 组装移动的参数
+		// dbID
 		updateContent.put("dbId", command.getDbId());
 
 		JSONObject data = new JSONObject();
-
+		// 移动的经纬度
 		data.put("longitude", this.command.getPoint().getX());
-
 		data.put("latitude", this.command.getPoint().getY());
 		updateContent.put("objId", this.command.getNodePid());
 		updateContent.put("data", data);
+		// 调用移动的API
 		com.navinfo.dataservice.engine.edit.operation.topo.move.moverdnode.Command updatecommand = new com.navinfo.dataservice.engine.edit.operation.topo.move.moverdnode.Command(
 				updateContent, this.command.getRdLink(), this.command.getNode());
 		com.navinfo.dataservice.engine.edit.operation.topo.move.moverdnode.Process process = new com.navinfo.dataservice.engine.edit.operation.topo.move.moverdnode.Process(
@@ -93,6 +161,13 @@ public class Operation implements IOperation {
 		process.innerRun();
 	}
 
+	/***
+	 * 修改分离后线的几何 如果分离后线跨图幅需要有打断功能
+	 * 
+	 * @param result
+	 * @param nodePid
+	 * @throws Exception
+	 */
 	private void updateLinkGeomtry(Result result, int nodePid) throws Exception {
 		Geometry geom = GeoTranslator.transform(this.command.getRdLink()
 				.getGeometry(), 0.00001, 5);
@@ -162,7 +237,8 @@ public class Operation implements IOperation {
 			while (it.hasNext()) {
 				String meshIdStr = it.next();
 				Geometry geomInter = MeshUtils.linkInterMeshPolygon(geo,
-						GeoTranslator.transform(MeshUtils.mesh2Jts(meshIdStr),1,5));
+						GeoTranslator.transform(MeshUtils.mesh2Jts(meshIdStr),
+								1, 5));
 				geomInter = GeoTranslator.geojson2Jts(
 						GeoTranslator.jts2Geojson(geomInter), 1, 5);
 				RdLinkOperateUtils.createRdLinkWithMesh(geomInter, maps,
@@ -174,6 +250,7 @@ public class Operation implements IOperation {
 					this.command.getRdLink().pid());
 
 		}
+		// 分离节点属性关系维护
 		this.updateRelation(links, result);
 	}
 

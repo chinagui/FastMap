acc41ca72d2918377db34937d04f803bcb291672
songdongyan <songdongyan@navinfo.com>
2016-11-17 15:12:52 +0800

    修改edit_plus模型配置

--- engine/engine-editplus/src/main/java/com/navinfo/dataservice/engine/editplus/model/selector/ObjSelector.java
+++ engine/engine-editplus/src/main/java/com/navinfo/dataservice/engine/editplus/model/selector/ObjSelector.java
@@ -1,12 +1,15 @@
 package com.navinfo.dataservice.engine.editplus.model.selector;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Map;
 
 import com.navinfo.dataservice.engine.editplus.glm.GlmFactory;
 import com.navinfo.dataservice.engine.editplus.glm.GlmObject;
+import com.navinfo.dataservice.engine.editplus.glm.GlmRef;
 import com.navinfo.dataservice.engine.editplus.glm.GlmTable;
+import com.navinfo.dataservice.engine.editplus.glm.GlmTableNotFoundException;
 import com.navinfo.dataservice.engine.editplus.model.BasicRow;
 import com.navinfo.dataservice.engine.editplus.model.obj.BasicObj;
 import com.navinfo.dataservice.engine.editplus.model.obj.ObjFactory;
@@ -24,7 +27,7 @@ public class ObjSelector {
 	public static BasicObj selectByPid(Connection conn,String objType,SelectorConfig selConfig,long pid,boolean isOnlyMain,boolean isLock)throws Exception{
 		GlmObject glmObj = GlmFactory.getInstance().getObjByType(objType);
 		GlmTable mainTable = glmObj.getMainTable();
-		Class<?> clazz = Class.forName(mainTable.getModelClassName());
+//		Class<?> clazz = Class.forName(mainTable.getModelClassName());
 		String sql = "SELECT * FROM "+mainTable.getName()+" WHERE "+mainTable.getPkColumn()+"=?";
 		BasicRow mainrow = new QueryRunner().query(conn, sql, new SelRsHandler(mainTable,pid),pid);
 		BasicObj obj = ObjFactory.getInstance().create4Select(mainrow);
@@ -50,14 +53,29 @@ public class ObjSelector {
 		return null;
 	}
 	
-	public static void selectChildren(Connection conn,BasicObj obj,GlmObject glmObj){
+	/**
+	 * 获取全部子表
+	 * @param conn
+	 * @param obj
+	 * @param glmObj
+	 * @throws SQLException 
+	 */
+	public static void selectChildren(Connection conn,BasicObj obj,GlmObject glmObj) throws SQLException{
 		Map<String,GlmTable> tables = glmObj.getTables();
 		for(Map.Entry<String, GlmTable> entry:tables.entrySet()){
-			
+			selectChildren(conn,obj,entry.getValue());
 		}
 	}
 
-	public static void selectChildren(Connection conn,BasicObj obj,GlmObject glmObj,Collection<String> filterTables){
+	/**
+	 * 获取配置之外的子表
+	 * @param conn
+	 * @param obj
+	 * @param glmObj
+	 * @param filterTables
+	 * @throws SQLException 
+	 */
+	public static void selectChildren(Connection conn,BasicObj obj,GlmObject glmObj,Collection<String> filterTables) throws SQLException{
 		Map<String,GlmTable> tables = glmObj.getTables();
 		for(Map.Entry<String, GlmTable> entry:tables.entrySet()){
 			if(filterTables!=null&&filterTables.contains(entry.getKey())){
@@ -67,26 +85,65 @@ public class ObjSelector {
 		}
 	}
 	
-	public static void selectChildren(Connection conn,BasicObj obj,SelectorConfig selConfig){
+	/**
+	 * 根据配置获取子表
+	 * @param conn
+	 * @param obj
+	 * @param selConfig
+	 * @throws SQLException 
+	 * @throws GlmTableNotFoundException 
+	 */
+	public static void selectChildren(Connection conn,BasicObj obj,SelectorConfig selConfig) throws GlmTableNotFoundException, SQLException{
 		if(selConfig!=null){
+			//存在配置
 			if(selConfig.getSpecTables()!=null){
 				for(String tab:selConfig.getSpecTables()){
-					//
+					//获取单个子表
+					selectChildren(conn,obj,tab);
 				}
 			}else if(selConfig.getFilterTables()!=null){
-				
+				selectChildren(conn,obj,GlmFactory.getInstance().getObjByType(obj.objType()),selConfig.getFilterTables());
 			}
 		}else{
+			//全部子表
 			selectChildren(conn,obj,GlmFactory.getInstance().getObjByType(obj.objType()));
 		}
 	}
 
-	public static void selectChildren(Connection conn,BasicObj obj,String specTable){
+	/**
+	 * 根据子表名获取子表
+	 * @param conn
+	 * @param obj
+	 * @param specTable 子表名
+	 * @throws SQLException 
+	 * @throws GlmTableNotFoundException 
+	 */
+	public static void selectChildren(Connection conn,BasicObj obj,String specTable) throws GlmTableNotFoundException, SQLException{
 		selectChildren(conn,obj,GlmFactory.getInstance().getTableByName(specTable));
 	}
-	
-	private static void selectChildren(Connection conn,BasicObj obj,GlmTable glmTab){
-		//todo
+
+	/**
+	 * 获取子表最终实现
+	 * @param conn
+	 * @param obj
+	 * @param glmTab 子表
+	 * @throws SQLException 
+	 */
+	private static void selectChildren(Connection conn,BasicObj obj,GlmTable glmTab) throws SQLException{
+		long objPid = obj.objPid();
+		GlmRef objRef = glmTab.getObjRef();
+		//直接关联
+		if(objRef.isRefMain()){
+			String sql = "SELECT T.* FROM "+glmTab.getName()+"T,"+objRef.getRefTable()+"R WHERE T."
+					+ objRef.getCol() + "=" + objRef.getRefCol();
+			BasicRow childRow = new QueryRunner().query(conn, sql, new SelRsHandler(glmTab,objPid),objPid);
+			//更新obj
+		}
+		//间接关联
+		else{
+			
+		}
+		
 	}
 	
 }

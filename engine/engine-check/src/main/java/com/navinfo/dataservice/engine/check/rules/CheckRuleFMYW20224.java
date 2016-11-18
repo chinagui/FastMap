3ab9c41b5d4aab6e98bf52f946d923c4d53712dd
zhangpengpeng04994 <zhangpengpeng@navinfo.com>
2016-11-18 11:31:40 +0800

    修改检查项FM-YW-20-224

--- engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/CheckRuleFMYW20224.java
+++ engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/CheckRuleFMYW20224.java
@@ -72,7 +72,7 @@ public class CheckRuleFMYW20224 extends baseRule {
 							// 包含年、月字样，并且收费标准（IX_POI_PARKING.TOLL_STD)不为0或者1不包含0或1的报出
 							if (tollDes.contains("年") || tollDes.contains("月")) {
 								String tollStd = ixPoiParking.getTollStd();
-								if (tollStd.isEmpty() || !tollStd.contains("0") || !tollStd.contains("1")) {
+								if (StringUtils.isEmpty(tollStd) || !tollStd.contains("0") || !tollStd.contains("1")) {
 									errorLog.append("收费信息与收费标准矛盾; ");
 								}
 
@@ -93,7 +93,7 @@ public class CheckRuleFMYW20224 extends baseRule {
 							if (tollDes.contains("：00")) {
 								// IX_POI_PARKING.OPEN_TIME营业时间为空
 								String openTime = ixPoiParking.getOpenTiime();
-								if (StringUtils.isNotEmpty(openTime)) {
+								if (StringUtils.isEmpty(openTime)) {
 									errorLog.append("收费信息包含“：00”，营业时间为空; ");
 								}
 							}

71bfb44daf3a64ad2d8403840bfda755efd8a97c
Xiaowen-Navinfo <xiaoxiaowen4127@navinfo.com>
2016-11-21 11:20:33 +0800

    multisrc

--- engine/engine-editplus/src/main/java/com/navinfo/dataservice/engine/editplus/convert/MultiSrcPoiConvertor.java
+++ engine/engine-editplus/src/main/java/com/navinfo/dataservice/engine/editplus/convert/MultiSrcPoiConvertor.java
@@ -2,7 +2,9 @@ package com.navinfo.dataservice.engine.editplus.convert;
 
 import java.util.List;
 
+import com.navinfo.dataservice.engine.editplus.model.ixpoi.IxPoi;
 import com.navinfo.dataservice.engine.editplus.model.obj.BasicObj;
+import com.navinfo.dataservice.engine.editplus.model.obj.IxPoiObj;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
@@ -13,21 +15,22 @@ import net.sf.json.JSONObject;
  * @date 2016年11月10日
  * @Description: MultiSrcPoiConvertor.java
  */
-public class MultiSrcPoiConvertor implements ObjConvertor {
+public class MultiSrcPoiConvertor {
 
 	/**
 	 * 参考接口说明
 	 */
-	@Override
-	public JSONObject toJson(BasicObj obj) throws ObjConvertException {
-		// TODO 
+	public JSONObject toJson(IxPoiObj poi) throws ObjConvertException {
+		JSONObject jo = new JSONObject();
+		IxPoi ixPoi = (IxPoi)poi.getMainrow();
+		jo.put("fid", ixPoi.getPoiNum());
+		jo.put("name", poi.getNameByLct("CHI", 1, 1).getName());
 		return null;
 	}
 
 	/**
 	 * 参考接口说明
 	 */
-	@Override
 	public BasicObj fromJson(JSONObject jo, String objType,boolean isSetPid) throws ObjConvertException {
 		// 暂不实现
 		return null;

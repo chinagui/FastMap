5089fcfbc2af4792864f682877377ad8bdcc5a57
Xiaowen-Navinfo <xiaoxiaowen4127@navinfo.com>
2016-11-25 00:26:35 +0800

    multisrc

--- engine/engine-editplus/src/main/java/com/navinfo/dataservice/engine/editplus/operation/OperationResult.java
+++ engine/engine-editplus/src/main/java/com/navinfo/dataservice/engine/editplus/operation/OperationResult.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.navinfo.dataservice.engine.editplus.model.obj.BasicObj;
+import com.navinfo.navicommons.database.sql.RunnableSQL;
 
 
 /**
@@ -17,16 +18,6 @@ import com.navinfo.dataservice.engine.editplus.model.obj.BasicObj;
  */
 public class OperationResult{
 	
-	/**
-	 * 新增对象列表:key:objType,value:key:objPid,value:obj
-	 */
-	private Map<String,Map<Long,BasicObj>> addObjs = new HashMap<String,Map<Long,BasicObj>>();
-
-	/**
-	 * 修改对象列表
-	 */
-	private Map<String,Map<Long,BasicObj>> updateObjs = new HashMap<String,Map<Long,BasicObj>>();
-
 	private List<BasicObj> allObjs=new ArrayList<BasicObj>();
 	
 	public List<BasicObj> getAllObjs() {
@@ -39,61 +30,25 @@ public class OperationResult{
 	 * 新增、删除和修改的对象添加到result中
 	 * @param obj
 	 */
-	protected void putObj(BasicObj obj)throws OperationResultException{
+	public void putObj(BasicObj obj)throws OperationResultException{
 		if(obj.opType().equals(OperationType.INITIALIZE)){
 			throw new OperationResultException("未设置操作类型的对象");
 		}
 		allObjs.add(obj);
-		//分类，方便后续使用
-		Map<Long,BasicObj> objs=null;
-		String obType = obj.objType();
-		if(obj.opType().equals(OperationType.INSERT)){
-			if(addObjs.containsKey(obType)){
-				addObjs.get(obType).put(obj.objPid(), obj);
-			}else{
-				objs = new HashMap<Long,BasicObj>();
-				objs.put(obj.objPid(), obj);
-				addObjs.put(obType, objs);
-			}
-		}else if(obj.opType().equals(OperationType.UPDATE)){
-			if(updateObjs.containsKey(obType)){
-				updateObjs.get(obType).put(obj.objPid(), obj);
-			}else{
-				objs = new HashMap<Long,BasicObj>();
-				objs.put(obj.objPid(), obj);
-				updateObjs.put(obType, objs);
-			}
-		}
 	}
 
-	/**
-	 * @return 新增记录列表
-	 * key:objType,value:key:objPid,value:obj
-	 */
-	public Map<String,Map<Long,BasicObj>> getAddObjs() {
-		return addObjs;
-	}
-
-	/**
-	 * @return 修改记录列表
-	 * key:objType,value:key:objPid,value:obj
-	 */
-	public Map<String,Map<Long,BasicObj>> getUpdateObjs() {
-		return updateObjs;
-	}
-
-	protected void clear() {
-		this.addObjs.clear();
-		this.updateObjs.clear();
+	public void clear() {
+		this.allObjs.clear();
 	}
 	/**
 	 * 
 	 * @param result
 	 */
-	protected void putAll(List<BasicObj> objs) {
+	public void putAll(List<BasicObj> objs) {
 		//key:objType,value:key:objPid,value:obj
 		if(objs!=null&&objs.size()>0){
 			//
+			allObjs.addAll(objs);
 		}
 	}
 	
@@ -101,6 +56,7 @@ public class OperationResult{
 	 * 持久化一次操作的变更，持久化包括数据和履历
 	 * 把本次操作有变更的对象写入result中，不需要加入delete状态的对象
 	 * 把变更的对象的变更信息写hisChangeLogs中，并设置所有对象当前操作状态为update
+	 * SQL执行再优化
 	 * @param conn
 	 * @param objs
 	 * @throws Exception
@@ -109,7 +65,12 @@ public class OperationResult{
 		if(objs==null||objs.size()==0)return;
 		//持久化一次操作的变更，持久化包括数据和履历
 		for(BasicObj obj:objs){
-			obj.generateSql();
+			List<RunnableSQL> sqls = obj.generateSql();
+			if(sqls!=null){
+				for(RunnableSQL sql:sqls){
+					sql.run(conn);
+				}
+			}
 		}
 		//...
 		//把本次操作有变更的对象写入result中，不需要加入delete状态的对象

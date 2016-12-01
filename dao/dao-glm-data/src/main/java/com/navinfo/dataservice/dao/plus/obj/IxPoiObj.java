706b1077ec1b4f047aa97fbc1c5bf04c821bf1f4
songdongyan <songdongyan@navinfo.com>
2016-12-01 11:51:03 +0800

    edit-plus IxPoiObj增加方法createIxPoiGasstation

--- dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/plus/obj/IxPoiObj.java
+++ dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/plus/obj/IxPoiObj.java
@@ -114,7 +114,15 @@ public class IxPoiObj extends AbstractIxObj {
 		return (List)subrows.get("IxPoiGasstation");
 	}
 	public IxPoiGasstation createIxPoiGasstation()throws Exception{
-		return null;
+		IxPoiGasstation ixPoiGasstation = (IxPoiGasstation)(ObjFactory.getInstance().createRow("IX_POI_GASSTATION", this.objPid()));
+		if(subrows.containsKey("IX_POI_GASSTATION")){
+			subrows.get("IX_POI_GASSTATION").add(ixPoiGasstation);
+		}else{
+			List<BasicRow> ixixPoiGasstationList = new ArrayList<BasicRow>();
+			ixixPoiGasstationList.add(ixPoiGasstation);
+			subrows.put("IX_POI_GASSTATION", ixixPoiGasstationList);
+		}
+		return ixPoiGasstation;
 	}
 	public List<IxPoiHotel> getIxPoiHotels(){
 		return (List)subrows.get("IX_POI_HOTEL");

dae836577a853188117d283d14988cbf0bfc016a
zhangxiaolong4943 <zhangxiaolong4943@navinfo.com>
2016-12-01 11:10:17 +0800

    CRF制作添加控制

--- engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/obj/rdobject/create/Check.java
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/obj/rdobject/create/Check.java
@@ -1,20 +1,50 @@
 package com.navinfo.dataservice.engine.edit.operation.obj.rdobject.create;
 
 import java.sql.Connection;
+import java.util.Map;
+
+import com.navinfo.dataservice.commons.util.JsonUtils;
+import com.navinfo.dataservice.dao.glm.iface.ObjType;
+import com.navinfo.dataservice.dao.glm.model.rd.crf.RdObject;
+import com.navinfo.dataservice.dao.glm.selector.rd.crf.RdObjectSelector;
 
 public class Check {
-	
+
 	private Command command;
-	
-	public Check(Command command)
-	{
+
+	public Check(Command command) {
 		this.command = command;
 	}
-	
+
 	/**
 	 * @param conn
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public void hasRdObject(Connection conn) throws Exception {
+		RdObjectSelector selector = new RdObjectSelector(conn);
+
+		String linkPids = JsonUtils.getStringValueFromJSONArray(this.command.getLinkArray());
+
+		Map<String, RdObject> linkObjMap = selector.loadRdObjectByPidAndType(linkPids, ObjType.RDLINK, true);
+
+		if (linkObjMap.size() > 0) {
+			throw new Exception("所选link已经存在CRFO对象");
+		}
+
+		String interPids = JsonUtils.getStringValueFromJSONArray(this.command.getInterArray());
+
+		Map<String, RdObject> interObjMap = selector.loadRdObjectByPidAndType(interPids, ObjType.RDINTER, true);
+
+		if (interObjMap.size() > 0) {
+			throw new Exception("所选inter已经存在CRFO对象");
+		}
+
+		String roadPids = JsonUtils.getStringValueFromJSONArray(this.command.getRoadArray());
+
+		Map<String, RdObject> roadObjMap = selector.loadRdObjectByPidAndType(roadPids, ObjType.RDROAD, true);
+
+		if (roadObjMap.size() > 0) {
+			throw new Exception("所选road已经存在CRFO对象");
+		}
 	}
 }

52d0e49640233f51d96abad2a2f66e0a06be1546
songdongyan <songdongyan@navinfo.com>
2016-12-01 15:47:56 +0800

    edit-plus Operation持久化修改

--- dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/plus/operation/AbstractOperation.java
+++ dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/plus/operation/AbstractOperation.java
@@ -1,15 +1,23 @@
 package com.navinfo.dataservice.dao.plus.operation;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 
 import com.navinfo.dataservice.dao.plus.editman.PoiEditStatus;
+import com.navinfo.dataservice.dao.plus.glm.GlmFactory;
+import com.navinfo.dataservice.dao.plus.glm.GlmTable;
 import com.navinfo.dataservice.dao.plus.log.LogGenerator;
 import com.navinfo.dataservice.dao.plus.model.basic.OperationType;
 import com.navinfo.dataservice.dao.plus.obj.BasicObj;
+import com.navinfo.dataservice.dao.plus.selector.ObjChildrenIncreSelector;
 import com.navinfo.navicommons.database.sql.RunnableSQL;
 
 /** 
@@ -50,6 +58,10 @@ public abstract class AbstractOperation {
 	public void persistChangeLog(int opSg,long userId)throws Exception{
 		if(result==null||result.getAllObjs().size()==0)return;
 		//持久化一次操作的变更，持久化包括数据和履历
+		
+		//删除对象删除其所有子表
+		deleteObjHandler();
+
 		//新增IX_POI对象向poi_edit_status表中插入记录
 		PoiEditStatus.insertPoiEditStatus(conn,result);
 		//持久化履历
@@ -68,6 +80,44 @@ public abstract class AbstractOperation {
 				it.remove();
 			}else{//如果不为删除，则将修改加入历史变更，给下一操作阶段做参考
 				obj.afterPersist();
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * @throws Exception 
+	 * 
+	 */
+	private void deleteObjHandler() throws Exception {
+		// TODO Auto-generated method stub
+		Map<String,Map<Long,BasicObj>> objs = new HashMap<String,Map<Long,BasicObj>>();
+		Map<String,Set<String>> selConfigs = new HashMap<String,Set<String>>();
+		for(Iterator<BasicObj> it= result.getAllObjs().iterator();it.hasNext();){
+			BasicObj obj = it.next();
+			if(objs.containsKey(obj.objType())){
+				objs.get(obj.objType()).put(obj.objPid(), obj);
+			}else{
+				Map<Long,BasicObj> objMap = new HashMap<Long,BasicObj>();
+				objMap.put(obj.objPid(), obj);
+				objs.put(obj.objType(), objMap);
+			}
+			//子表配置
+			if(!selConfigs.containsKey(obj.objType())){
+				//对象全部子表
+				Set<String> selConfig = new HashSet<String>();
+				for(Map.Entry<String, GlmTable> entry:GlmFactory.getInstance().getObjByType(obj.objType()).getTables().entrySet()){
+					selConfig.add(entry.getKey());
+				}
+				selConfigs.put(obj.objType(), selConfig);
+			}	
+		}
+		ObjChildrenIncreSelector.increSelect(conn, objs, selConfigs);
+		for(Map.Entry<String, Map<Long,BasicObj>> entry:objs.entrySet()){
+			for(Map.Entry<Long, BasicObj> subEntry:entry.getValue().entrySet()){
+				BasicObj obj = subEntry.getValue();
+				obj.deleteObj();
+				result.putObj(obj);
 			}
 		}
 	}

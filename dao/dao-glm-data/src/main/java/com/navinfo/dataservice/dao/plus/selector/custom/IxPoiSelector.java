cf09cad369205006135e81a973730fdb0c9772c8
Xiaowen-Navinfo <xiaoxiaowen4127@navinfo.com>
2016-12-07 17:11:51 +0800

    multisrc

--- dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/plus/selector/custom/IxPoiSelector.java
+++ dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/plus/selector/custom/IxPoiSelector.java
@@ -44,6 +44,19 @@ import com.navinfo.navicommons.exception.ServiceException;
 public class IxPoiSelector {
 	protected static Logger log = LoggerRepos.getLogger(IxPoiSelector.class);
 
+	public static Map<String,Long> getPidByFids(Connection conn,Collection<String> fids)throws Exception{
+		if(fids==null|fids.size()==0)return null;
+
+		if(fids.size()>1000){
+			String sql= "SELECT PID,POI_NUM FROM IX_POI WHERE POI_NUM IN (SELECT COLUMN_VALUE FROM TABLE(CLOB_TO_TABLE(?)))";
+			Clob clob = ConnectionUtil.createClob(conn);
+			clob.setString(1, StringUtils.join(fids, ","));
+			return new QueryRunner().query(conn, sql, new FidPidSelHandler(),clob);
+		}else{
+			String sql= "SELECT PID,POI_NUM FROM IX_POI WHERE POI_NUM IN ('"+StringUtils.join(fids, "','")+"')";
+			return new QueryRunner().query(conn,sql,new FidPidSelHandler());
+		}
+	}
 	public static Map<Long,Long> getAdminIdByPids(Connection conn,Collection<Long> pids)throws Exception{
 		if(pids!=null&&pids.size()>0){
 			if(pids.size()>1000){

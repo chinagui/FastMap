5c957b6d97d0dd56df450cc45fa7d2668c22ab5f
zhangyuntao <zhangyuntao@navinfo.com>
2016-12-07 11:22:53 +0800

    修改错误引用

--- engine/engine-edit/src/test/java/com/navinfo/dataservice/engine/edit/zhangyuntao/rd/TranslateTest.java
+++ engine/engine-edit/src/test/java/com/navinfo/dataservice/engine/edit/zhangyuntao/rd/TranslateTest.java
@@ -17,10 +17,10 @@ public class TranslateTest extends InitApplication {
 
     @Test
     public void loadData() throws IOException {
-        EngConverterHelper engConverterHelper = new EngConverterHelper();
-        System.out.println(engConverterHelper.chiToEng("drfg银行201省道201店"));
-        System.out.println(engConverterHelper.chiToEng("210省道130国道好汉岗"));
-        System.out.println(engConverterHelper.chiToEng("电子二路与电子正街口"));
-        System.out.println(engConverterHelper.chiToEng("北京13号楼安顺路drfg银行商业广场33号库 中关村45号大街好自助餐输入自助餐厅over商业广场ii商业广场自助餐厅201省道201店"));
+//        EngConverterHelper engConverterHelper = new EngConverterHelper();
+//        System.out.println(engConverterHelper.chiToEng("drfg银行201省道201店"));
+//        System.out.println(engConverterHelper.chiToEng("210省道130国道好汉岗"));
+//        System.out.println(engConverterHelper.chiToEng("电子二路与电子正街口"));
+//        System.out.println(engConverterHelper.chiToEng("北京13号楼安顺路drfg银行商业广场33号库 中关村45号大街好自助餐输入自助餐厅over商业广场ii商业广场自助餐厅201省道201店"));
     }
 }

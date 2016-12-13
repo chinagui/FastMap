ed17101ac0aaf2ee0c2f4fa23b31004f600b44e8
jicaihua <jicaihua@navinfo.com>
2016-12-13 17:07:50 +0800

    增加sc_point_spec_kindcode_new元数据查询类

---
+++ engine/engine-meta/src/main/java/com/navinfo/dataservice/engine/meta/scPointSpecKindcode/ScPointSpecKindcode.java
@@ -0,0 +1,56 @@
+package com.navinfo.dataservice.engine.meta.scPointSpecKindcode;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.dbutils.DbUtils;
+
+import com.navinfo.dataservice.bizcommons.datasource.DBConnector;
+
+public class ScPointSpecKindcode {
+	private Map<String, String> typeMap8= new HashMap<String, String>();
+
+	private static class SingletonHolder {
+		private static final ScPointSpecKindcode INSTANCE = new ScPointSpecKindcode();
+	}
+
+	public static final ScPointSpecKindcode getInstance() {
+		return SingletonHolder.INSTANCE;
+	}
+	
+	public Map<String, String> scPointSpecKindCodeType8() throws Exception{
+		if (typeMap8==null||typeMap8.isEmpty()) {
+				synchronized (this) {
+					if (typeMap8==null||typeMap8.isEmpty()) {
+						try {
+							String sql = "select POI_KIND,CHAIN from sc_point_spec_kindcode_new t WHERE TYPE=8";
+								
+							PreparedStatement pstmt = null;
+							ResultSet rs = null;
+							Connection conn = null;
+							try {
+								conn = DBConnector.getInstance().getMetaConnection();
+								pstmt = conn.prepareStatement(sql);
+								rs = pstmt.executeQuery();
+								while (rs.next()) {
+									typeMap8.put(rs.getString("POI_KIND"), rs.getString("CHAIN"));					
+								} 
+							} catch (Exception e) {
+								throw new Exception(e);
+							} finally {
+								DbUtils.commitAndCloseQuietly(conn);
+							}
+						} catch (Exception e) {
+							throw new SQLException("加载sc_point_spec_kindcode_new失败："+ e.getMessage(), e);
+						}
+					}
+				}
+			}
+			return typeMap8;
+	}
+
+}

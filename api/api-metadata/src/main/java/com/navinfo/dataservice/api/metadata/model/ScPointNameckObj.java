3ad58f1495949b112b31973aedd9e6653458ad77
xiaolvluo <zhangxiaoyi@navinfo.com>
2016-12-16 10:03:25 +0800

    找回代码

---
+++ api/api-metadata/src/main/java/com/navinfo/dataservice/api/metadata/model/ScPointNameckObj.java
@@ -0,0 +1,25 @@
+package com.navinfo.dataservice.api.metadata.model;
+
+public class ScPointNameckObj {
+	private String preKey;
+	private String resultKey;
+	private int type;
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public String getPreKey() {
+		return preKey;
+	}
+	public void setPreKey(String preKey) {
+		this.preKey = preKey;
+	}
+	public String getResultKey() {
+		return resultKey;
+	}
+	public void setResultKey(String resultKey) {
+		this.resultKey = resultKey;
+	}
+}

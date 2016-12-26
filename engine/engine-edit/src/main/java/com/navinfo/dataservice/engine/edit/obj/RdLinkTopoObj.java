6f0526b421eb1328f2e28c63f0e7dfa3db624967
Xiaowen-Navinfo <xiaoxiaowen4127@navinfo.com>
2016-12-26 11:22:07 +0800

    clm连通关系

---
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/obj/RdLinkTopoObj.java
@@ -0,0 +1,13 @@
+package com.navinfo.dataservice.engine.edit.obj;
+
+import com.navinfo.dataservice.dao.glm.model.rd.link.RdLink;
+
+/** 
+ * @ClassName: RdLinkTopoObj
+ * @author xiaoxiaowen4127
+ * @date 2016年12月26日
+ * @Description: RdLinkTopoObj.java
+ */
+public class RdLinkTopoObj {
+	protected RdLink inLink;
+}

d1921704b1cbbb2300024e357065b9e7bba0b106
Xiaowen-Navinfo <xiaoxiaowen4127@navinfo.com>
2016-12-26 14:46:00 +0800

    clm连通关系

--- engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/obj/RdLinkTopoObj.java
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/obj/RdLinkTopoObj.java
@@ -1,6 +1,17 @@
 package com.navinfo.dataservice.engine.edit.obj;
 
+import java.util.List;
+import java.util.Map;
+
+import com.navinfo.dataservice.dao.glm.model.rd.cross.RdCross;
+import com.navinfo.dataservice.dao.glm.model.rd.gate.RdGate;
+import com.navinfo.dataservice.dao.glm.model.rd.lane.RdLane;
+import com.navinfo.dataservice.dao.glm.model.rd.lane.RdLaneTopoDetail;
+import com.navinfo.dataservice.dao.glm.model.rd.laneconnexity.RdLaneConnexity;
 import com.navinfo.dataservice.dao.glm.model.rd.link.RdLink;
+import com.navinfo.dataservice.dao.glm.model.rd.node.RdNode;
+import com.navinfo.dataservice.dao.glm.model.rd.restrict.RdRestriction;
+import com.navinfo.dataservice.dao.glm.model.rd.tollgate.RdTollgate;
 
 /** 
  * @ClassName: RdLinkTopoObj
@@ -9,5 +20,12 @@ import com.navinfo.dataservice.dao.glm.model.rd.link.RdLink;
  * @Description: RdLinkTopoObj.java
  */
 public class RdLinkTopoObj {
-	protected RdLink inLink;
+	protected List<RdLink> Links;
+	protected Map<Long,List<RdLane>> lanes;//key:link_pid
+	protected List<RdLaneTopoDetail> laneTopoDetails;
+	protected RdTollgate tollage;//inlink,innode参与的收费站
+	protected RdLaneConnexity laneConexity;//inlink，innode参与的车信
+	protected RdCross cross;//inLink参与的路口
+	protected RdRestriction restriction;//inlink,innode参与的交限
+	protected RdGate gate;//inlink,innode参与的大门
 }

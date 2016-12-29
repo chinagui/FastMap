54cd1d4d4d2264d0c4bdeac969925f56e3fdac83
gaopengrong@navinfo.com <gaopengrong@navinfo.com>
2016-12-29 16:27:39 +0800

    新增检查项：FMYW20092.java

---
+++ engine/engine-editplus/src/main/java/com/navinfo/dataservice/engine/editplus/batchAndCheck/check/rule/FMYW20092.java
@@ -0,0 +1,67 @@
+package com.navinfo.dataservice.engine.editplus.batchAndCheck.check.rule;
+
+import java.util.Collection;
+import java.util.List;
+
+import com.navinfo.dataservice.dao.plus.model.basic.OperationType;
+import com.navinfo.dataservice.dao.plus.model.ixpoi.IxPoi;
+import com.navinfo.dataservice.dao.plus.model.ixpoi.IxPoiName;
+import com.navinfo.dataservice.dao.plus.obj.BasicObj;
+import com.navinfo.dataservice.dao.plus.obj.IxPoiObj;
+import com.navinfo.dataservice.dao.plus.obj.ObjectName;
+/**
+ * 检查条件：
+ *  以下条件其中之一满足时，需要进行检查：
+ * (1)存在官方原始中文名称IX_POI_NAME新增；
+ * (2)存在官方原始中文名称IX_POI_NAME修改或KIND_CODE字段修改；
+ * 检查原则：
+ * 1）官方标准化中文名中包含“口”且不包含“入”和“出”，官方原始英文名称中包含“exit”或“entrance”且不包含“exit/entrance”时
+ * 2）官方标准化中文名中包含“出入口”，官方原始英文名中不包含“exit/entrance”时
+ * 报log：出入口翻译错误
+ * @author gaopengrong
+ */
+public class FMYW20092 extends BasicCheckRule {
+	
+	@Override
+	public void loadReferDatas(Collection<BasicObj> batchDataList)
+			throws Exception {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void runCheck(BasicObj obj) throws Exception {
+		if(obj.objName().equals(ObjectName.IX_POI)){
+			IxPoiObj poiObj=(IxPoiObj) obj;
+			if (!isCheck(poiObj)){return;}
+			IxPoi poi=(IxPoi) poiObj.getMainrow();
+			IxPoiName name=poiObj.getOfficeStandardCHIName();
+			
+		}
+	}
+	
+	private boolean isCheck(IxPoiObj poiObj){
+		IxPoi poi=(IxPoi) poiObj.getMainrow();
+		if(poi.hisOldValueContains(IxPoi.KIND_CODE)){
+			String oldKindCode=(String) poi.getHisOldValue(IxPoi.KIND_CODE);
+			String newKindCode=poi.getKindCode();
+			if(!newKindCode.equals(oldKindCode)){
+				return true;
+			}
+		}
+		//存在IX_POI_NAME新增或者修改履历
+		IxPoiName br=poiObj.getOfficeOriginCHIName();
+		if(br!=null){
+			if(br.getHisOpType().equals(OperationType.INSERT)){return true;}
+			if(br.getHisOpType().equals(OperationType.UPDATE) && br.hisOldValueContains(IxPoiName.NAME)){
+				String oldName=(String) br.getHisOldValue(IxPoiName.NAME);
+				String newName=br.getName();
+				if(!newName.equals(oldName)){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	
+}

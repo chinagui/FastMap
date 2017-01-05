9c8c7d9ece41b6ad5ee16ce3b74fb2d309857887
wangdongbin <wangdongbin@navinfo.com>
2017-01-05 19:55:01 +0800

    月编检查项

--- engine/engine-editplus/src/main/java/com/navinfo/dataservice/engine/editplus/batchAndCheck/check/rule/FMZY20138.java
+++ engine/engine-editplus/src/main/java/com/navinfo/dataservice/engine/editplus/batchAndCheck/check/rule/FMZY20138.java
@@ -12,16 +12,17 @@ import com.navinfo.dataservice.dao.plus.model.ixpoi.IxPoi;
 import com.navinfo.dataservice.dao.plus.model.ixpoi.IxPoiAddress;
 import com.navinfo.dataservice.dao.plus.obj.BasicObj;
 import com.navinfo.dataservice.dao.plus.obj.IxPoiObj;
+import com.navinfo.dataservice.engine.editplus.batchAndCheck.common.CheckUtil;
 
 import net.sf.json.JSONObject;
 
 /**
  * 
- * 1.可以为空；2.字符串；3.不为空时，不能含有非法字符。
- * Log：地址道路名含有非法字符“XX”。
+ * 1.可以为空；2.字符串；3.不为空时，不能含有汉字和非法字符。
+ *	 Log：地址道路名拼音含有汉字和非法字符“XX”。
  *
  */
-public class FMZY20136 extends BasicCheckRule {
+public class FMZY20138 extends BasicCheckRule {
 
 	@Override
 	public void runCheck(BasicObj obj) throws Exception {
@@ -35,20 +36,20 @@ public class FMZY20136 extends BasicCheckRule {
 		JSONObject characterMap = metaApi.getCharacterMap();
 		List<String> errList = new ArrayList<String>();
 		for (IxPoiAddress address:addresses) {
-			String addStr = address.getRoadname();
+			String addStr = address.getRoadnamePhonetic();
 			if (addStr == null || addStr.isEmpty()) {
 				continue;
 			}
 			addStr = addStr.replace("|","");
 			for (int i=0;i<addStr.length();i++) {
 				char character = addStr.charAt(i);
-				if (!characterMap.containsKey(String.valueOf(character))) {
-					errList.add("地址道路名含有非法字符“"+character+"”。");
+				if (!characterMap.containsKey(String.valueOf(character)) || CheckUtil.isChinese(String.valueOf(character))) {
+					errList.add("地址道路名拼音含有汉字和非法字符“"+character+"”。");
 				}
 			}
 		}
 		if (errList.size()>0) {
-			setCheckResult(poi.getGeometry(), "[IX_POI,"+poi.getPid()+"]", poi.getMeshId(),StringUtils.join(errList, ";"));
+			setCheckResult(poi.getGeometry(), "[IX_POI,"+poi.getPid()+"]", poi.getMeshId(), StringUtils.join(errList, ";"));
 		}
 	}
 

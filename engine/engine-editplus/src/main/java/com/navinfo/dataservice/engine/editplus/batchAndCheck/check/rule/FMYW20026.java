5bf4aeb43eb226bca1c1fff03c174d078364ed73
gaopengrong@navinfo.com <gaopengrong@navinfo.com>
2017-01-06 11:16:58 +0800

    修改重分类检查项：由于重分类检查原则有误，重写分类检查

--- engine/engine-editplus/src/main/java/com/navinfo/dataservice/engine/editplus/batchAndCheck/check/rule/FMYW20026.java
+++ engine/engine-editplus/src/main/java/com/navinfo/dataservice/engine/editplus/batchAndCheck/check/rule/FMYW20026.java
@@ -16,11 +16,11 @@ import com.navinfo.dataservice.engine.editplus.batchAndCheck.common.ScPointNamec
 /**
  * 检查条件：
  * 以下条件其中之一满足时，需要进行检查：
- * (1)存在IX_POI_ADDRESS新增 ；
- * (2)存在IX_POI_ADDRESS修改且IX_POI_ADDRESS存在记录；
+ * (1)存在官方中文地址IX_POI_ADDRESS新增 ；
+ * (2)存在官方中文地址IX_POI_ADDRESS修改且官方中文地址IX_POI_ADDRESS存在记录；
  * 检查原则： 
- * 检查SC_POINT_NAMECK表中TYPE=5且HM_FLAG<>’HM’，ROADNAME+ADDRNAME包含关键字且拼音与配置表中拼音不相同的报出
- * 检查SC_POINT_NAMECK表中TYPE=7且HM_FLAG<>’HM’，只要ROADNAME+ADDRNAME中包含关键字的报出。
+ * 检查SC_POINT_NAMECK表中TYPE=5且HM_FLAG<>’HM’，18个字段组合后(省名|市名|区县名|街道名|小区名|街巷名|标志物名|前缀|门牌号|类型名|子号|后缀|附属设施名|楼栋号|楼层|楼门号|房间号|附加信息)包含关键字且拼音与配置表中拼音不相同的报出
+ * 检查SC_POINT_NAMECK表中TYPE=7且HM_FLAG<>’HM’，只要18个字段中包含关键字的报出。
  * 提示：地址常见多音字检查：地址中含有多音字“xx”
  * @author gaopengrong
  */
@@ -39,6 +39,8 @@ public class FMYW20026 extends BasicCheckRule {
 			IxPoi poi=(IxPoi) poiObj.getMainrow();
 			//存在IxPoiAddress新增或者修改履历
 			IxPoiAddress address=poiObj.getCHAddress();
+			//错误数据
+			if(address==null){return;}
 			if(address.getHisOpType().equals(OperationType.INSERT)||(address.getHisOpType().equals(OperationType.UPDATE))){
 				
 				MetadataApi metadataApi=(MetadataApi) ApplicationContextUtil.getBean("metadataApi");
@@ -52,9 +54,11 @@ public class FMYW20026 extends BasicCheckRule {
 						+address.getTypePhonetic()+"|"+address.getSubnumPhonetic()+"|"+address.getSurfixPhonetic()+"|"+address.getEstabPhonetic()+"|"+address.getBuildingPhonetic()+"|"
 						+address.getUnitPhonetic()+"|"+address.getFloorPhonetic()+"|"+address.getRoomPhonetic()+"|"+address.getAddonsPhonetic();
 				String[] allStrSplit= allStr.split("\\|");
-				//检查SC_POINT_NAMECK表中TYPE=5且HM_FLAG<>’HM’名称包含关键字且拼音与配置表中拼音不相同的报出
+				
 				Map<String, String> typeD5 = metadataApi.scPointNameckTypeD5();
+				Map<String, String> typeD7 = metadataApi.scPointNameckTypeD7();
 				for (String strSplit:allStrSplit){	
+					//检查SC_POINT_NAMECK表中TYPE=5且HM_FLAG<>’HM’名称包含关键字且拼音与配置表中拼音不相同的报出
 					Map<String, String> keyResult5=ScPointNameckUtil.matchType(strSplit, typeD5);
 					for(String preKey:keyResult5.keySet()){
 						if (!allStrPhonetic.contains(keyResult5.get(preKey))){
@@ -62,25 +66,15 @@ public class FMYW20026 extends BasicCheckRule {
 							setCheckResult(poi.getGeometry(), "[IX_POI,"+poi.getPid()+"]", poi.getMeshId(),log);
 						}
 					}
-				}
 
-				String newRoadNme= address.getRoadname();
-				String newAddrName= address.getAddrname();
-				String RAName=newRoadNme+"|"+newAddrName;
-				
-				String[] RANameStr= RAName.split("\\|");
-				Map<String, String> typeD7 = metadataApi.scPointNameckTypeD7();
-				for (String nameStr:RANameStr){
 					//检查SC_POINT_NAMECK表中TYPE=7且HM_FLAG<>’HM’只要名称中包含关键字的报出
-					Map<String, String> keyResult7=ScPointNameckUtil.matchType(nameStr, typeD7);
+					Map<String, String> keyResult7=ScPointNameckUtil.matchType(strSplit, typeD7);
 					if (keyResult7.size()!=0){
 						String log="ROADNAME+ADDRNAME中包含关键字“"+(keyResult7.keySet()).toString()+"”";
 						setCheckResult(poi.getGeometry(), "[IX_POI,"+poi.getPid()+"]", poi.getMeshId(),log);
 					}
 				}
 			}
-		 }
+		}
 	}
-	
-	
 }

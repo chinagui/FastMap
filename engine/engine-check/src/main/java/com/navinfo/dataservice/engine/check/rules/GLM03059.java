94e2968f6cc532d95cfffee6243fe1077c54f512
hhmhsm <531476471@qq.com>
2017-01-13 15:47:25 +0800

    修改检查项

--- engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/GLM03059.java
+++ engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/GLM03059.java
@@ -54,29 +54,28 @@ public class GLM03059 extends baseRule {
 	private void checkRdLink(RdLink rdLink) throws Exception {
 		// TODO Auto-generated method stub
 		Map<String, Object> changedFields = rdLink.changedFields();
-		int kind = 7;
 		if(changedFields != null && changedFields.containsKey("kind")){
-			kind = (int) changedFields.get("kind");
-		}
-		if(kind == 1 || kind == 2){
-			StringBuilder sb = new StringBuilder();
-			      
-			sb.append("SELECT DISTINCT RS.LINK_PID FROM RD_NODE_FORM F, RD_LINK RS, RD_LINK RE");
-			sb.append(" WHERE RS.LINK_PID = "+rdLink.getPid()+" AND F.FORM_OF_WAY = 15");
-			sb.append(" AND (RS.S_NODE_PID = F.NODE_PID OR RS.E_NODE_PID = F.NODE_PID)");
-			sb.append(" AND (RE.S_NODE_PID = F.NODE_PID OR RE.E_NODE_PID = F.NODE_PID)");
-			sb.append(" AND RS.LINK_PID <> RE.LINK_PID AND F.U_RECORD <> 2 AND RS.U_RECORD <> 2");
-			sb.append(" AND RE.U_RECORD <> 2 AND RE.KIND IN (1, 2)");
-			String sql = sb.toString();
-			log.info("RdLink后检查GLM03059--sql:" + sql);
-			
-			DatabaseOperator getObj = new DatabaseOperator();
-			List<Object> resultList = new ArrayList<Object>();
-			resultList = getObj.exeSelect(this.getConn(), sql);
-
-			if(!resultList.isEmpty()){
-				String target = "[RD_LINK," + rdLink.getPid() + "]";
-				this.setCheckResult("", target, 0);
+			int kind = (int) changedFields.get("kind");
+			if(kind == 1 || kind == 2){
+				StringBuilder sb = new StringBuilder();
+				
+				sb.append("SELECT DISTINCT RS.LINK_PID FROM RD_NODE_FORM F, RD_LINK RS, RD_LINK RE");
+				sb.append(" WHERE RS.LINK_PID = "+rdLink.getPid()+" AND F.FORM_OF_WAY = 15");
+				sb.append(" AND (RS.S_NODE_PID = F.NODE_PID OR RS.E_NODE_PID = F.NODE_PID)");
+				sb.append(" AND (RE.S_NODE_PID = F.NODE_PID OR RE.E_NODE_PID = F.NODE_PID)");
+				sb.append(" AND RS.LINK_PID <> RE.LINK_PID AND F.U_RECORD <> 2 AND RS.U_RECORD <> 2");
+				sb.append(" AND RE.U_RECORD <> 2 AND RE.KIND IN (1, 2)");
+				String sql = sb.toString();
+				log.info("RdLink后检查GLM03059--sql:" + sql);
+				
+				DatabaseOperator getObj = new DatabaseOperator();
+				List<Object> resultList = new ArrayList<Object>();
+				resultList = getObj.exeSelect(this.getConn(), sql);
+				
+				if(!resultList.isEmpty()){
+					String target = "[RD_LINK," + rdLink.getPid() + "]";
+					this.setCheckResult("", target, 0);
+				}
 			}
 		}
 	}
@@ -89,29 +88,28 @@ public class GLM03059 extends baseRule {
 	private void checkRdNodeForm(RdNodeForm rdNodeForm) throws Exception {
 		// TODO Auto-generated method stub
 		Map<String, Object> changedFields = rdNodeForm.changedFields();
-		int formOfWay = 1;
 		if(changedFields != null && changedFields.containsKey("formOfWay")){
-			formOfWay = (int) changedFields.get("formOfWay");
-		}
-		if(formOfWay == 15){
-			StringBuilder sb = new StringBuilder();
-			     
-			sb.append("SELECT DISTINCT F.NODE_PID FROM RD_NODE_FORM F, RD_LINK RS, RD_LINK RE");
-			sb.append(" WHERE F.NODE_PID = "+rdNodeForm.getNodePid());
-			sb.append(" AND (RS.S_NODE_PID = F.NODE_PID OR RS.E_NODE_PID = F.NODE_PID)");
-			sb.append(" AND (RE.S_NODE_PID = F.NODE_PID OR RE.E_NODE_PID = F.NODE_PID)");
-			sb.append(" AND RS.LINK_PID <> RE.LINK_PID AND F.U_RECORD <> 2 AND RS.U_RECORD <> 2");
-			sb.append(" AND RE.U_RECORD <> 2 AND RS.KIND IN (1, 2) AND RE.KIND IN (1, 2)");
-			String sql = sb.toString();
-			log.info("RdNode后检查GLM03059--sql:" + sql);
-			
-			DatabaseOperator getObj = new DatabaseOperator();
-			List<Object> resultList = new ArrayList<Object>();
-			resultList = getObj.exeSelect(this.getConn(), sql);
-
-			if(!resultList.isEmpty()){
-				String target = "[RD_NODE," + rdNodeForm.getNodePid() + "]";
-				this.setCheckResult("", target, 0);
+			int formOfWay = (int) changedFields.get("formOfWay");
+			if(formOfWay == 15){
+				StringBuilder sb = new StringBuilder();
+				
+				sb.append("SELECT DISTINCT F.NODE_PID FROM RD_NODE_FORM F, RD_LINK RS, RD_LINK RE");
+				sb.append(" WHERE F.NODE_PID = "+rdNodeForm.getNodePid());
+				sb.append(" AND (RS.S_NODE_PID = F.NODE_PID OR RS.E_NODE_PID = F.NODE_PID)");
+				sb.append(" AND (RE.S_NODE_PID = F.NODE_PID OR RE.E_NODE_PID = F.NODE_PID)");
+				sb.append(" AND RS.LINK_PID <> RE.LINK_PID AND F.U_RECORD <> 2 AND RS.U_RECORD <> 2");
+				sb.append(" AND RE.U_RECORD <> 2 AND RS.KIND IN (1, 2) AND RE.KIND IN (1, 2)");
+				String sql = sb.toString();
+				log.info("RdNode后检查GLM03059--sql:" + sql);
+				
+				DatabaseOperator getObj = new DatabaseOperator();
+				List<Object> resultList = new ArrayList<Object>();
+				resultList = getObj.exeSelect(this.getConn(), sql);
+				
+				if(!resultList.isEmpty()){
+					String target = "[RD_NODE," + rdNodeForm.getNodePid() + "]";
+					this.setCheckResult("", target, 0);
+				}
 			}
 		}
 	}

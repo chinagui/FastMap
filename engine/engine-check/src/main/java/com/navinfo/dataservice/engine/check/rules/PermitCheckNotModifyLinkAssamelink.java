ca8e65d2a6015bd8c4fe09230938a0b0dcf6785e
zhangyuntao <zhangyuntao@navinfo.com>
2017-01-19 15:01:21 +0800

    去除错误引用

--- engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/PermitCheckNotModifyLinkAssamelink.java
+++ engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/PermitCheckNotModifyLinkAssamelink.java
@@ -2,17 +2,13 @@ package com.navinfo.dataservice.engine.check.rules;
 
 import com.navinfo.dataservice.dao.check.CheckCommand;
 import com.navinfo.dataservice.dao.glm.iface.IRow;
-import com.navinfo.dataservice.dao.glm.model.ad.zone.ZoneLink;
 import com.navinfo.dataservice.dao.glm.model.ad.zone.ZoneNode;
 import com.navinfo.dataservice.dao.glm.model.rd.same.RdSameLink;
 import com.navinfo.dataservice.dao.glm.model.rd.same.RdSameLinkPart;
-import com.navinfo.dataservice.dao.glm.selector.ad.zone.ZoneLinkSelector;
 import com.navinfo.dataservice.dao.glm.selector.rd.same.RdSameLinkSelector;
 import com.navinfo.dataservice.engine.check.core.baseRule;
 
 import java.util.List;
-
-import static com.sun.tools.doclint.Entity.part;
 
 /**
  * Created by chaixin on 2017/1/19 0019.

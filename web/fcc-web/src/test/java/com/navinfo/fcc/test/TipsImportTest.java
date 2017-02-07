1f0319d977b2c00ee84bd9ba168ff8c2986dfa83
liya02922 <liya@navinfo.com>
2017-02-07 14:24:43 +0800

    FCc:tips 查询修改

--- web/fcc-web/src/test/java/com/navinfo/fcc/test/TipsImportTest.java
+++ web/fcc-web/src/test/java/com/navinfo/fcc/test/TipsImportTest.java
@@ -26,7 +26,7 @@ public class TipsImportTest extends InitApplication {
 
 	 @Test
 	public void test() {
-		String parameter = "{\"jobId\":123}";
+		String parameter = "{\"jobId\":2677}";
 		try {
 
 			JSONObject jsonReq = JSONObject.fromObject(parameter);
@@ -37,7 +37,8 @@ public class TipsImportTest extends InitApplication {
 
 			// String filePath = upload.unzipByJobId(jobId); //服务测试
 
-			String filePath = "E:\\03 ni_robot\\Nav_Robot\\10测试数据\\01上传下载\\音频测试数据\\1425"; // 本地测试用
+			//E:\03 ni_robot\Nav_Robot\10测试数据\01上传下载\音频测试数据\2677  2677道路名
+			String filePath = "E:\\03 ni_robot\\Nav_Robot\\10测试数据\\01上传下载\\音频测试数据\\2677"; // 本地测试用
 
 			// String
 			// filePath="E:\\03 ni_robot\\Nav_Robot\\10测试数据\\01上传下载\\upload\\893";

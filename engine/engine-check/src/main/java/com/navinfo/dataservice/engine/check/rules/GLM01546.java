408fd285330dfb56d6a8d180498b386af387d5b3
Zhang YT <Zhang YT>
2017-02-07 10:13:36 +0800

    修改检查项01244，01515，01546

---
+++ engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/GLM01546.java
@@ -0,0 +1,79 @@
+package com.navinfo.dataservice.engine.check.rules;
+
+import com.navinfo.dataservice.dao.check.CheckCommand;
+import com.navinfo.dataservice.dao.glm.iface.IRow;
+import com.navinfo.dataservice.dao.glm.iface.ObjStatus;
+import com.navinfo.dataservice.dao.glm.model.rd.link.RdLink;
+import com.navinfo.dataservice.dao.glm.model.rd.link.RdLinkForm;
+import com.navinfo.dataservice.engine.check.core.baseRule;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Crayeres on 2017/2/7.
+ */
+public class GLM01546 extends baseRule {
+
+    private List<Integer> pedestrianStreetLink = new ArrayList<>();
+
+    @Override
+    public void preCheck(CheckCommand checkCommand) throws Exception {
+
+    }
+
+    @Override
+    public void postCheck(CheckCommand checkCommand) throws Exception {
+        preparData(checkCommand);
+
+        for (IRow row : checkCommand.getGlmList()) {
+            if (row instanceof RdLink) {
+                RdLink link = (RdLink) row;
+                int walkFlag = link.getWalkFlag();
+                if (link.changedFields().containsKey("walkFlag"))
+                    walkFlag = (int) link.changedFields().get("walkFlag");
+
+                if (pedestrianStreetLink.contains(link.pid()) && walkFlag != 1) {
+                    setCheckResult(link.getGeometry().toString(), "[RD_LINK, " + link.pid() + "]", link.mesh());
+                }
+            }
+        }
+    }
+
+    private void preparData(CheckCommand checkCommand) {
+        for (IRow obj : checkCommand.getGlmList()) {
+            if (obj instanceof RdLink) {
+                RdLink link = (RdLink) obj;
+                if (link.status() == ObjStatus.DELETE)
+                    continue;
+
+                Map<String, Integer> formOfWays = new HashMap<>();
+                for (IRow f : link.getForms()) {
+                    RdLinkForm form = (RdLinkForm) f;
+                    formOfWays.put(form.getRowId(), form.getFormOfWay());
+                }
+
+                for (IRow row : checkCommand.getGlmList()) {
+                    if (row instanceof RdLinkForm) {
+                        RdLinkForm form = (RdLinkForm) row;
+                        if (form.getLinkPid() == link.pid()) {
+                            if (form.status() == ObjStatus.DELETE) {
+                                formOfWays.remove(form.getRowId());
+                            } else {
+                                int formOfWay = form.getFormOfWay();
+                                if (form.changedFields().containsKey("formOfWay"))
+                                    formOfWay = (int) form.changedFields().get("formOfWay");
+                                formOfWays.put(form.getRowId(), formOfWay);
+                            }
+                        }
+                    }
+                }
+
+                if (formOfWays.containsValue(20))
+                    pedestrianStreetLink.add(link.pid());
+            }
+        }
+    }
+}

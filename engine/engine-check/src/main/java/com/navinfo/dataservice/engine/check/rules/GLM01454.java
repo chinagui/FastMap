1dd49c5f6462c77217cb0a6de100208921ffb407
Zhang YT <Zhang YT>
2017-02-14 17:03:03 +0800

    修改代码注释

--- engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/GLM01454.java
+++ engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/GLM01454.java
@@ -56,21 +56,21 @@ public class GLM01454 extends baseRule {
         for (IRow n : cross.getNodes()) {
             nodes.add(((RdCrossNode) n).getNodePid());
         }
-        log.info("GLM01454:[nodes.size()=" + nodes.size() + ",nodes=" + Arrays.toString(nodes.toArray()) + "]");
+        // log.info("GLM01454:[nodes.size()=" + nodes.size() + ",nodes=" + Arrays.toString(nodes.toArray()) + "]");
 
         List<Integer> innerLinks = new ArrayList<>();
         for (IRow l : cross.getLinks()) {
             innerLinks.add(((RdCrossLink) l).getLinkPid());
         }
-        log.info("GLM01454:[innerLinks.size()=" + innerLinks.size() + ",innerLinks=" + Arrays.toString(innerLinks
-                .toArray()) + "]");
+        // log.info("GLM01454:[innerLinks.size()=" + innerLinks.size() + ",innerLinks=" + Arrays.toString(innerLinks
+        //        .toArray()) + "]");
 
         List<RdLink> links = linkSelector.loadByNodePids(nodes, false);
-        log.info("GLM01454:[links.size()=" + links.size() + "]");
+        // log.info("GLM01454:[links.size()=" + links.size() + "]");
 
         boolean flag = false;
         for (RdLink l : links) {
-            log.info("GLM01454:[linkPid=" + l.pid() + "]");
+            // log.info("GLM01454:[linkPid=" + l.pid() + "]");
             if (!innerLinks.contains(l.pid())) {
                 List<IRow> forms = new AbstractSelector(RdLinkForm.class, getConn()).loadRowsByParentId(l.pid(), false);
                 l.setForms(forms);
@@ -89,16 +89,16 @@ public class GLM01454 extends baseRule {
             }
         }
         if (!flag) {
-            log.info("GLM01454:[非交叉点内link均为区域内道路]");
+            // log.info("GLM01454:[非交叉点内link均为区域内道路]");
             for (RdLink l : links) {
                 if (innerLinks.contains(l.pid())) {
                     List<IRow> forms = new AbstractSelector(RdLinkForm.class, getConn()).loadRowsByParentId(l.pid(),
                             false);
                     flag = true;
-                    log.info("GLM01454:[forms.size()=" + forms.size() + "]");
+                    // log.info("GLM01454:[forms.size()=" + forms.size() + "]");
                     for (IRow f : forms) {
                         RdLinkForm ff = (RdLinkForm) f;
-                        log.info("GLM01454:[formOfWay=" + ff.getFormOfWay() + "]");
+                        // log.info("GLM01454:[formOfWay=" + ff.getFormOfWay() + "]");
                         if (ff.getFormOfWay() == 52) {
                             flag = false;
                             break;

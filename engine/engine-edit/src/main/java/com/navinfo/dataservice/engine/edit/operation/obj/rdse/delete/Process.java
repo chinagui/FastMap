d70ef17eccc77dec6a91d67b80cb4ccac4f0e197
hhmhsm <531476471@qq.com>
2017-02-15 20:38:36 +0800

    修改道路检查项bug

--- engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/obj/rdse/delete/Process.java
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/obj/rdse/delete/Process.java
@@ -1,5 +1,12 @@
 package com.navinfo.dataservice.engine.edit.operation.obj.rdse.delete;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.navinfo.dataservice.dao.glm.iface.IRow;
+import com.navinfo.dataservice.dao.glm.model.rd.link.RdLinkForm;
+import com.navinfo.dataservice.dao.glm.model.rd.link.RdLinkIntRtic;
+import com.navinfo.dataservice.dao.glm.model.rd.link.RdLinkRtic;
 import com.navinfo.dataservice.dao.glm.model.rd.se.RdSe;
 import com.navinfo.dataservice.dao.glm.selector.rd.se.RdSeSelector;
 import com.navinfo.dataservice.engine.edit.operation.AbstractCommand;
@@ -32,6 +39,20 @@ public class Process extends AbstractProcess<Command> {
 	@Override
 	public String exeOperation() throws Exception {
 		return new Operation(this.getCommand()).run(this.getResult());
+	}
+	
+	@Override
+	public void postCheck() throws Exception {
+		List<IRow> glmList = new ArrayList<IRow>();
+		glmList.addAll(this.getResult().getAddObjects());
+		glmList.addAll(this.getResult().getUpdateObjects());
+		for(IRow irow:this.getResult().getDelObjects()){
+			if(irow instanceof RdSe){
+				glmList.add(irow);
+			}
+		}
+		this.checkCommand.setGlmList(glmList);
+		this.checkEngine.postCheck();
 	}
 
 }

b4229abf7e905bbbc573b7b94994ae47b9e571fd
hhmhsm <531476471@qq.com>
2017-02-16 10:23:11 +0800

    修改poi日编中文地址检查项

--- engine/engine-editplus/src/main/java/com/navinfo/dataservice/engine/editplus/batchAndCheck/check/rule/FM11Win0502.java
+++ engine/engine-editplus/src/main/java/com/navinfo/dataservice/engine/editplus/batchAndCheck/check/rule/FM11Win0502.java
@@ -31,6 +31,9 @@ public class FM11Win0502 extends BasicCheckRule {
 		if(obj.objName().equals(ObjectName.IX_POI)){
 			IxPoiObj poiObj=(IxPoiObj) obj;
 			IxPoi poi=(IxPoi) poiObj.getMainrow();
+			//充电桩（230227）不参与检查
+			String kindCode = poi.getKindCode();
+			if(kindCode == null || "230227".equals(kindCode)){return;}
 			//存在IxPoiAddress
 			IxPoiAddress ixPoiAddress=poiObj.getCHAddress();
 			//错误数据
--- engine/engine-editplus/src/main/java/com/navinfo/dataservice/engine/editplus/batchAndCheck/check/rule/FMYW20062.java
+++ engine/engine-editplus/src/main/java/com/navinfo/dataservice/engine/editplus/batchAndCheck/check/rule/FMYW20062.java
@@ -30,6 +30,9 @@ public class FMYW20062 extends BasicCheckRule {
 		if(obj.objName().equals(ObjectName.IX_POI)){
 			IxPoiObj poiObj=(IxPoiObj) obj;
 			IxPoi poi=(IxPoi) poiObj.getMainrow();
+			//充电桩（230227）不参与检查
+			String kindCode = poi.getKindCode();
+			if(kindCode == null || "230227".equals(kindCode)){return;}
 			//存在IxPoiAddress修改
 			IxPoiAddress ixPoiAddress=poiObj.getCHAddress();
 			//错误数据
@@ -44,27 +47,22 @@ public class FMYW20062 extends BasicCheckRule {
 						||(fullname.contains("{")&&fullname.contains("｝"))
 						||(fullname.contains("｛")&&fullname.contains("}"))){
 					setCheckResult(poi.getGeometry(), poiObj, poi.getMeshId(), "中文地址中存在一边是半角一边是全角的括号");
-					return;
 				}
 				String errorMsg=CheckUtil.isRightKuohao(fullname,"（","）");
 				if(errorMsg!=null){
 					setCheckResult(poi.getGeometry(), poiObj, poi.getMeshId(), "中文地址中"+errorMsg);
-					return;
 				}
 				errorMsg=CheckUtil.isRightKuohao(fullname,"［","］");
 				if(errorMsg!=null){
 					setCheckResult(poi.getGeometry(), poiObj, poi.getMeshId(), "中文地址中"+errorMsg);
-					return;
 				}
 				errorMsg=CheckUtil.isRightKuohao(fullname,"｛","｝");
 				if(errorMsg!=null){
 					setCheckResult(poi.getGeometry(), poiObj, poi.getMeshId(), "中文地址中"+errorMsg);
-					return;
 				}
 				errorMsg=CheckUtil.isRightKuohao(fullname,"《","》");
 				if(errorMsg!=null){
 					setCheckResult(poi.getGeometry(), poiObj, poi.getMeshId(), "中文地址中"+errorMsg);
-					return;
 				}
 			}
 		}

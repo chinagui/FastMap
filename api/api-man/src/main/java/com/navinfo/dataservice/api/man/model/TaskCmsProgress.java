dd948255e56f78b49477233b63b4d2c0e3c8d1c9
xiaolvluo <zhangxiaoyi@navinfo.com>
2017-02-18 17:12:04 +0800

    progress

---
+++ api/api-man/src/main/java/com/navinfo/dataservice/api/man/model/TaskCmsProgress.java
@@ -0,0 +1,48 @@
+package com.navinfo.dataservice.api.man.model;
+
+import java.util.Set;
+
+public class TaskCmsProgress {
+	public int getPhaseId() {
+		return phaseId;
+	}
+	public void setPhaseId(int phaseId) {
+		this.phaseId = phaseId;
+	}
+	public Set<Integer> getGridIds() {
+		return gridIds;
+	}
+	public void setGridIds(Set<Integer> gridIds) {
+		this.gridIds = gridIds;
+	}
+	public Set<Integer> getMeshIds() {
+		return meshIds;
+	}
+	public void setMeshIds(Set<Integer> meshIds) {
+		this.meshIds = meshIds;
+	}
+	public int getTaskId() {
+		return taskId;
+	}
+	public void setTaskId(int taskId) {
+		this.taskId = taskId;
+	}
+	public int getPhase() {
+		return phase;
+	}
+	public void setPhase(int phase) {
+		this.phase = phase;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	private int taskId;
+	private int phase;
+	private int status;
+	private int phaseId;
+	private Set<Integer> gridIds;
+	private Set<Integer> meshIds;
+}

c1437ba8163db0da1bc3dc889c6b8cde7f65cfe2
Zhang YT <Zhang YT>
2017-02-20 16:34:50 +0800

    修改检查项

--- engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/ShapingCheckLinkRingobreak2.java
+++ engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/ShapingCheckLinkRingobreak2.java
@@ -1,11 +1,15 @@
 package com.navinfo.dataservice.engine.check.rules;
 
+import com.navinfo.dataservice.commons.geom.GeoTranslator;
 import com.navinfo.dataservice.dao.check.CheckCommand;
 import com.navinfo.dataservice.dao.glm.iface.IRow;
+import com.navinfo.dataservice.dao.glm.iface.ObjStatus;
 import com.navinfo.dataservice.dao.glm.iface.OperType;
 import com.navinfo.dataservice.dao.glm.model.ad.zone.ZoneLink;
 import com.navinfo.dataservice.dao.glm.model.ad.zone.ZoneNode;
 import com.navinfo.dataservice.engine.check.core.baseRule;
+import com.vividsolutions.jts.geom.Geometry;
+import net.sf.json.JSONObject;
 
 /**
  * Created by chaixin on 2017/1/19 0019.
@@ -13,21 +17,16 @@ import com.navinfo.dataservice.engine.check.core.baseRule;
 public class ShapingCheckLinkRingobreak2 extends baseRule {
     @Override
     public void preCheck(CheckCommand checkCommand) throws Exception {
-        for (IRow nodeRow : checkCommand.getGlmList()) {
-            if (nodeRow instanceof ZoneNode) {
-                ZoneNode node = (ZoneNode) nodeRow;
-                if (!node.status().equals(OperType.CREATE)) continue;
+        for (IRow row : checkCommand.getGlmList()) {
+            if (row instanceof ZoneLink && row.status() != ObjStatus.DELETE) {
+                ZoneLink link = (ZoneLink) row;
 
-                for (IRow linkRow : checkCommand.getGlmList()) {
-                    if (linkRow instanceof ZoneLink) {
-                        ZoneLink link = (ZoneLink) linkRow;
-                        if (link.status().equals(OperType.CREATE) && (link.getsNodePid() == node.pid() || link
-                                .geteNodePid() == node.pid())) {
-                            double length = link.getLength();
-                            if (length <= 2)
-                                setCheckResult("", "", 0);
-                        }
-                    }
+                Geometry geometry = GeoTranslator.transform(link.getGeometry(), 0.00001, 5);
+                if (link.changedFields().containsKey("geometry"))
+                    geometry = GeoTranslator.geojson2Jts((JSONObject) link.changedFields().get("geometry"), 0.00001, 5);
+
+                if (geometry.getLength() <= 2) {
+                    setCheckResult(link.getGeometry(), "[ZONE_LINK," + link.pid() + "]", link.mesh());
                 }
             }
         }

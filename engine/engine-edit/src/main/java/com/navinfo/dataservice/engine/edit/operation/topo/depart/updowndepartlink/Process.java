90e17db0b1385348b35704f20d49297c5be93129
Zhang YT <zhangyuntao@navinfo.com>
2017-02-22 09:44:46 +0800

    分离节点添加CRFI限制

--- engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/topo/depart/updowndepartlink/Process.java
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/topo/depart/updowndepartlink/Process.java
@@ -1,4 +1,5 @@
 package com.navinfo.dataservice.engine.edit.operation.topo.depart.updowndepartlink;
+
 import com.navinfo.dataservice.dao.glm.selector.rd.link.RdLinkSelector;
 import com.navinfo.dataservice.engine.edit.operation.AbstractCommand;
 import com.navinfo.dataservice.engine.edit.operation.AbstractProcess;
@@ -6,32 +7,34 @@ import com.navinfo.dataservice.engine.edit.operation.topo.depart.updowndepartlin
 
 /**
  * @author zhaokk
- * 制作上下线分离
+ *         制作上下线分离
  */
 
 public class Process extends AbstractProcess<Command> {
 
-	public Process(AbstractCommand command) throws Exception {
-		super(command);
-		// TODO Auto-generated constructor stub
-	}
+    public Process(AbstractCommand command) throws Exception {
+        super(command);
+        // TODO Auto-generated constructor stub
+    }
 
-	/*
-	 * 加载上下线分离线
-	 */
-	public void lockRdLinks() throws Exception {
-		RdLinkSelector linkSelector = new RdLinkSelector(this.getConn());
-		this.getCommand().setLinks(linkSelector.loadByPids(this.getCommand().getLinkPids(), true));
-	}
-	@Override
-	public boolean prepareData() throws Exception {
-		lockRdLinks();
-		return true;
-	}
+    /*
+     * 加载上下线分离线
+     */
+    public void lockRdLinks() throws Exception {
+        RdLinkSelector linkSelector = new RdLinkSelector(this.getConn());
+        this.getCommand().setLinks(linkSelector.loadByPids(this.getCommand().getLinkPids(), true));
+    }
 
-	@Override
-	public String exeOperation() throws Exception {
-		return new Operation(this.getCommand(),this.getConn()).run(this.getResult());
-	}
+    @Override
+    public boolean prepareData() throws Exception {
+        lockRdLinks();
+        new Check(getCommand(), getConn()).preCheck();
+        return true;
+    }
+
+    @Override
+    public String exeOperation() throws Exception {
+        return new Operation(this.getCommand(), this.getConn()).run(this.getResult());
+    }
 
 }

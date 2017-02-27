32b41930f24819dac87de1314107ae1453b94aeb
xiaolvluo <zhangxiaoyi@navinfo.com>
2017-02-27 11:14:25 +0800

    检查返回值修改：只返回存入数据库的错误

--- engine/engine-editplus/src/main/java/com/navinfo/dataservice/engine/editplus/batchAndCheck/check/Check.java
+++ engine/engine-editplus/src/main/java/com/navinfo/dataservice/engine/editplus/batchAndCheck/check/Check.java
@@ -9,7 +9,11 @@ import java.util.Set;
 
 import org.apache.log4j.Logger;
 
+import com.navinfo.dataservice.commons.util.StringUtils;
 import com.navinfo.dataservice.dao.check.NiValExceptionOperator;
+import com.navinfo.dataservice.dao.plus.glm.GlmFactory;
+import com.navinfo.dataservice.dao.plus.glm.GlmObject;
+import com.navinfo.dataservice.dao.plus.obj.ObjectName;
 import com.navinfo.dataservice.dao.plus.operation.AbstractCommand;
 import com.navinfo.dataservice.dao.plus.operation.AbstractOperation;
 import com.navinfo.dataservice.dao.plus.operation.OperationResult;
@@ -33,18 +37,23 @@ public class Check extends AbstractOperation{
 
 	//private static Logger log = Logger.getLogger(Check.class);
 	
-	/*
+	/**
 	 * 保存检查结果，调用此方法将检查结果插入到Ni_val_exception中
 	 */
-	public void saveCheckResult(Connection conn,List<NiValException> checkResultList) throws Exception{
+	public List<NiValException> saveCheckResult(Connection conn,List<NiValException> checkResultList) throws Exception{
 		log.debug("start call insert ni_val");
-		if (checkResultList==null || checkResultList.size()==0) {return;}		
+		List<NiValException> resultList=new ArrayList<NiValException>();
+		if (checkResultList==null || checkResultList.size()==0) {return resultList;}		
 		NiValExceptionOperator check = new NiValExceptionOperator(conn);
 		for(int i=0;i<checkResultList.size();i++){			
-			check.insertCheckLog(checkResultList.get(i).getRuleId(), checkResultList.get(i).getLoc(), 
+			boolean isError=check.insertCheckLog(checkResultList.get(i).getRuleId(), checkResultList.get(i).getLoc(), 
 					checkResultList.get(i).getTargets(), checkResultList.get(i).getMeshId(),checkResultList.get(i).getInformation(), checkResultList.get(i).getLogLevel(),"TEST");
+			if(isError){
+				resultList.add(checkResultList.get(i));
+			}
 		}
 		log.debug("end call insert ni_val");
+		return resultList;
 	}
 	@Override
 	public void operate(AbstractCommand cmd) throws Exception {
@@ -94,11 +103,14 @@ public class Check extends AbstractOperation{
 		}
 		log.info("start save checkResult");
 		//isSaveResult=true，则检查结果保存；否则不保存检查结果
+		List<NiValException> resultList=new ArrayList<NiValException>();
 		if(checkCommand.isSaveResult() &&!checkResult.isEmpty() && checkResult.size()>0){
-			saveCheckResult(conn,checkResult);
+			resultList = saveCheckResult(conn,checkResult);
+		}else {
+			resultList=checkResult;
 		}
 		log.info("end exe check");
-		setReturnExceptions(checkResult);
+		setReturnExceptions(resultList);
 	}
 
 	private CheckRule loadCheckRule(String ruleId) throws Exception {
@@ -120,14 +132,34 @@ public class Check extends AbstractOperation{
 		this.returnExceptions = returnExceptions;
 	}
 	
-	/**
-	 * 
+	/** 
+	 * targets拆分后存入list,主要用于poi精编重分类，目前仅支持poi类。
+	 * 放在check框架最外层处理，以便排除检查例外数据
+	 *
 	 * @return:
 	 * IX_POI对象的检查错误，按照pid查找规则号的集合
 	 * key:ObjectName.java中的对象名 
 	 * value：Map<Long, Set<String>> key:pid value:检查规则号集合
 	 */
 	public Map<String, Map<Long, Set<String>>> getErrorPidMap() {
+		//Map<String, Map<Long, Set<String>>> errorPidMap=new HashMap<String, Map<Long, Set<String>>>();
+		if(this.returnExceptions==null||this.returnExceptions.size()==0){return this.checkRuleCommand.getErrorPidRuleMap();}
+		GlmObject glmObject = GlmFactory.getInstance().getObjByType(ObjectName.IX_POI);
+		String mainTableName=glmObject.getMainTable().getName();
+		for(NiValException exception:this.returnExceptions){
+			String value=StringUtils.removeBlankChar(exception.getTargets());
+			if (value != null && value.length() > 2) {
+				String subValue = value.substring(1, value.length() - 1);
+				for (String table : subValue.split("\\];\\[")) {
+					String[] arr = table.split(",");
+					String tableName= arr[0];
+					String pid=arr[1];
+					if(mainTableName.equals(tableName)){
+						this.checkRuleCommand.setErrorPidRuleMap(ObjectName.IX_POI, Long.valueOf(pid), 
+								exception.getRuleId());
+					}
+				}
+			}}
 		return this.checkRuleCommand.getErrorPidRuleMap();
 	}
 

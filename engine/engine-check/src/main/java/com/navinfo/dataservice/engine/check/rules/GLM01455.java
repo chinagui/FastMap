26293a21b199b4e8f139dd09de536c8337072a46
fenghaixia04361 <fenghaixia@navinfo.com>
2017-03-02 17:34:39 +0800

    日编辑平台_道路_检查_路口_GLM01455：路口上除交叉口link外的其它挂接仅有poi连接路属性，删除交叉口poi连接路属性，应该报出检查，但是没报

--- engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/GLM01455.java
+++ engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/GLM01455.java
@@ -17,101 +17,103 @@ import com.navinfo.dataservice.engine.check.core.baseRule;
 
 /**
  * 同一路口挂接的所有道路（交叉口内LINK除外）都有POI连接路形态，路口中任意一条交叉口内LINK没有POI连接路形态时，程序报LOG
-* @ClassName: GLM01455 
-* @author Zhang Xiaolong
-* @date 2017年2月24日 下午4:12:03 
-* @Description: TODO
+ * 
+ * @ClassName: GLM01455
+ * @author Zhang Xiaolong
+ * @date 2017年2月24日 下午4:12:03
+ * @Description: TODO
  */
 public class GLM01455 extends baseRule {
-    @Override
-    public void preCheck(CheckCommand checkCommand) throws Exception {
+	@Override
+	public void preCheck(CheckCommand checkCommand) throws Exception {
 
-    }
+	}
 
-    @Override
-    public void postCheck(CheckCommand checkCommand) throws Exception {
-        for (IRow row : checkCommand.getGlmList()) {
-            if (row instanceof RdLinkForm) {
-                RdLinkForm form = (RdLinkForm) row;
+	@Override
+	public void postCheck(CheckCommand checkCommand) throws Exception {
+		for (IRow row : checkCommand.getGlmList()) {
+			if (row instanceof RdLinkForm) {
+				RdLinkForm form = (RdLinkForm) row;
 
-                RdLinkSelector linkSelector = new RdLinkSelector(getConn());
-                RdLink link = (RdLink) linkSelector.loadByIdOnlyRdLink(form.getLinkPid(), false);
+				RdLinkSelector linkSelector = new RdLinkSelector(getConn());
+				RdLink link = (RdLink) linkSelector.loadByIdOnlyRdLink(form.getLinkPid(), false);
 
-                int sNodePid = link.getsNodePid();
-                checkCrossNode(linkSelector, link, sNodePid);
+				int sNodePid = link.getsNodePid();
+				checkCrossNode(linkSelector, link, sNodePid);
 
-                int eNodePid = link.geteNodePid();
-                checkCrossNode(linkSelector, link, eNodePid);
-            }
-        }
-    }
+				int eNodePid = link.geteNodePid();
+				checkCrossNode(linkSelector, link, eNodePid);
+			}
+		}
+	}
 
-    private void checkCrossNode(RdLinkSelector linkSelector, RdLink link, int sNodePid) throws Exception {
-        RdCrossSelector crossSelector = new RdCrossSelector(getConn());
-        RdCross cross = null;
-        try {
-            cross = crossSelector.loadCrossByNodePid(sNodePid, false);
-        } catch (Exception e) {
-            return;
-        }
-        List<Integer> nodes = new ArrayList<>();
-        for (IRow n : cross.getNodes()) {
-            nodes.add(((RdCrossNode) n).getNodePid());
-        }
-        // log.info("GLM01454:[nodes.size()=" + nodes.size() + ",nodes=" + Arrays.toString(nodes.toArray()) + "]");
+	private void checkCrossNode(RdLinkSelector linkSelector, RdLink link, int sNodePid) throws Exception {
+		RdCrossSelector crossSelector = new RdCrossSelector(getConn());
+		RdCross cross = null;
+		try {
+			cross = crossSelector.loadCrossByNodePid(sNodePid, false);
+		} catch (Exception e) {
+			return;
+		}
 
-        List<Integer> innerLinks = new ArrayList<>();
-        for (IRow l : cross.getLinks()) {
-            innerLinks.add(((RdCrossLink) l).getLinkPid());
-        }
-        // log.info("GLM01454:[innerLinks.size()=" + innerLinks.size() + ",innerLinks=" + Arrays.toString(innerLinks
-        //        .toArray()) + "]");
+		// 路口Nodes
+		List<Integer> nodes = new ArrayList<>();
+		for (IRow n : cross.getNodes()) {
+			nodes.add(((RdCrossNode) n).getNodePid());
+		}
+		List<RdLink> links = linkSelector.loadByNodePids(nodes, false);
 
-        List<RdLink> links = linkSelector.loadByNodePids(nodes, false);
-        // log.info("GLM01454:[links.size()=" + links.size() + "]");
+		// 路口内Links<交叉口内link>
+		List<Integer> innerLinks = new ArrayList<>();
+		for (IRow l : cross.getLinks()) {
+			innerLinks.add(((RdCrossLink) l).getLinkPid());
+		}
 
-        boolean flag = false;
-        for (RdLink l : links) {
-            // log.info("GLM01454:[linkPid=" + l.pid() + "]");
-            if (!innerLinks.contains(l.pid())) {
-                List<IRow> forms = new AbstractSelector(RdLinkForm.class, getConn()).loadRowsByParentId(l.pid(), false);
-                l.setForms(forms);
-                boolean formFlag = false;
-                for (IRow f : forms) {
-                    RdLinkForm ff = (RdLinkForm) f;
-                    if (ff.getFormOfWay() == 36) {
-                        formFlag = true;
-                        break;
-                    }
-                }
-                if (!formFlag) {
-                    flag = true;
-                    break;
-                }
-            }
-        }
-        if (!flag) {
-            // log.info("GLM01454:[非交叉点内link均为区域内道路]");
-            for (RdLink l : links) {
-                if (innerLinks.contains(l.pid())) {
-                    List<IRow> forms = new AbstractSelector(RdLinkForm.class, getConn()).loadRowsByParentId(l.pid(),
-                            false);
-                    flag = true;
-                    // log.info("GLM01454:[forms.size()=" + forms.size() + "]");
-                    for (IRow f : forms) {
-                        RdLinkForm ff = (RdLinkForm) f;
-                        // log.info("GLM01454:[formOfWay=" + ff.getFormOfWay() + "]");
-                        if (ff.getFormOfWay() == 36) {
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if (flag) {
-                        setCheckResult(link.getGeometry(), "[RD_LINK," + link.pid() + "]", link.mesh());
-                        break;
-                    }
-                }
-            }
-        }
-    }
+		boolean flag = false;
+
+		for (RdLink l : links) {
+			if (!innerLinks.contains(l.pid())) {
+				List<IRow> forms = new AbstractSelector(RdLinkForm.class, getConn()).loadRowsByParentId(l.pid(), false);
+				l.setForms(forms);
+
+				boolean formFlag = false;
+				for (IRow f : forms) {
+					RdLinkForm ff = (RdLinkForm) f;
+					if (ff.getFormOfWay() == 36) {
+						formFlag = true;
+						break;
+					}
+				}
+				if (!formFlag) {
+					flag = true;
+					break;
+				}
+			}
+		}
+
+		// 除交叉口内link外，其余link均为poi连接路
+		if (!flag) {
+			for (RdLink innerLink : links) {
+				if (innerLinks.contains(innerLink.pid())) {
+					List<IRow> forms = new AbstractSelector(RdLinkForm.class, getConn()).loadRowsByParentId(innerLink.pid(), false);
+					flag = true;
+
+					for (IRow linkform : forms) {
+						RdLinkForm ff = (RdLinkForm) linkform;
+
+						if (ff.getFormOfWay() == 36) {
+							flag = false;
+							break;
+						}
+					}
+					if (flag) {
+						setCheckResult(innerLink.getGeometry(), "[RD_LINK," + innerLink.pid() + "]",
+
+								innerLink.mesh());
+						break;
+					}
+				}
+			}
+		}
+	}
 }

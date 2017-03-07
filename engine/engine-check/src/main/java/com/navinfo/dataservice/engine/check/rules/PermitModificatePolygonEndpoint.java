2616280002bd8c3711baab6bac9a2f9528bf55c6
Zhang YT <zhangyuntao@navinfo.com>
2017-03-07 10:18:37 +0800

    维护内部生成线属性

--- engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/PermitModificatePolygonEndpoint.java
+++ engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/PermitModificatePolygonEndpoint.java
@@ -3,6 +3,7 @@ package com.navinfo.dataservice.engine.check.rules;
 import com.navinfo.dataservice.dao.check.CheckCommand;
 import com.navinfo.dataservice.dao.glm.iface.IRow;
 import com.navinfo.dataservice.dao.glm.model.ad.zone.ZoneFace;
+import com.navinfo.dataservice.dao.glm.model.ad.zone.ZoneLink;
 import com.navinfo.dataservice.dao.glm.model.ad.zone.ZoneNode;
 import com.navinfo.dataservice.dao.glm.selector.ad.zone.ZoneFaceSelector;
 import com.navinfo.dataservice.engine.check.core.baseRule;
@@ -10,20 +11,20 @@ import com.navinfo.dataservice.engine.check.core.baseRule;
 import java.util.List;
 
 /**
- * Created by chaixin on 2017/1/19 0019.
+ * Created by Crayeres on 2017/1/19 0019.
  */
 public class PermitModificatePolygonEndpoint extends baseRule {
     @Override
     public void preCheck(CheckCommand checkCommand) throws Exception {
-        for (IRow nodeRow : checkCommand.getGlmList()) {
-            if (nodeRow instanceof ZoneNode) {
-                ZoneNode node = (ZoneNode) nodeRow;
-                if (!node.changedFields().containsKey("geometry")) continue;
-
-                ZoneFaceSelector selector = new ZoneFaceSelector(getConn());
-                List<ZoneFace> faces = selector.loadZoneFaceByNodeId(node.pid(), false);
-                if (faces.size() > 0)
-                    setCheckResult("", "", 0);
+        for (IRow row : checkCommand.getGlmList()) {
+            if (row instanceof ZoneLink) {
+                ZoneLink link = (ZoneLink) row;
+                if (link.changedFields().containsKey("sNodePid") || link.changedFields().containsKey("eNodePid")) {
+                    ZoneFaceSelector selector = new ZoneFaceSelector(getConn());
+                    List<ZoneFace> faces = selector.loadZoneFaceByLinkId(link.pid(), false);
+                    if (faces.size() > 0)
+                        setCheckResult("", "", 0);
+                }
             }
         }
     }

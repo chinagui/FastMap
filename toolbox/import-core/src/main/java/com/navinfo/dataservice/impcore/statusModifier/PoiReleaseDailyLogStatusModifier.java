582ed7e6cf4542b1d1626f8f2fd1dfa77f006627
gaopengrong@navinfo.com <gaopengrong@navinfo.com>
2017-03-16 21:14:10 +0800

    修改日出品完成后，未修改履历状态问题；

--- toolbox/import-core/src/main/java/com/navinfo/dataservice/impcore/statusModifier/PoiReleaseDailyLogStatusModifier.java
+++ toolbox/import-core/src/main/java/com/navinfo/dataservice/impcore/statusModifier/PoiReleaseDailyLogStatusModifier.java
@@ -9,16 +9,16 @@ import com.navinfo.dataservice.commons.database.OracleSchema;
  */
 public class PoiReleaseDailyLogStatusModifier extends LogStatusModifier {
 
-	private String tempTable=null;
+	private String tempTablePoi=null;
 
 	public PoiReleaseDailyLogStatusModifier(OracleSchema logSchema,String tempTable) {
 		super(logSchema);
-		this.tempTable = tempTable;
+		this.tempTablePoi = tempTable;
 	}
 
 	@Override
 	protected String getStatusModSql() {
-		return "update LOG_DAY_RELEASE set rel_poi_dt = sysdate,rel_poi_sta=1,REL_POI_LOCK=0 where OP_ID IN (SELECT OP_ID FROM "+tempTable+")";
+		return "update LOG_DAY_RELEASE set rel_poi_dt = sysdate,rel_poi_sta=1,REL_POI_LOCK=0 where OP_ID IN (SELECT OP_ID FROM "+tempTablePoi+")";
 	}
 
 }

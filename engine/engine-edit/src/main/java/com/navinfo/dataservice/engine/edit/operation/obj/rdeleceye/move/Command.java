0ac641ba253abdc15bceddaeae7ec74bd52dff9a
ZhangYt <zhangyuntao@navinfo.com>
2017-03-23 14:41:15 +0800

    电子眼移动功能增加参数 web重构

--- engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/obj/rdeleceye/move/Command.java
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/obj/rdeleceye/move/Command.java
@@ -10,86 +10,92 @@ import com.navinfo.dataservice.engine.edit.operation.AbstractCommand;
 import net.sf.json.JSONObject;
 
 /**
+ * @author zhangyt
  * @Title: Command.java
  * @Description: TODO
- * @author zhangyt
  * @date: 2016年7月29日 下午3:39:08
  * @version: v1.0
  */
 public class Command extends AbstractCommand {
 
-	private String requester;
+    private String requester;
 
-	private RdElectroniceye eleceye;
+    private RdElectroniceye eleceye;
 
-	private int pid;
+    private int pid;
 
-	private RdLink link;
+    private RdLink link;
 
-	private JSONObject content = new JSONObject();
+    private JSONObject content = new JSONObject();
 
-	public RdElectroniceye getEleceye() {
-		return eleceye;
-	}
+    public RdElectroniceye getEleceye() {
+        return eleceye;
+    }
 
-	public void setEleceye(RdElectroniceye eleceye) {
-		this.eleceye = eleceye;
-	}
+    public void setEleceye(RdElectroniceye eleceye) {
+        this.eleceye = eleceye;
+    }
 
-	public int getPid() {
-		return pid;
-	}
+    public int getPid() {
+        return pid;
+    }
 
-	public void setPid(int pid) {
-		this.pid = pid;
-	}
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
 
-	public JSONObject getContent() {
-		return content;
-	}
+    public JSONObject getContent() {
+        return content;
+    }
 
-	public void setContent(JSONObject content) {
-		this.content = content;
-	}
+    public void setContent(JSONObject content) {
+        this.content = content;
+    }
 
-	public RdLink getLink() {
-		return link;
-	}
+    public RdLink getLink() {
+        return link;
+    }
 
-	public void setLink(RdLink link) {
-		this.link = link;
-	}
+    public void setLink(RdLink link) {
+        this.link = link;
+    }
 
-	public Command(JSONObject json, String requester) {
-		this.requester = requester;
-		this.setDbId(json.getInt("dbId"));
+    public Command(JSONObject json, String requester) {
+        this.requester = requester;
+        this.setDbId(json.getInt("dbId"));
 
-		JSONObject data = json.getJSONObject("data");
-		this.pid = data.getInt("pid");
-		int linkPid = data.getInt("linkPid");
-		double latitude = data.getDouble("latitude");
-		double longitude = data.getDouble("longitude");
+        JSONObject data = json.getJSONObject("data");
+        this.pid = data.getInt("pid");
+        int linkPid = data.getInt("linkPid");
+        double latitude = data.getDouble("latitude");
+        double longitude = data.getDouble("longitude");
+        int direct = -1;
+        if (data.containsKey("direct"))
+            direct = Integer.valueOf(data.get("direct").toString());
 
-		JSONObject geoPoint = new JSONObject();
-		geoPoint.put("type", "Point");
-		geoPoint.put("coordinates", new double[] { longitude, latitude });
-		content.put("geometry", geoPoint);
-		content.put("linkPid", linkPid);
-	}
+        JSONObject geoPoint = new JSONObject();
+        geoPoint.put("type", "Point");
+        geoPoint.put("coordinates", new double[]{longitude, latitude});
+        content.put("geometry", geoPoint);
+        content.put("linkPid", linkPid);
+        if (-1 != direct) {
+            content.put("direct", direct);
+        }
+    }
 
-	@Override
-	public OperType getOperType() {
-		return OperType.MOVE;
-	}
+    @Override
+    public OperType getOperType() {
+        return OperType.MOVE;
+    }
 
-	@Override
-	public String getRequester() {
-		return this.requester;
-	}
+    @Override
+    public String getRequester() {
+        return this.requester;
+    }
 
-	@Override
-	public ObjType getObjType() {
-		return ObjType.RDELECTRONICEYE;
-	}
+    @Override
+    public ObjType getObjType() {
+        return ObjType.RDELECTRONICEYE;
+    }
 
 }

76803326f508b32f28c4133fd919348dfbc299df
zhaokaikai <zhaokaikai@navinfo.com>
2017-04-17 18:10:03 +0800

    修改传值taskId

--- engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/service/EditApiImpl.java
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/service/EditApiImpl.java
@@ -27,16 +27,26 @@ import net.sf.json.JSONObject;
 @Service("editApi")
 public class EditApiImpl implements EditApi {
 	private Connection conn;
-	
+
 	private long token;
-	
-	public EditApiImpl(){
+
+	private int subtaskId;
+
+	public int getSubtaskId() {
+		return subtaskId;
 	}
-	
-	public EditApiImpl(Connection conn){
+
+	public void setSubtaskId(int subtaskId) {
+		this.subtaskId = subtaskId;
+	}
+
+	public EditApiImpl() {
+	}
+
+	public EditApiImpl(Connection conn) {
 		this.conn = conn;
 	}
-	
+
 	public long getToken() {
 		return token;
 	}
@@ -48,8 +58,9 @@ public class EditApiImpl implements EditApi {
 	@Override
 	public JSONObject run(JSONObject dataObj) throws Exception {
 		Transaction t = new Transaction(dataObj.toString());
-		
+
 		t.setUserId(token);
+		t.setSubTaskId(subtaskId);
 
 		String msg = t.run();
 
@@ -67,11 +78,12 @@ public class EditApiImpl implements EditApi {
 
 		return json;
 	}
-	
+
 	public JSONObject runPoi(JSONObject dataObj) throws Exception {
-		Transaction t = new Transaction(dataObj.toString(),conn);
-		
+		Transaction t = new Transaction(dataObj.toString(), conn);
+
 		t.setUserId(this.token);
+		t.setSubTaskId(subtaskId);
 
 		String msg = t.innerRun();
 
@@ -92,45 +104,51 @@ public class EditApiImpl implements EditApi {
 
 	/**
 	 * @Title: updatePoifreshVerified
-	 * @Description: 修改的数据，安卓上传：1)若只有照片和备注的履历，则为鲜度验证,更新为待作业;2)若没任何修改履历，也为鲜度验证，更新为已作业
-	 * web:不修改状态，只做鲜度验证字段的判断修改，web保存的数据总在已作业
-	 * (修)(第七迭代) 变更:当新增 poi_edit_status 时,为 commit_his_status 字段赋默认值 0 
+	 * @Description: 
+	 *               修改的数据，安卓上传：1)若只有照片和备注的履历，则为鲜度验证,更新为待作业;2)若没任何修改履历，也为鲜度验证，更新为已作业
+	 *               web:不修改状态，只做鲜度验证字段的判断修改，web保存的数据总在已作业 (修)(第七迭代) 变更:当新增
+	 *               poi_edit_status 时,为 commit_his_status 字段赋默认值 0
 	 * @param pid
 	 * @param platform
-	 * @throws Exception  void
-	 * @throws 
+	 * @throws Exception
+	 *             void
+	 * @throws
 	 * @author zl zhangli5174@navinfo.com
-	 * @date 2016年12月8日 下午1:52:16 
+	 * @date 2016年12月8日 下午1:52:16
 	 */
-	public void updatePoifreshVerified(int pid,String platform) throws Exception {
-			LogReader lr=new LogReader(conn);
-			int freshVerified=0;
-			int status=1;
-			if(!lr.isExistObjHis(pid)){
-				freshVerified=1;
-				status=2;
-			}
-			if(lr.isExistObjHis(pid) && lr.isOnlyPhotoAndMetoHis(pid)){
-				freshVerified=1;
-			}
-			String sql=null;
-			if ("web".endsWith(platform)){
-				sql="UPDATE poi_edit_status T1 SET T1.fresh_verified = :1 where T1.pid =" + pid ;
-			}else{
-				sql="UPDATE poi_edit_status T1 SET T1.fresh_verified = :1,T1.status="+status+" where T1.pid = " + pid;
-			}
-			PreparedStatement pstmt = null;
-			try {
-				pstmt = conn.prepareStatement(sql);
-				pstmt.setInt(1, freshVerified);
-				pstmt.executeUpdate();
-			} catch (Exception e) {
-				throw e;
-
-			} finally {
-				DBUtils.closeStatement(pstmt);
-			}
+	public void updatePoifreshVerified(int pid, String platform)
+			throws Exception {
+		LogReader lr = new LogReader(conn);
+		int freshVerified = 0;
+		int status = 1;
+		if (!lr.isExistObjHis(pid)) {
+			freshVerified = 1;
+			status = 2;
 		}
+		if (lr.isExistObjHis(pid) && lr.isOnlyPhotoAndMetoHis(pid)) {
+			freshVerified = 1;
+		}
+		String sql = null;
+		if ("web".endsWith(platform)) {
+			sql = "UPDATE poi_edit_status T1 SET T1.fresh_verified = :1 where T1.pid ="
+					+ pid;
+		} else {
+			sql = "UPDATE poi_edit_status T1 SET T1.fresh_verified = :1,T1.status="
+					+ status + " where T1.pid = " + pid;
+		}
+		PreparedStatement pstmt = null;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, freshVerified);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			throw e;
+
+		} finally {
+			DBUtils.closeStatement(pstmt);
+		}
+	}
+
 	@Override
 	public long applyPid(String tableName, int count) throws Exception {
 		return PidService.getInstance().applyPid(tableName, count);
@@ -138,13 +156,14 @@ public class EditApiImpl implements EditApi {
 
 	@Override
 	public void runBatch(JSONObject dataObj) throws Exception {
-		BatchProcess batchProcess = new BatchProcess("row","save");
+		BatchProcess batchProcess = new BatchProcess("row", "save");
 		Connection subConn = null;
 		int dbId = dataObj.getInt("dbId");
 		try {
 			subConn = DBConnector.getInstance().getConnectionById(dbId);
 			List<String> batchList = batchProcess.getRowRules();
-			batchProcess.execute(dataObj, subConn, new EditApiImpl(subConn), batchList);
+			batchProcess.execute(dataObj, subConn, new EditApiImpl(subConn),
+					batchList);
 			subConn.commit();
 		} catch (Exception e) {
 			throw e;
@@ -153,11 +172,15 @@ public class EditApiImpl implements EditApi {
 		}
 	}
 
-	/* (non-Javadoc)
-	 * @see com.navinfo.dataservice.api.edit.iface.EditApi#getGridIdListBySubtaskIdFromLog(java.lang.Integer, java.lang.Integer)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.navinfo.dataservice.api.edit.iface.EditApi#
+	 * getGridIdListBySubtaskIdFromLog(java.lang.Integer, java.lang.Integer)
 	 */
 	@Override
-	public List<Integer> getGridIdListBySubtaskIdFromLog(Integer dbId, Integer subtaskId) {
+	public List<Integer> getGridIdListBySubtaskIdFromLog(Integer dbId,
+			Integer subtaskId) {
 		// TODO Auto-generated method stub
 		return null;
 	}

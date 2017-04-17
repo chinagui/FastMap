e8c8f12479d1373a0d59766133e658e33b38e58b
ZhangYt <zhangyuntao@navinfo.com>
2017-04-17 15:39:04 +0800

    fix cmg face create bug

--- engine/engine-edit/src/test/java/com/navinfo/dataservice/engine/edit/zhangyuntao/cmg/CmgBuildfaceTest.java
+++ engine/engine-edit/src/test/java/com/navinfo/dataservice/engine/edit/zhangyuntao/cmg/CmgBuildfaceTest.java
@@ -23,8 +23,23 @@ public class CmgBuildfaceTest extends InitApplication {
     }
 
     @Test
+    public void testCreateCmgBuildface(){
+        String requester = "{\"command\":\"CREATE\",\"type\":\"CMGBUILDFACE\",\"dbId\":13," +
+                "\"data\":{\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[116.5281629562378,39.74002111583855]," +
+                "[116.52935385704039,39.740252114686754],[116.52893543243408,39.7395508657908],[116.52809858322144,39.739616865991074]," +
+                "[116.5281629562378,39.74002111583855]]}}}";
+        TestUtil.run(requester);
+    }
+
+    @Test
     public void testRender() throws Exception {
         CmgBuildfaceSearch search = new CmgBuildfaceSearch(DBConnector.getInstance().getConnectionById(13));
         search.searchDataByTileWithGap(107963, 49743, 17, 10);
+    }
+
+    @Test
+    public void testGetByPid() throws Exception {
+        CmgBuildfaceSearch search = new CmgBuildfaceSearch(DBConnector.getInstance().getConnectionById(13));
+        System.out.println(search.searchDataByPid(508000002));
     }
 }

1cf0acc184f3660ea3da3faad69f6cbc58de66e0
Xiaowen-Navinfo <xiaoxiaowen4127@navinfo.com>
2017-04-20 20:18:54 +0800

    poi upload

--- edit-control/edit-control-app/src/test/java/com/navinfo/dataservice/control/app/UploadTest.java
+++ edit-control/edit-control-app/src/test/java/com/navinfo/dataservice/control/app/UploadTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.navinfo.dataservice.commons.springmvc.ApplicationContextUtil;
 import com.navinfo.dataservice.control.app.upload.UploadOperation;
+import com.navinfo.dataservice.engine.editplus.operation.imp.UploadOperationByGather;
+
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
@@ -29,6 +31,17 @@ public class UploadTest {
 			Date endTime = new Date();
 			System.out.println("total time:"+ (endTime.getTime() - startTime.getTime()));
 //			System.out.println(UuidUtils.genUuid());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	@Test
+	public void testUpload() {
+		try {
+			UploadOperationByGather op = new UploadOperationByGather(4127L,24);
+			JSONObject retArray = op.importPoi("F:\\data\\collector\\poi.txt");
+			System.out.println(retArray.toString());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}

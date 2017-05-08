f80283408bab3ede50a89a322746a15ec8ae969f
songhe <songhe@navinfo.com>
2017-05-08 11:29:36 +0800

    修改了编码集。

--- web/man-web/src/main/java/com/navinfo/dataservice/web/man/controller/CityController.java
+++ web/man-web/src/main/java/com/navinfo/dataservice/web/man/controller/CityController.java
@@ -105,9 +105,9 @@ public class CityController extends BaseController {
 	@RequestMapping(value = "/city/listAll")
 	public ModelAndView queryListAll(HttpServletRequest request){
 		try{
-//			JSONObject dataJson = JSONObject.fromObject(URLDecode(request.getParameter("parameter")));
+			JSONObject dataJson = JSONObject.fromObject(URLDecode(request.getParameter("parameter")));
 			//TODO 这里编码集需要确认
-			JSONObject dataJson = JSONObject.fromObject(new String(request.getParameter("parameter").getBytes("iso8859-1"),"utf-8"));
+//			JSONObject dataJson = JSONObject.fromObject(new String(request.getParameter("parameter").getBytes("iso8859-1"),"utf-8"));
 			String cityName = "";
 			if(dataJson.containsKey("condition")){
 				JSONObject cityPrame = JSONObject.fromObject(dataJson.getString("condition"));

77e182025dc65a9cc6d3b98817ac8496dbf373dd
zhangli5174 <zhangli5174@navinfo.com>
2017-05-16 11:35:48 +0800

    提交关于web端道路名查询的修改

--- engine/engine-meta/src/test/java/org/navinfo/dataservice/engine/meta/RdNameSelectorTest.java
+++ engine/engine-meta/src/test/java/org/navinfo/dataservice/engine/meta/RdNameSelectorTest.java
@@ -77,11 +77,52 @@ public class RdNameSelectorTest {
 		}
 	}
 	
+	@Test
+	public void testSearchRdName()
+	{
+		String parameter = "{'subtaskId':76,'pageNum':1,'pageSize':20,'flag':1,'sortby':'-name','params':{'name':'','nameGroupid':'','adminId':''}}";//
+
+		try {
+			JSONObject jsonReq = JSONObject.fromObject(parameter);
+
+			RdNameSelector selector = new RdNameSelector();
+			
+			int subtaskId = jsonReq.getInt("subtaskId");
+			
+			/*ManApi apiService=(ManApi) ApplicationContextUtil.getBean("manApi");
+			
+			Subtask subtask = apiService.queryBySubtaskId(subtaskId);*/
+			
+			/*FccApi apiFcc=(FccApi) ApplicationContextUtil.getBean("fccApi");
+			
+			JSONArray tips = apiFcc.searchDataBySpatial(subtask.getGeometry(),1901,new JSONArray());
+			*/
+			String tipsStr = "[{'id':'021901d59b5e2f89c443a08f5a68690edf60c5','wkt':'POINT (116.38779 39.98016)','stage':1,'t_operateDate':'20170107153440','t_date':'20170302160111','t_lifecycle':3,'t_command':0,'t_cStatus':1,'t_dStatus':0,'t_inMeth':0,'t_pStatus':0,'t_dInProc':0,'t_mInProc':0,'handler':3662,'t_mStatus':0,'s_sourceCode':2,'s_sourceType':'1901','g_location':{'type':'LineString','coordinates':[[116.3252,39.8014],[116.32619,39.80295]]},'g_guide':{'type':'Point','coordinates':[116.32569,39.80218]},'deep':{'geo':{'coordinates':[116.32569,39.80218],'type':'Point'},'n_array':['左']},'feedback':{'f_array':[]},'s_reliability':100,'tipdiff':{},'s_qTaskId':0,'s_mTaskId':0,'_version_':1560744057518424064,'t':1}]";
+			
+			
+			JSONArray tips = JSONArray.fromObject(tipsStr);
+					//new JSONArray();
+			/*JSONObject jobj = new JSONObject();
+					jobj.put("id", "02190105732d7d9fb74c6e9c66c8269b19d371");
+			JSONObject jobj2 = new JSONObject();
+					jobj2.put("id", "0219010714071FD07048E191F7803BE65EA02B");
+					tips.add(jobj);
+					tips.add(jobj2);*/
+			
+			JSONObject data = selector.searchForWeb(jsonReq, tips);//selector.searchForWeb(jsonReq,tips);
+			
+			System.out.println(data);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 //	@Test
 	public void testGetRdNameAll()
 	{
 		//{"pageNum":1,"pageSize":20,"sortby":"","flag":0,"params":{"name":"","nameGroupid":"","adminId":"","roadTypes":[]}}
-		String parameter = "{'pageNum':1,'pageSize':20,'flag':0,'sortby':'-name','params':{'name':'定武高速','nameGroupid':'','adminId':214,'roadTypes':[1]}}";//
+		String parameter = "{'pageNum':1,'pageSize':20,'flag':0,'sortby':'','params':{'name':'定武高速','nameGroupid':'','adminId':214,'roadTypes':[1]}}";//
 
 		try {
 			JSONObject jsonReq = JSONObject.fromObject(parameter);
@@ -152,7 +193,7 @@ public class RdNameSelectorTest {
 		}
 	}
 	
-	@Test
+//	@Test
 	public void searchRdNameFix(){
 		RdNameSelector a = new RdNameSelector();
 		/*JSONObject jsonReq = JSONObject.fromObject("{'data':{'options':{},'geoLiveType':'ROADNAME','pid':null,'nameId':206000028,'nameGroupid':208000027,'langCode':'CHI','name':'巴拿马高速公路','type':'','base':'','prefix':'','infix':'','suffix':'','namePhonetic':'','typePhonetic':'','basePhonetic':'','prefixPhonetic':'','infixPhonetic':'','suffixPhonetic':'','srcFlag':0,'roadType':3,'adminId':214,'codeType':0,'voiceFile':'','srcResume':'','paRegionId':null,'splitFlag':0,'memo':'','routeId':0,'uRecord':null,'uFields':'','city':'','adminName':'全国','rowId':null,'_originalJson':{'nameId':null,'nameGroupid':null,'langCode':'ENG','name':'','type':'','base':'','prefix':'','infix':'','suffix':'','namePhonetic':'','typePhonetic':'','basePhonetic':'','prefixPhonetic':'','infixPhonetic':'','suffixPhonetic':'','srcFlag':0,'roadType':3,'adminId':120000,'codeType':0,'voiceFile':'','srcResume':'','paRegionId':null,'splitFlag':0,'memo':'','routeId':0,'uRecord':null,'uFields':'','city':'','adminName':'','rowId':null},'_initHooksCalled':true},'dbId':243,'subtaskId':76}");

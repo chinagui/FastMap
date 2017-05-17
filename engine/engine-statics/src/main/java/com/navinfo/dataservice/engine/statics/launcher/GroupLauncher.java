b4d9c0fec64ade6ad63a5bcc6d91f5c7d41b3db4
Xiaowen-Navinfo <xiaoxiaowen4127@navinfo.com>
2017-05-17 19:26:15 +0800

    poi upload

---
+++ engine/engine-statics/src/main/java/com/navinfo/dataservice/engine/statics/launcher/GroupLauncher.java
@@ -0,0 +1,13 @@
+package com.navinfo.dataservice.engine.statics.launcher;
+
+import java.util.Observable;
+
+/** 
+ * @ClassName: GroupLauncher
+ * @author xiaoxiaowen4127
+ * @date 2017年5月17日
+ * @Description: GroupLauncher.java
+ */
+public class GroupLauncher  extends Observable{
+	
+}

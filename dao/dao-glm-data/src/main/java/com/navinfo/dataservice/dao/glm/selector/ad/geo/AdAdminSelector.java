dc272d0e545227e310e4d4f895f92743eb7448b8
Zhang Yt <zhangyuntao@navinfo.com>
2017-06-07 10:51:35 +0800

    fix bug of admin selector

--- dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/selector/ad/geo/AdAdminSelector.java
+++ dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/glm/selector/ad/geo/AdAdminSelector.java
@@ -1,20 +1,20 @@
 package com.navinfo.dataservice.dao.glm.selector.ad.geo;
 
+import com.navinfo.dataservice.dao.glm.iface.IRow;
+import com.navinfo.dataservice.dao.glm.model.ad.geo.AdAdmin;
+import com.navinfo.dataservice.dao.glm.model.ad.geo.AdAdminName;
+import com.navinfo.dataservice.dao.glm.selector.AbstractSelector;
+import com.navinfo.dataservice.dao.glm.selector.ReflectionAttrUtils;
+import com.navinfo.navicommons.database.sql.DBUtils;
+import com.navinfo.navicommons.exception.DAOException;
+import org.apache.commons.lang.StringUtils;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-
-import com.navinfo.dataservice.commons.exception.DataNotFoundException;
-import com.navinfo.dataservice.dao.glm.iface.IRow;
-import com.navinfo.dataservice.dao.glm.model.ad.geo.AdAdmin;
-import com.navinfo.dataservice.dao.glm.model.ad.geo.AdAdminName;
-
-import com.navinfo.dataservice.dao.glm.selector.AbstractSelector;
-import com.navinfo.dataservice.dao.glm.selector.ReflectionAttrUtils;
-import com.navinfo.navicommons.database.sql.DBUtils;
 
 public class AdAdminSelector extends AbstractSelector {
 
@@ -26,46 +26,57 @@ public class AdAdminSelector extends AbstractSelector {
 		this.setCls(AdAdmin.class);
 	}
 
-	public AdAdmin loadByAdminId(int adadminId, boolean isLock)
-			throws Exception {
+	public AdAdmin loadByAdminId(int adadminId, boolean isLock) throws DAOException {
 		AdAdmin adAdmin = new AdAdmin();
-
-		String sql = "select * from " + adAdmin.tableName()
-				+ " where admin_id =:1 and  u_record !=2";
-
+		String sql = "select * from " + adAdmin.tableName() + " where admin_id =:1 and  u_record !=2";
+        if (isLock) {
+            sql += " for update nowait";
+        }
 		PreparedStatement pstmt = null;
-
 		ResultSet resultSet = null;
-
 		try {
 			pstmt = conn.prepareStatement(sql);
-
 			pstmt.setInt(1, adadminId);
-
 			resultSet = pstmt.executeQuery();
-
 			if (resultSet.next()) {
-
-				adAdmin.setPid(resultSet.getInt("region_id"));
-
-				adAdmin.setAdminId(resultSet.getInt("admin_id"));
-
-				adAdmin.setRowId(resultSet.getString("row_id"));
-
+				ReflectionAttrUtils.executeResultSet(adAdmin, resultSet);
 			} else {
-				throw new DataNotFoundException("数据不存在");
+				throw new DAOException("数据不存在");
 			}
 		} catch (Exception e) {
-
-			throw e;
-
+			throw new DAOException(e.getMessage());
 		} finally {
 			DBUtils.closeResultSet(resultSet);
 			DBUtils.closeStatement(pstmt);
 		}
-
 		return adAdmin;
 	}
+
+	public AdAdmin loadAdAdminRefAdminType(int adminId, int adminType, boolean isLock) throws DAOException{
+	    AdAdmin adAdmin = null;
+        String sql = "SELECT * FROM AD_ADMIN WHERE ADMIN_ID = :1 AND ADMIN_TYPE < :2 AND U_RECORD <> 2";
+        if (isLock) {
+            sql += "FOR UPDATE NOWAIT";
+        }
+        PreparedStatement pstmt = null;
+        ResultSet resultSet = null;
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, adminId);
+            pstmt.setInt(2, adminType);
+            resultSet = pstmt.executeQuery();
+            if (resultSet.next()) {
+                adAdmin = new AdAdmin();
+                ReflectionAttrUtils.executeResultSet(adAdmin, resultSet);
+            }
+        } catch (Exception e) {
+            throw new DAOException(e.getMessage());
+        } finally {
+            DBUtils.closeResultSet(resultSet);
+            DBUtils.closeStatement(pstmt);
+        }
+        return adAdmin;
+    }
 
 	/**
 	 * 根据引导LinkPid查询行政区划代表点
@@ -75,39 +86,26 @@ public class AdAdminSelector extends AbstractSelector {
 	 * @return
 	 * @throws Exception
 	 */
-	public List<AdAdmin> loadRowsByLinkId(int id, boolean isLock)
-			throws Exception {
-
+	public List<AdAdmin> loadRowsByLinkId(int id, boolean isLock) throws DAOException {
 		List<AdAdmin> adAdminList = new ArrayList<AdAdmin>();
-
 		String sql = "SELECT * FROM ad_admin WHERE link_pid = :1 and u_record!=2";
-
 		if (isLock) {
 			sql += " for update nowait";
 		}
-
 		PreparedStatement pstmt = null;
-
 		ResultSet resultSet = null;
-
 		try {
 			pstmt = conn.prepareStatement(sql);
-
 			pstmt.setInt(1, id);
-
 			resultSet = pstmt.executeQuery();
-
 			while (resultSet.next()) {
 				AdAdmin adAdmin = new AdAdmin();
-
 				ReflectionAttrUtils.executeResultSet(adAdmin, resultSet);
 				this.setChildData(adAdmin, isLock);
 				adAdminList.add(adAdmin);
 			}
 		} catch (Exception e) {
-
-			throw e;
-
+			throw new DAOException(e.getMessage());
 		} finally {
 			DBUtils.closeResultSet(resultSet);
 			DBUtils.closeStatement(pstmt);
@@ -123,73 +121,55 @@ public class AdAdminSelector extends AbstractSelector {
 	 * @return 代表点对象集合
 	 * @throws Exception
 	 */
-	public List<AdAdmin> loadRowsByLinkPids(List<Integer> linkPids,
-			boolean isLock) throws Exception {
-		List<AdAdmin> adAdminList = new ArrayList<AdAdmin>();
-
+	public List<AdAdmin> loadRowsByLinkPids(List<Integer> linkPids, boolean isLock) throws DAOException {
+		List<AdAdmin> adAdminList = new ArrayList<>();
 		if (linkPids.size() == 0) {
 			return adAdminList;
 		}
-
 		// 去重操作
-		HashSet<Integer> linkPidsSet = new HashSet<Integer>(linkPids);
+		HashSet<Integer> linkPidsSet = new HashSet<>(linkPids);
+		//StringBuffer s = new StringBuffer("");
+		//for (Integer pid : linkPidsSet) {
+		//	s.append(pid + ",");
+		//}
+		//s.deleteCharAt(s.lastIndexOf(","));
 
-		StringBuffer s = new StringBuffer("");
-		for (Integer pid : linkPidsSet) {
-			s.append(pid + ",");
-		}
-		s.deleteCharAt(s.lastIndexOf(","));
-
-		String sql = "SELECT * FROM ad_admin WHERE link_pid in ("
-				+ s.toString() + ") and u_record!=2";
-
+		String sql = "SELECT * FROM ad_admin WHERE link_pid in (" + StringUtils.join(linkPidsSet, ",") + ") and u_record!=2";
 		if (isLock) {
 			sql += " for update nowait";
 		}
 
 		PreparedStatement pstmt = null;
-
 		ResultSet resultSet = null;
-
 		try {
 			pstmt = conn.prepareStatement(sql);
-
 			resultSet = pstmt.executeQuery();
-
 			while (resultSet.next()) {
 				AdAdmin adAdmin = new AdAdmin();
-
 				ReflectionAttrUtils.executeResultSet(adAdmin, resultSet);
 				this.setChildData(adAdmin, isLock);
 				adAdminList.add(adAdmin);
 			}
 		} catch (Exception e) {
-
-			throw e;
-
+			throw new DAOException(e.getMessage());
 		} finally {
 			DBUtils.closeResultSet(resultSet);
 			DBUtils.closeStatement(pstmt);
-
 		}
 		return adAdminList;
-
 	}
 
 	private void setChildData(AdAdmin adAdmin, boolean isLock) throws Exception {
 		// ad_admin_name
-		List<IRow> adAdminNameList = new AdAdminNameSelector(conn)
-				.loadRowsByParentId(adAdmin.getPid(), isLock);
+		List<IRow> adAdminNameList = new AdAdminNameSelector(conn).loadRowsByParentId(adAdmin.getPid(), isLock);
 
 		for (IRow row : adAdminNameList) {
 			row.setMesh(adAdmin.mesh());
 		}
-
 		adAdmin.setNames(adAdminNameList);
 
 		for (IRow row : adAdminNameList) {
 			AdAdminName obj = (AdAdminName) row;
-
 			adAdmin.adAdminNameMap.put(obj.rowId(), obj);
 		}
 	}

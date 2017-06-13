f59dd106a3e2869dc7571105a14da9eb883399b5
Xiaowen-Navinfo <xiaoxiaowen4127@navinfo.com>
2017-06-13 22:06:03 +0800

    monitor project

---
+++ monitor/monitor-agent/src/main/java/com/navinfo/dataservice/monitor/agent/service/ServiceStatInfoLoader.java
@@ -0,0 +1,11 @@
+package com.navinfo.dataservice.monitor.agent.service;
+
+/** 
+ * @ClassName: ServiceStatInfoLoader
+ * @author xiaoxiaowen4127
+ * @date 2017年6月13日
+ * @Description: ServiceStatInfoLoader.java
+ */
+public class ServiceStatInfoLoader {
+
+}

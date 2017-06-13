4c73a311a8e147236690cb9c91ae9e90cf657311
zhangli5174 <zhangli5174@navinfo.com>
2017-06-13 16:14:53 +0800

    提交针对city block 导入时检查脚本

--- scripts-interface/src/main/java/com/navinfo/dataservice/scripts/CheckImportCityBlockJson.java
+++ scripts-interface/src/main/java/com/navinfo/dataservice/scripts/CheckImportCityBlockJson.java
@@ -4,34 +4,20 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.sql.Clob;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import org.apache.commons.dbutils.DbUtils;
-import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-import com.navinfo.dataservice.api.datahub.model.DbInfo;
-import com.navinfo.dataservice.commons.database.ConnectionUtil;
-import com.navinfo.dataservice.commons.database.DbConnectConfig;
-import com.navinfo.dataservice.commons.database.OracleSchema;
 import com.navinfo.dataservice.commons.log.LoggerRepos;
-import com.navinfo.dataservice.commons.util.JtsGeometryFactory;
-import com.navinfo.dataservice.datahub.service.DbService;
 import com.navinfo.dataservice.scripts.model.Block4Imp;
 import com.navinfo.dataservice.scripts.model.City4Imp;
-import com.navinfo.navicommons.database.QueryRunner;
-import com.navinfo.navicommons.database.sql.DbLinkCreator;
-import com.navinfo.navicommons.database.sql.SqlExec;
 import com.navinfo.navicommons.geo.computation.CompGridUtil;
-import com.navinfo.navicommons.geo.computation.MeshUtils;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 /** 
@@ -42,14 +28,13 @@ import net.sf.json.JSONObject;
  */
 public class CheckImportCityBlockJson {
 	private static Logger log = LoggerRepos.getLogger(CheckImportCityBlockJson.class);
-	private static QueryRunner runner = new QueryRunner();
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
 		try {
-			if(args==null||args.length!=2){
+			if(args==null||args.length!=3){
 				log.error("ERROR:need args:cityFile blockFile");
 				return;
 			}
@@ -57,8 +42,10 @@ public class CheckImportCityBlockJson {
 			String initBlockFile = args[0];
 			String cityFile = args[1];
 			String blockFile = args[2];
-//			String cityFile = "F:\\data\\blocksImp\\CITYS.JSON";
-//			String blockFile = "F:\\data\\blocksImp\\BLOCKS.JSON";
+			
+//			String initBlockFile="F:\\newblock.txt";
+//			String cityFile = "F:\\CITYS.JSON";
+//			String blockFile = "F:\\BLOCKS.JSON";
 
 			check(initBlockFile,cityFile,blockFile);
 			
@@ -69,93 +56,43 @@ public class CheckImportCityBlockJson {
 			e.printStackTrace();
 		}
 	}
-	private static long getCityId(Connection conn)throws SQLException{
-		String sql = "SELECT CITY_SEQ.NEXTVAL FROM DUAL";
-		return runner.queryForLong(conn, sql);
-	}
-	
-	private static long getBlockId(Connection conn)throws SQLException{
-		String sql = "SELECT BLOCK_SEQ.NEXTVAL FROM DUAL";
-		return runner.queryForLong(conn, sql);
-	}
 	
 	public static void check(String initBlockFile,String cityFile,String blockFile)throws Exception{
-		Connection conn = null;
-		PreparedStatement stmt = null;
 		try{
-			DbInfo manInfo = DbService.getInstance().getOnlyDbByBizType("fmMan");
-
-			OracleSchema manSchema = new OracleSchema(
-					DbConnectConfig.createConnectConfig(manInfo.getConnectParam()));
-			conn = manSchema.getPoolDataSource().getConnection();
-			QueryRunner run = new QueryRunner();
-//			parseInitBlock(initBlockFile);
-			
 			
 			Map<String,City4Imp> citys = parseCity(cityFile);
+			
 			//write citys
-			String insCitySql = "INSERT INTO CITY (CITY_ID,CITY_NAME,ADMIN_ID,PROVINCE_NAME,GEOMETRY,REGION_ID) VALUES (?,?,?,?,SDO_GEOMETRY(?,8307),(select REGION_ID from cp_region_province where province=?))";
-			String updCityGridSql = "UPDATE GRID SET CITY_ID=? WHERE GRID_ID IN (select to_number(column_value) from table(clob_to_table(?)))";
-			for(City4Imp city:citys.values()){
-				long cityId= getCityId(conn);
+			/*for(City4Imp city:citys.values()){
 				Geometry cityGeo = MeshUtils.meshes2Jts(city.getMeshes());
 				cityGeo.getGeometryType();
-				Clob clob = ConnectionUtil.createClob(conn);
-				clob.setString(1, JtsGeometryFactory.writeWKT(cityGeo));
-				run.update(conn, insCitySql, cityId,city.getCityName(),city.getAdminId(),city.getProvName(),clob,city.getProvName());
-				//update grid table
-				Set<String> grids = new HashSet<String>();
-				for(String mesh:city.getMeshes()){
-					grids.addAll(CompGridUtil.mesh2Grid(mesh));
-				}
-
-				Clob clob2 = ConnectionUtil.createClob(conn);
-				clob2.setString(1, StringUtils.join(grids,","));
-				run.update(conn, updCityGridSql,cityId, clob2);
-			}
-			//
+			}*/
 			Map<String,Block4Imp> blocks = parseBlock(citys,blockFile);
-			//write blocks
-
-			String insBlockSql = "INSERT INTO BLOCK (BLOCK_ID,CITY_ID,BLOCK_NAME,GEOMETRY,WORK_PROPERTY) VALUES (?,(SELECT CITY_ID FROM CITY WHERE CITY_NAME=?),?,SDO_GEOMETRY(?,8307),?)";
-			String updBlockGridSql = "UPDATE GRID SET BLOCK_ID=? WHERE GRID_ID IN (select to_number(column_value) from table(clob_to_table(?)))";
-			for(Block4Imp block:blocks.values()){
-//				long blockId= getBlockId(conn);
-				Geometry blockGeo = CompGridUtil.grids2Jts(block.getGrids());
-				/*Clob clob = ConnectionUtil.createClob(conn);
-				clob.setString(1, JtsGeometryFactory.writeWKT(blockGeo));*/
-				
-//				blockGeo.getGeometryType().
-				
-				
-				
-				//run.update(conn, insBlockSql, blockId,block.getCityName(),block.getBlockName(),clob,block.getWrokProperty());
-				//update grid table
-//				Clob clob2 = ConnectionUtil.createClob(conn);
-//				clob2.setString(1, StringUtils.join(block.getGrids(),","));
-//				run.update(conn, updBlockGridSql,blockId, clob2);
-			}
-
-			//update city admin geo
-			//初始化脚本，忽略事务一致了
-			DbInfo gdbInfo = DbService.getInstance().getOnlyDbByBizType("nationRoad");
-			DbConnectConfig gdbConfig = DbConnectConfig.createConnectConfig(gdbInfo.getConnectParam());
-			DbLinkCreator cr = new DbLinkCreator();
-			cr.create("FMGDB_LINK", false, manSchema.getPoolDataSource(), 
-					gdbConfig.getUserName(), gdbConfig.getUserPasswd(), gdbConfig.getServerIp(), String.valueOf(gdbConfig.getServerPort()), gdbConfig.getServiceName());
-			SqlExec sqlExec = new SqlExec(conn);
-			String sqlFile = "/com/navinfo/dataservice/scripts/resources/update_man_city_admingeo.sql";
-			sqlExec.execute(sqlFile);
 			
+			for(Map.Entry<String,Block4Imp> block:blocks.entrySet()){
+				
+				Geometry blockGeo = CompGridUtil.grids2Jts(block.getValue().getGrids());
+				//判断是否出现复杂多边形
+//				String geometryType = blockGeo.getGeometryType();
+//				System.out.println("geometryType: "+geometryType);
+				
+				if (blockGeo instanceof MultiPolygon) {
+					JSONArray jsonarray = JSONArray.fromObject(block.getValue().getGrids()); 
+					//判断geometry 是否是复杂多边形
+					log.info(blockFile+" 检查: "+"此block 几何含复杂多边形!");
+					log.info(block.getKey()+": 此block 范围内 grids: "+jsonarray);
+				}
+			}
+			
+			parseInitBlock(initBlockFile,citys.size(),blocks.size());
+
 		}catch(Exception e){
-			DbUtils.rollbackAndCloseQuietly(conn);
 			e.printStackTrace();
 			throw e;
 		}finally{
-			DbUtils.closeQuietly(stmt);
-			DbUtils.commitAndCloseQuietly(conn);
 		}
 	}
+	
 	
 	public static Map<String,City4Imp> parseCity(String cityFile)throws Exception{
 		BufferedReader reader = null;
@@ -184,6 +121,8 @@ public class CheckImportCityBlockJson {
 					newCity.setMeshes(meshes);
 					citys.put(cityName, newCity);
 				}
+				
+//				cityCount+=1;
 			}
 			System.out.println("read city file over. city size:"+citys.size());
 			return citys;
@@ -195,7 +134,6 @@ public class CheckImportCityBlockJson {
 				reader.close();
 			}
 		}
-		
 	}
 
 	public static Map<String,Block4Imp> parseBlock(Map<String,City4Imp> citys,String blockFile)throws Exception{
@@ -207,6 +145,8 @@ public class CheckImportCityBlockJson {
 
 			List<String> allGrids = new ArrayList<String>();
 			List<String> appliedGrids = new ArrayList<String>();
+
+			Map<String,String> meshCity = new HashMap<String,String>();
 			
 			InputStreamReader read = new InputStreamReader(
 					new FileInputStream(file));
@@ -221,6 +161,17 @@ public class CheckImportCityBlockJson {
 				//添加到全部的grid 集合中
 				allGrids.add(grid);
 				String mesh = grid.substring(0, 6);
+				if(meshCity.containsKey(mesh)){
+					//图幅已经有对应的city  比较已有city 与现有city是否一致
+					String oldCityName = meshCity.get(mesh);
+					if(!cityName.equals(oldCityName)){
+						//同一个图幅的grid 被分配到了不同的 city
+						log.info(blockFile+" 检查: "+"同一个图幅的grid 被分配到了不同的 city ");
+						log.info(" 当前grid: "+grid+" 当前city: "+cityName+" 同组grid分配city: "+oldCityName);
+					}
+				}else{
+					meshCity.put(mesh, cityName);
+				}
 				if(citys.containsKey(cityName)&&citys.get(cityName).getMeshes().contains(mesh)){
 
 					String blockCode = blockJson.getString("code");
@@ -242,16 +193,16 @@ public class CheckImportCityBlockJson {
 						appliedGrids.add(grid);
 					}
 				}
+				
+//				blockCount+=1;
 			}
 			
 			//未分配给任何block的grid 集合
 			allGrids.removeAll(appliedGrids);
 			if(allGrids.size() > 0){
-				log.info(blockFile+" 检查: "+"未分配给任何block的grids : "+allGrids.toArray());
+				JSONArray jsonarray = JSONArray.fromObject(allGrids); 
+				log.info(blockFile+" 检查: "+"未分配给任何block的grids : "+jsonarray);
 			}
-			
-			
-			
 			System.out.println("read block file over. block size:"+citys.size());
 			return blocks;
 		}catch(Exception e){
@@ -264,4 +215,49 @@ public class CheckImportCityBlockJson {
 		}
 		
 	}
+	
+	private static void parseInitBlock(String initBlockFile,int cityCount,int blockCount) throws Exception {
+		Set<String> cityImps = null;
+		Set<String> blockImps = null;
+		int lineCount = 0;
+		BufferedReader reader = null;
+		try{
+			System.out.println("Starting read initBlockFile file...");
+			cityImps = new HashSet<String>(); 
+			blockImps = new HashSet<String>(); 
+			
+			File file = new File(initBlockFile);
+
+			InputStreamReader read = new InputStreamReader(
+					new FileInputStream(file));
+
+			reader = new BufferedReader(read);
+			String line=null;
+			while ((line = reader.readLine()) != null){
+				JSONObject blockJson = JSONObject.fromObject(line);
+				//先判断计算的grid是否超过了城市界定的图幅，如果超过了，忽略
+				cityImps.add(blockJson.getString("city"));
+				blockImps.add(blockJson.getString("BlockCode"));
+				lineCount+=1;
+			}
+			
+			if(cityImps.size() != cityCount){
+				log.info(initBlockFile+" 检查:"+" 导入的city ("+cityImps.size()+")和原始文件中的city("+cityCount+") 数量不一致!");
+			}
+			
+			if(blockImps.size() != blockCount){
+				log.info(initBlockFile+" 检查:"+" 导入的block ("+blockImps.size()+")和原始文件中的block("+blockCount+") 数量不一致!");
+			}
+			
+			System.out.println("read initBlockFile file over. initBlockFile size:"+lineCount);
+		}catch(Exception e){
+			e.printStackTrace();
+			throw e;
+		}finally{
+			if(reader!=null){
+				reader.close();
+			}
+		}
+	}
+
 }

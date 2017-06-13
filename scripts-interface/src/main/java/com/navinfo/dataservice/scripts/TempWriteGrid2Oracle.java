5e8722ceb350506ecd492bf51b60e87d830ceb00
Xiaowen-Navinfo <xiaoxiaowen4127@navinfo.com>
2017-06-13 10:28:54 +0800

    log analysis

---
+++ scripts-interface/src/main/java/com/navinfo/dataservice/scripts/TempWriteGrid2Oracle.java
@@ -0,0 +1,14 @@
+package com.navinfo.dataservice.scripts;
+
+/** 
+ * @ClassName: TempWriteGrid2Oracle
+ * @author xiaoxiaowen4127
+ * @date 2017年6月12日
+ * @Description: TempWriteGrid2Oracle.java
+ */
+public class TempWriteGrid2Oracle {
+	
+	public static void main(String[] args) {
+		
+	}
+}

128e6d0a7131d7ebea082bf1f41f34c1aab1ffec
Zhang Yt <zhangyuntao@navinfo.com>
2017-06-15 10:30:21 +0800

    fix bug 6487

--- engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/GLM01026.java
+++ engine/engine-check/src/main/java/com/navinfo/dataservice/engine/check/rules/GLM01026.java
@@ -1,10 +1,5 @@
 package com.navinfo.dataservice.engine.check.rules;
 
-import java.util.Map;
-
-
-import net.sf.json.JSONObject;
-
 import com.navinfo.dataservice.commons.geom.GeoTranslator;
 import com.navinfo.dataservice.dao.check.CheckCommand;
 import com.navinfo.dataservice.dao.glm.iface.IRow;
@@ -13,6 +8,9 @@ import com.navinfo.dataservice.engine.check.core.baseRule;
 import com.navinfo.navicommons.geo.computation.MeshUtils;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
+import net.sf.json.JSONObject;
+
+import java.util.Map;
 
 /**
  * Rdlink	html	GLM01026	后台	除起终点，Link的形状点不能在图廓上？？？
@@ -29,10 +27,9 @@ public class GLM01026 extends baseRule {
                 Geometry geo = rdLink.getGeometry();
                 Map<String, Object> changedFields = rdLink.changedFields();
                 if (changedFields != null && changedFields.containsKey("geometry")) {
-                    JSONObject geojson = (JSONObject) changedFields.get("geometry");
-                    geo = GeoTranslator.geojson2Jts(geojson);
+                    geo = GeoTranslator.geojson2Jts((JSONObject) changedFields.get("geometry"));
                 }
-
+                geo = GeoTranslator.transform(geo, GeoTranslator.dPrecisionMap, 5);
                 if (this.hasBorderNode(geo)) {
                     this.setCheckResult("", "", 0);
                     return;
@@ -51,7 +48,7 @@ public class GLM01026 extends baseRule {
         Coordinate[] coords = geo.getCoordinates();
         for (int j = 1; j < coords.length - 1; j++) {
             Coordinate current = coords[j];
-            if (MeshUtils.isPointAtMeshBorderWith100000(current.x, current.y) == true) {
+            if (MeshUtils.isPointAtMeshBorder(current.x, current.y) == true) {
                 return true;
             }
         }

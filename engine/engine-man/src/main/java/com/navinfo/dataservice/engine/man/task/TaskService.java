58ebab207c5795119f9156997fd6fb6764e79f6f
songhe <songhe@navinfo.com>
2017-07-05 16:42:29 +0800

    获取条件规划

--- engine/engine-man/src/main/java/com/navinfo/dataservice/engine/man/task/TaskService.java
+++ engine/engine-man/src/main/java/com/navinfo/dataservice/engine/man/task/TaskService.java
@@ -1,5 +1,6 @@
 package com.navinfo.dataservice.engine.man.task;
 
+import java.io.IOException;
 import java.sql.Clob;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -4438,7 +4439,11 @@ public class TaskService {
 							taskPrograss.setStartDate(rs.getTimestamp("START_DATE"));
 							taskPrograss.setEndDate(rs.getTimestamp("END_DATE"));
 							taskPrograss.setMessage(rs.getString("MESSAGE"));
-							taskPrograss.setParameter(rs.getClob("PARAMETER").toString());
+							try {
+								taskPrograss.setParameter(TaskProgressOperation.ClobToString(rs.getClob("PARAMETER")));
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
 							taskPrograss.setOperator(rs.getLong("OPERATOR"));
 							taskPrograss.setPhaseId(rs.getInt("PHASE_ID"));
 							return taskPrograss;
@@ -4505,5 +4510,33 @@ public class TaskService {
 //			}
 //		
 //		}
+		
+		/**
+		 *  获取条件规划
+		 *  获取该任务保存的条件规划情况task_progess中phase=2的记录的parameter
+		 *  应用场景：独立工具--外业规划--数据规划
+		 * @param int taskId
+		 * @return String parameter
+		 * @throws Exception
+		 * 
+		 * */
+		public JSONObject getPlan(int taskId) throws Exception{
+			Connection conn = null;
+			try{
+				conn = DBConnector.getInstance().getManConnection();
+				TaskProgress tp = taskInPrograssCount(conn, taskId);
+				String parameter = tp.getParameter();
+				JSONObject json = JSONObject.fromObject(parameter);
+				return json;
+			}catch(Exception e){
+				log.error("获取条件规划异常，原因为：" + e);
+				DbUtils.closeQuietly(conn);
+				throw new Exception("获取条件规划异常");
+			}finally{
+				DbUtils.closeQuietly(conn);
+			}
+		}
+		
+
 		
 }

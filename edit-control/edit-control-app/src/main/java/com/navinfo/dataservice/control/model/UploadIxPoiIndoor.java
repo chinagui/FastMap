4b86b7fac86aa6f6467043e0816b1afaa47fdf37
zhangli5174 <zhangli5174@navinfo.com>
2017-07-06 21:44:27 +0800

    提交 遗漏

---
+++ edit-control/edit-control-app/src/main/java/com/navinfo/dataservice/control/model/UploadIxPoiIndoor.java
@@ -0,0 +1,27 @@
+package com.navinfo.dataservice.control.model;
+
+
+/** 
+* @ClassName:  IxPoiChildren 
+* @author zl
+* @date 2017-06-20
+* @Description: TODO
+*/
+public class UploadIxPoiIndoor{
+	private int type;
+	private String floor;
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public String getFloor() {
+		return floor;
+	}
+	public void setFloor(String floor) {
+		this.floor = floor;
+	}
+	
+	
+}

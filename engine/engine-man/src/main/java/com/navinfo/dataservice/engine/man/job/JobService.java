ab052b24b1aeb393017b9b37c748b27c6d1898f1
wangshishuai3966 <wangshishuai3966@navinfo.com>
2017-07-11 20:32:22 +0800

    增加更新job步骤状态的api

--- engine/engine-man/src/main/java/com/navinfo/dataservice/engine/man/job/JobService.java
+++ engine/engine-man/src/main/java/com/navinfo/dataservice/engine/man/job/JobService.java
@@ -1,12 +1,13 @@
 package com.navinfo.dataservice.engine.man.job;
 
 import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.navinfo.dataservice.bizcommons.datasource.DBConnector;
 import com.navinfo.dataservice.commons.log.LoggerRepos;
 import com.navinfo.dataservice.engine.man.job.Tips2Mark.Tips2MarkJobRunner;
 import com.navinfo.dataservice.engine.man.job.bean.ItemType;
+import com.navinfo.dataservice.engine.man.job.bean.Job;
 import com.navinfo.dataservice.engine.man.job.bean.JobProgressStatus;
+import com.navinfo.dataservice.engine.man.job.bean.JobStatus;
 import com.navinfo.dataservice.engine.man.job.operator.JobOperator;
 import com.navinfo.dataservice.engine.man.job.operator.JobProgressOperator;
 import org.apache.commons.dbutils.DbUtils;
@@ -19,77 +20,104 @@ import java.sql.Connection;
  */
 public class JobService {
     private Logger log = LoggerRepos.getLogger(JobService.class);
-    private JobService(){}
-    private static class SingletonHolder{
-        private static final JobService INSTANCE =new JobService();
+
+    private JobService() {
     }
-    public static JobService getInstance(){
+
+    public static JobService getInstance() {
         return JobService.SingletonHolder.INSTANCE;
     }
 
-
     /**
      * 执行tips转mark
-     * @param itemId    目标对象ID
-     * @param itemType  目标对象类型（项目、任务、子任务、批次）
-     * @param operator  执行人
+     *
+     * @param itemId     目标对象ID
+     * @param itemType   目标对象类型（项目、任务、子任务、批次）
+     * @param operator   执行人
      * @param isContinue 是否继续
      * @return jobId
      * @throws Exception
      */
-    public long tips2Mark(long itemId, ItemType itemType, long operator, boolean isContinue, String parameter) throws Exception{
+    public long tips2Mark(long itemId, ItemType itemType, long operator, boolean isContinue, String parameter) throws Exception {
         try {
             Tips2MarkJobRunner runner = new Tips2MarkJobRunner();
             return runner.run(itemId, itemType, isContinue, operator, parameter);
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error(e.getMessage(), e);
-            throw new Exception("执行tips转mark失败，原因为:"+e.getMessage(),e);
+            throw new Exception("执行tips转mark失败，原因为:" + e.getMessage(), e);
         }
     }
 
     /**
      * 获取JOB每个步骤的执行状态
+     *
      * @param jobId
      * @return
      * @throws Exception
      */
-    public JSONArray getJobProgress(long jobId) throws Exception{
-        Connection conn=null;
-        try{
+    public JSONArray getJobProgress(long jobId) throws Exception {
+        Connection conn = null;
+        try {
             conn = DBConnector.getInstance().getManConnection();
             JobOperator jobOperator = new JobOperator(conn);
             return jobOperator.getJobProgressStatus(jobId);
-        }catch (Exception e){
+        } catch (Exception e) {
             DbUtils.rollbackAndCloseQuietly(conn);
             log.error(e.getMessage(), e);
-            throw new Exception("获取JOB状态失败，原因为:"+e.getMessage(),e);
-        }finally {
+            throw new Exception("获取JOB状态失败，原因为:" + e.getMessage(), e);
+        } finally {
             DbUtils.commitAndCloseQuietly(conn);
         }
     }
 
     /**
      * 更新Job步骤的状态，并继续执行Job
+     *
      * @param phaseId
      * @param status
      * @param outParameter
      * @throws Exception
      */
-    public void updateJobProgress(long phaseId, JobProgressStatus status, String outParameter) throws Exception{
-        Connection conn=null;
-        try{
+    public void updateJobProgress(long phaseId, JobProgressStatus status, String outParameter) throws Exception {
+        Connection conn = null;
+        try {
             conn = DBConnector.getInstance().getManConnection();
             JobProgressOperator jobProgressOperator = new JobProgressOperator(conn);
             jobProgressOperator.updateStatus(phaseId, status, outParameter);
 
+            if (status == JobProgressStatus.FAILURE) {
+                //步骤失败，更新job状态为失败，停止执行
+                JobOperator jobOperator = new JobOperator(conn);
+                jobOperator.updateStatusByPhaseId(phaseId, JobStatus.FAILURE);
+            } else {
+                //步骤成果，进行执行job
+                JobOperator jobOperator = new JobOperator(conn);
+                Job job = jobOperator.getByPhaseId(phaseId);
+                if (job == null) {
+                    throw new Exception("phaseId:" + phaseId + "对应的job不存在！");
+                }
+                JobRunner runner = null;
+                switch (job.getType()) {
+                    case TiPS2MARK:
+                        runner = new Tips2MarkJobRunner();
+                        break;
+                }
 
-
-        }catch (Exception e){
+                if (runner == null) {
+                    throw new Exception("不支持的任务类型：jobid" + job.getJobId() + ",type" + job.getType().value());
+                }
+                runner.resume(job);
+            }
+        } catch (Exception e) {
             DbUtils.rollbackAndCloseQuietly(conn);
             log.error(e.getMessage(), e);
-            throw new Exception("获取JOB状态失败，原因为:"+e.getMessage(),e);
-        }finally {
+            throw new Exception("更新JOB步骤状态失败，原因为:" + e.getMessage(), e);
+        } finally {
             DbUtils.commitAndCloseQuietly(conn);
         }
+    }
+
+    private static class SingletonHolder {
+        private static final JobService INSTANCE = new JobService();
     }
 }

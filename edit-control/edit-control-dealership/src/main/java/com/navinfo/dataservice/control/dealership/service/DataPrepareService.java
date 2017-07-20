daeafaf3c7c5c97fe2566b8115c77d123331cce7
jicaihua <jicaihua@navinfo.com>
2017-07-20 17:45:35 +0800

    修改excel检查问题

--- edit-control/edit-control-dealership/src/main/java/com/navinfo/dataservice/control/dealership/service/DataPrepareService.java
+++ edit-control/edit-control-dealership/src/main/java/com/navinfo/dataservice/control/dealership/service/DataPrepareService.java
@@ -415,7 +415,7 @@ public class DataPrepareService {
 					throw new Exception("表表差分结果中“UUID”在IX_DEALERSHIP_RESULT.RESULT_ID中不存在:uuid="+resultId);
 				}
 				resultObj = resultObjSet.get(resultId);
-				if(resultObj.getName()!=diffSub.getName()||resultObj.getAddress()!=diffSub.getAddress()){
+				if(!(resultObj.getName().equals(diffSub.getName()))||!(resultObj.getAddress().equals(diffSub.getAddress()))){
 					log.info("resultObj.name:"+resultObj.getName()+",resultObj.address:"+resultObj.getAddress()+",diffSub.name:"+diffSub.getName()+",diffSub.address:"+diffSub.getAddress());
 					log.info("表表差分结果中“厂商提供名称”或“厂商提供地址”和库中不一致:uuid="+resultId);
 					throw new Exception("表表差分结果中“厂商提供名称”或“厂商提供地址”和库中不一致:uuid="+resultId);

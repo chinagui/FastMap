2128c8e1086508087c36f3695751c0b63e315245
sunjiawei <sunjiawei@navinfo.com>
2017-08-01 15:32:10 +0800

    day_plan 定时任务统计

---
+++ engine/engine-statics/src/main/java/com/navinfo/dataservice/engine/statics/launcher/starter/DayPlanStatStarter.java
@@ -0,0 +1,13 @@
+package com.navinfo.dataservice.engine.statics.launcher.starter;
+
+import com.navinfo.dataservice.engine.statics.launcher.StatJobStarter;
+
+
+public class DayPlanStatStarter extends StatJobStarter {
+	@Override
+	public String jobType() {
+		// TODO Auto-generated method stub
+		return "dayPlanStat";
+	}
+
+}

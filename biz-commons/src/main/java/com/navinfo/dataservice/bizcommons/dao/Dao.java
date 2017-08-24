d5966a14c650733e01cbce8926d964437ea2fc03
Xiaowen-Navinfo <xiaoxiaowen4127@navinfo.com>
2017-08-24 18:34:25 +0800

    poi upload

---
+++ biz-commons/src/main/java/com/navinfo/dataservice/bizcommons/dao/Dao.java
@@ -0,0 +1,11 @@
+package com.navinfo.dataservice.bizcommons.dao;
+
+/** 
+ * @ClassName: Dao
+ * @author xiaoxiaowen4127
+ * @date 2017年8月24日
+ * @Description: Dao.java
+ */
+public abstract class Dao {
+	public abstract Object[] attrArray();
+}

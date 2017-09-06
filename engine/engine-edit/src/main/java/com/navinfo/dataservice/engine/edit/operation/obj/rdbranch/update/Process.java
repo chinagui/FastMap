8b69252189a2ba830d29282a182528ae93977cda
fenghaixia04361 <fenghaixia@navinfo.com>
2017-09-06 13:57:48 +0800

    修改_GLM01028_3增加分歧的触发时机

--- engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/obj/rdbranch/update/Process.java
+++ engine/engine-edit/src/main/java/com/navinfo/dataservice/engine/edit/operation/obj/rdbranch/update/Process.java
@@ -1,6 +1,12 @@
 package com.navinfo.dataservice.engine.edit.operation.obj.rdbranch.update;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.navinfo.dataservice.dao.glm.iface.IRow;
 import com.navinfo.dataservice.dao.glm.model.rd.branch.RdBranch;
+import com.navinfo.dataservice.dao.glm.model.rd.branch.RdBranchVia;
+import com.navinfo.dataservice.dao.glm.model.rd.link.RdLink;
 import com.navinfo.dataservice.dao.glm.selector.rd.branch.RdBranchSelector;
 import com.navinfo.dataservice.engine.edit.operation.AbstractCommand;
 import com.navinfo.dataservice.engine.edit.operation.AbstractProcess;
@@ -34,6 +40,21 @@ public class Process extends AbstractProcess<Command> {
 	public String exeOperation() throws Exception {
 		// TODO Auto-generated method stub
 		return new Operation(this.getCommand(),this.branch,this.getConn()).run(this.getResult());
+	}
+	
+	@Override
+	public void postCheck() throws Exception {
+		List<IRow> glmList = new ArrayList<IRow>();
+		glmList.addAll(this.getResult().getAddObjects());
+		glmList.addAll(this.getResult().getUpdateObjects());
+		
+		for (IRow irow : this.getResult().getDelObjects()) {
+			if (irow instanceof RdBranchVia) {
+				glmList.add(irow);
+			}
+		}
+		this.checkCommand.setGlmList(glmList);
+		this.checkEngine.postCheck();
 	}
 
 }

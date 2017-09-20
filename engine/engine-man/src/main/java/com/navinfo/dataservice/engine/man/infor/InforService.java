bef5686c3213da0fdeba7987fd2e34286c64c081
songhe <songhe@navinfo.com>
2017-09-20 16:03:38 +0800

    快线情报项目字段原则变更

--- engine/engine-man/src/main/java/com/navinfo/dataservice/engine/man/infor/InforService.java
+++ engine/engine-man/src/main/java/com/navinfo/dataservice/engine/man/infor/InforService.java
@@ -80,11 +80,28 @@ private Logger log = LoggerRepos.getLogger(this.getClass());
 		    long diff = date2.getTime() - date1.getTime();
 		    long days = diff / (1000 * 60 * 60 * 24);			
 
-			if(days<-21){
-				infor.setMethod("矢量制作");
-			}else if(days==-21){
+		    //modify by songhe 2017/09/20   13迭代method字段赋值原则变更
+			if(days == -21){
 				infor.setMethod("预采集");
-			}else if(days>=-6){
+			}else if(days >= -6){
+				infor.setMethod("正式采集");
+			}
+			int sourceCode = 0;
+			int featureKind = 0;
+			int inforStage = 0;
+			if(dataJson.containsKey("sourceCode")){
+				sourceCode = dataJson.getInt("sourceCode");
+			}
+			if(dataJson.containsKey("featureKind")){
+				inforStage = dataJson.getInt("featureKind");
+			}
+			if(dataJson.containsKey("inforStage")){
+				inforStage = dataJson.getInt("inforStage");
+			}
+			if(sourceCode == 1 && featureKind == 2 && inforStage == 2){
+				infor.setMethod("矢量制作");
+			}
+			if(sourceCode == 3){
 				infor.setMethod("正式采集");
 			}
 			

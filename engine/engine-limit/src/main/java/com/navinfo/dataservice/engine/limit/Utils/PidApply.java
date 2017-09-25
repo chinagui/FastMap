84c262aad9a738c66998928a8343ceaa39df4f8c
fenghaixia04361 <fenghaixia@navinfo.com>
2017-09-25 15:13:18 +0800

    修改_查找manouvre的编辑功能

---
+++ engine/engine-limit/src/main/java/com/navinfo/dataservice/engine/limit/Utils/PidApply.java
@@ -0,0 +1,12 @@
+package com.navinfo.dataservice.engine.limit.Utils;
+
+public class PidApply {
+	
+	public String pidForInsertGroup(String infoIntelId,int adAdmin){
+		return null;
+	} 
+	
+	public String pidForInsertManoeuvre(String groupId){
+		return null;
+	}
+}

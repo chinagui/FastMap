41d982d7313617ed5026a2608bed368a3bad2053
fenghaixia04361 <fenghaixia@navinfo.com>
2017-09-29 18:07:49 +0800

    修改，删除临时link

--- engine/engine-limit/src/main/java/com/navinfo/dataservice/engine/limit/operation/limit/scplateresrdlink/delete/Command.java
+++ engine/engine-limit/src/main/java/com/navinfo/dataservice/engine/limit/operation/limit/scplateresrdlink/delete/Command.java
@@ -1,5 +1,65 @@
 package com.navinfo.dataservice.engine.limit.operation.limit.scplateresrdlink.delete;
 
-public class Command {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.navinfo.dataservice.dao.glm.iface.OperType;
+import com.navinfo.dataservice.engine.limit.glm.iface.DbType;
+import com.navinfo.dataservice.engine.limit.glm.iface.LimitObjType;
+import com.navinfo.dataservice.engine.limit.glm.model.limit.ScPlateresLink;
+import com.navinfo.dataservice.engine.limit.operation.AbstractCommand;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+public class Command extends AbstractCommand{
+
+	private String requester;
+	
+	private JSONArray geometryIds;
+	
+	private List<ScPlateresLink> scplatereslinks = new ArrayList<>();
+	
+	public Command(JSONObject json,String requester){
+		this.requester = requester;
+		
+		this.geometryIds = json.getJSONArray("objId");
+	}
+	
+	public List<ScPlateresLink> getscplateresLinks(){
+		return this.scplatereslinks;
+	}
+	
+	public void setscplateresLinks(List<ScPlateresLink> values){
+		this.scplatereslinks = values;
+	}
+	
+	public JSONArray getGeometryIds(){
+		return this.geometryIds;
+	}
+	
+	@Override
+	public OperType getOperType() {
+		// TODO Auto-generated method stub
+		return OperType.DELETE;
+	}
+
+	@Override
+	public DbType getDbType() {
+		// TODO Auto-generated method stub
+		return DbType.LIMITDB;
+	}
+
+	@Override
+	public String getRequester() {
+		// TODO Auto-generated method stub
+		return this.requester;
+	}
+
+	@Override
+	public LimitObjType getObjType() {
+		// TODO Auto-generated method stub
+		return LimitObjType.SCPLATERESLINK;
+	}
+	
 }

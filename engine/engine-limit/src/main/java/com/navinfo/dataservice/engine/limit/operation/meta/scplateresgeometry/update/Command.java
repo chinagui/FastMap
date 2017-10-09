bb8deae2eace059cf229a7afeabe5e8530ca728c
ly <luyao@navinfo.com>
2017-10-09 11:27:06 +0800

    task 9070、9069、9068、9067

---
+++ engine/engine-limit/src/main/java/com/navinfo/dataservice/engine/limit/operation/meta/scplateresgeometry/update/Command.java
@@ -0,0 +1,68 @@
+package com.navinfo.dataservice.engine.limit.operation.meta.scplateresgeometry.update;
+
+import com.navinfo.dataservice.dao.glm.iface.OperType;
+import com.navinfo.dataservice.engine.limit.glm.iface.DbType;
+import com.navinfo.dataservice.engine.limit.glm.iface.LimitObjType;
+import com.navinfo.dataservice.engine.limit.glm.model.meta.ScPlateresGeometry;
+import com.navinfo.dataservice.engine.limit.operation.AbstractCommand;
+import net.sf.json.JSONObject;
+
+public class Command extends AbstractCommand {
+
+    private String requester;
+
+    private JSONObject content;
+
+    private String id;
+
+    private ScPlateresGeometry geometry;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public ScPlateresGeometry getGeometry() {
+        return geometry;
+    }
+
+    public void setGeometry(ScPlateresGeometry geometry) {
+        this.geometry = geometry;
+    }
+
+    JSONObject getContent() {
+        return content;
+    }
+
+    public Command(JSONObject json, String requester) {
+
+        this.requester = requester;
+
+        this.id = json.getString("objId");
+
+        this.content = json.getJSONObject("data");
+    }
+
+    @Override
+    public OperType getOperType() {
+        return OperType.UPDATE;
+    }
+
+    @Override
+    public DbType getDbType() {
+        return DbType.LIMITDB;
+    }
+
+    @Override
+    public String getRequester() {
+        return requester;
+    }
+
+    @Override
+    public LimitObjType getObjType() {
+        return LimitObjType.SCPLATERESGEOMETRY;
+    }
+}
---
+++ engine/engine-limit/src/main/java/com/navinfo/dataservice/engine/limit/operation/meta/scplateresgeometry/update/Operation.java
@@ -0,0 +1,40 @@
+package com.navinfo.dataservice.engine.limit.operation.meta.scplateresgeometry.update;
+
+import com.navinfo.dataservice.dao.glm.iface.ObjStatus;
+import com.navinfo.dataservice.engine.limit.glm.iface.IOperation;
+import com.navinfo.dataservice.engine.limit.glm.iface.Result;
+import com.navinfo.dataservice.engine.limit.glm.model.meta.ScPlateresGeometry;
+import net.sf.json.JSONObject;
+
+public class Operation implements IOperation {
+
+    private Command command;
+
+    public Operation(Command command) {
+        this.command = command;
+    }
+
+    @Override
+    public String run(Result result) throws Exception {
+        updateGroup(result);
+        return null;
+    }
+
+    private void updateGroup(Result result) throws Exception {
+        JSONObject content = command.getContent();
+
+        ScPlateresGeometry geometry = command.getGeometry();
+
+        if (content.containsKey("objStatus") && ObjStatus.UPDATE.toString().equals(
+                content.getString("objStatus"))) {
+
+            boolean isChanged = geometry.fillChangeFields(content);
+
+            if (isChanged) {
+                result.insertObject(geometry, ObjStatus.UPDATE, geometry.getGeometryId());
+            }
+
+        }
+        result.setPrimaryId(geometry.getGeometryId());
+    }
+}

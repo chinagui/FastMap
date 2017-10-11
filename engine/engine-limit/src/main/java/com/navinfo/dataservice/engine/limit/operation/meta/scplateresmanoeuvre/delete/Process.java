802955225ef0a24957f2cdfdf101ab8e79a31917
fenghaixia04361 <fenghaixia@navinfo.com>
2017-10-11 14:42:38 +0800

    行政区划编码母库到情报库转换

--- engine/engine-limit/src/main/java/com/navinfo/dataservice/engine/limit/operation/meta/scplateresmanoeuvre/delete/Process.java
+++ engine/engine-limit/src/main/java/com/navinfo/dataservice/engine/limit/operation/meta/scplateresmanoeuvre/delete/Process.java
@@ -33,6 +33,6 @@ public class Process extends AbstractProcess<Command> {
     
 	@Override
 	public String exeOperation() throws Exception {
-		return new Operation(this.getCommand()).run(this.getResult());
+		return new Operation(this.getCommand(),this.getConn()).run(this.getResult());
 	}
 }
---
+++ engine/engine-limit/src/main/java/com/navinfo/dataservice/engine/limit/search/limit/SearchHelp.java
@@ -0,0 +1,125 @@
+package com.navinfo.dataservice.engine.limit.search.limit;
+
+public class SearchHelp {
+	public static String updateInfoAdminCode(String mkadminCode){
+		String infoAdminCode = "";
+		
+		switch(mkadminCode){
+		case "110099":
+			infoAdminCode = "110100";
+			break;
+		case "120099":
+			infoAdminCode = "120100";
+			break;
+		case "310099":
+			infoAdminCode = "310100";
+			break;
+		case "419098":
+			infoAdminCode = "419001";
+			break;
+		case "429095":
+			infoAdminCode = "429004";
+			break;
+		case "429094":
+			infoAdminCode = "429005";
+			break;
+		case "429093":
+			infoAdminCode = "429006";
+			break;
+		case "429078":
+			infoAdminCode = "429021";
+			break;
+		case "441999":
+			infoAdminCode = "441900";
+			break;
+		case "442099":
+			infoAdminCode = "442000";
+			break;
+		case "460399":
+			infoAdminCode = "460300";
+			break;
+		case "460499":
+			infoAdminCode = "460400";
+			break;
+		case "469098":
+			infoAdminCode = "469001";
+			break;
+		case "469097":
+			infoAdminCode = "469002";
+			break;
+		case "469094":
+			infoAdminCode = "469005";
+			break;
+		case "469093":
+			infoAdminCode = "469006";
+			break;
+		case "469092":
+			infoAdminCode = "469007";
+			break;
+		case "469078":
+			infoAdminCode = "469021";
+			break;
+		case "469077":
+			infoAdminCode = "469022";
+			break;
+		case "469076":
+			infoAdminCode = "469023";
+			break;
+		case "469075":
+			infoAdminCode = "469024";
+			break;
+		case "469074":
+			infoAdminCode = "469025";
+			break;
+		case "469073":
+			infoAdminCode = "469026";
+			break;
+		case "469072":
+			infoAdminCode = "469027";
+			break;
+		case "469071":
+			infoAdminCode = "469028";
+			break;
+		case "469070":
+			infoAdminCode = "469029";
+			break;
+		case "469069":
+			infoAdminCode = "469030";
+			break;
+		case "500099":
+			infoAdminCode = "500100";
+			break;
+		case "659098":
+			infoAdminCode = "659001";
+			break;
+		case "659097":
+			infoAdminCode = "659002";
+			break;
+		case "659096":
+			infoAdminCode = "659003";
+			break;
+		case "659095":
+			infoAdminCode = "659004";
+			break;
+		case "659094":
+			infoAdminCode = "659005";
+			break;
+		case "659093":
+			infoAdminCode = "659006";
+			break;
+		case "659092":
+			infoAdminCode = "659007";
+			break;
+		case "659091":
+			infoAdminCode = "659008";
+			break;
+		case "659090":
+			infoAdminCode = "659009";
+			break;
+		default:
+			infoAdminCode = mkadminCode;
+			break;
+		}
+		return infoAdminCode;
+	}
+}

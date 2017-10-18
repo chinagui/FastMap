820d5b08dff0e6c3bf85f06d12deef4f2241460a
zhangli5174 <zhangli5174@navinfo.com>
2017-10-18 10:54:52 +0800

    提交点门牌及周poi转tab 脚本修改

--- scripts-interface/src/main/java/com/navinfo/dataservice/scripts/InitDataPoi2Tab.java
+++ scripts-interface/src/main/java/com/navinfo/dataservice/scripts/InitDataPoi2Tab.java
@@ -12,13 +12,18 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
 import javax.sql.DataSource;
+
 import org.apache.commons.dbutils.DbUtils;
 import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.lang.StringUtils;
+
 import com.navinfo.dataservice.api.datahub.iface.DatahubApi;
 import com.navinfo.dataservice.api.datahub.model.DbInfo;
 import com.navinfo.dataservice.bizcommons.datasource.DBConnector;
+import com.navinfo.dataservice.commons.config.SystemConfigFactory;
+import com.navinfo.dataservice.commons.constant.PropConstant;
 import com.navinfo.dataservice.commons.database.ConnectionUtil;
 import com.navinfo.dataservice.commons.database.MultiDataSourceFactory;
 import com.navinfo.dataservice.commons.springmvc.ApplicationContextUtil;
@@ -26,6 +31,7 @@ import com.navinfo.dataservice.dao.glm.model.ixpointaddress.IxPointaddress;
 import com.navinfo.dataservice.scripts.refinement.PoiTaskTabLogDependent;
 import com.navinfo.navicommons.database.QueryRunner;
 import com.navinfo.navicommons.database.sql.DbLinkCreator;
+
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
@@ -116,10 +122,10 @@ public class InitDataPoi2Tab {
 					if(daylyDbIds != null && daylyDbIds.size() > 0){
 						//遍历各个日大区库,将大区库中需要导出的数据导入目标大区库
 						for(Integer dDbID : daylyDbIds){
-							/*//TODO 临时加的
+							//TODO 临时加的
 							if(dDbID != 13){
 								continue;
-							}*/
+							}
 							moveDataToTargetDB(dataSource,conn,dDbID,taskId,subtIds,subTaskIdStr,db_conf);
 						}
 					}else{
@@ -245,7 +251,17 @@ public class InitDataPoi2Tab {
 			//10.为POI_TASK_TAB 表打删除标记,位移标记,新增标记
 				PoiTaskTabLogDependent pd = new PoiTaskTabLogDependent();
 				pd.run(conn,objPids,start_date,end_date,dDbID);
-			
+				
+			//11.为中间库的统计表添加数据 :fm_poi_cutout_stat
+				insertFmPoiCutoutStat(conn,taskId);
+				
+			//12.为中间库的统计表添加数据 :fm_poi_cutout
+				//12.1 在中间库向 中间库fm_poi_cutout 插入 pid,taskId,subtaskId,poi_num,
+				insertFmPoiCutout(conn);
+				
+				//12.2 根据 taskid 及 subtaskId 去man 库查询相关数据
+				insertFmPoiCutoutFromMan(conn,taskId,subts);
+				
 		} catch (Exception e) {
 			DbUtils.rollback(conn);
 			System.out.println(e.getMessage());
@@ -256,6 +272,222 @@ public class InitDataPoi2Tab {
 		
 	}
 	
+private static void insertFmPoiCutoutFromMan(Connection conn, int taskId, String subts) throws SQLException {
+		
+		Connection connMan = null;
+		PreparedStatement perstmt = null;
+		try{
+			String updateFmPoiCutoutSql = "update  fm_poi_cutout set subtask_name = ?,task_name = ?,program_id = ?,program_name = ?,inforcode = ?,province = ?  where subtask_id = ? ";
+			//获取man库连接
+			connMan = DBConnector.getInstance().getManConnection();
+			Clob clob = connMan.createClob();
+			clob.setString(1, subts);
+			String selectSql = "  select s.subtask_id,s.subtask_name ,t.task_id,t.name task_name,p.program_id,p.name program_name,p.infor_id inforcode ,c.province_name province  " 
+					+" from ( select s.subtask_id,s.name subtask_name ,"+taskId+" task_id  from subtask s  "
+					+ "where s.task_id = "+taskId+" or s.subtask_id in (select to_number(column_value) from table(clob_to_table(?)) )) s ,"
+					+ "task t,program p,city c "
+					+" where s.task_id =t.task_id(+)  and t.program_id = p.program_id(+) "
+					+"  and p.city_id = c.city_id(+)    ";
+			System.out.println("queryFmPoiCutoutFromMan: "+selectSql);
+			ResultSetHandler< List<Map<String, Object>>> rs = new ResultSetHandler< List<Map<String, Object>>>() {
+				@Override
+				public  List<Map<String, Object>> handle(ResultSet rs) throws SQLException {
+					List<Map<String, Object>> results = new ArrayList<Map<String, Object>>();
+					while(rs.next()){
+						Map<String, Object> map = new HashMap<String, Object>();
+						map.put("subtask_id", rs.getLong("subtask_id"));
+						map.put("subtask_name", rs.getString("subtask_name"));
+						map.put("task_id", rs.getLong("task_id"));
+						map.put("task_name", rs.getString("task_name"));
+						map.put("program_id",  rs.getLong("program_id"));
+						map.put("program_name", rs.getString("program_name"));
+						map.put("inforcode", rs.getLong("inforcode"));
+						map.put("province", rs.getString("province"));
+						
+						results.add(map);
+					}
+					return results;
+				}
+			};
+			QueryRunner run = new QueryRunner();
+			List<Map<String, Object>> result = run.query(connMan,selectSql,clob, rs);
+			
+			if(result != null && result.size() > 0){
+				if(perstmt==null){
+					perstmt = conn.prepareStatement(updateFmPoiCutoutSql);
+				}
+				for(Map<String, Object> fmPoiCutoutMap : result ){
+					//
+					long subtask_id = (long) fmPoiCutoutMap.get("subtask_id");
+					String subtask_name = (String) fmPoiCutoutMap.get("subtask_name");
+//					long task_id = (long) fmPoiCutoutMap.get("task_id");
+					String task_name = (String) fmPoiCutoutMap.get("task_name");
+					long program_id = (long) fmPoiCutoutMap.get("program_id");
+					String program_name = (String) fmPoiCutoutMap.get("program_name");
+					long inforcode = (long) fmPoiCutoutMap.get("inforcode");
+					String province = (String) fmPoiCutoutMap.get("province");
+					
+					
+					perstmt.setString(1,subtask_name);
+					perstmt.setString(2,task_name);
+					perstmt.setLong(3, program_id);
+					perstmt.setString(4,program_name);
+					perstmt.setLong(5, inforcode);
+					perstmt.setString(6,province);
+					perstmt.setLong(7, subtask_id);
+					perstmt.addBatch();
+					
+				}
+				if(perstmt!=null){
+					perstmt.executeBatch();
+					conn.commit();
+				}
+			}
+		}catch (SQLException e) {
+			conn.rollback();
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}finally {
+			DbUtils.close(perstmt);
+			DbUtils.commitAndCloseQuietly(connMan);
+		}
+		
+	}
+
+	private static void insertFmPoiCutout(Connection conn) throws Exception {
+		try {
+			QueryRunner r = new QueryRunner();
+			StringBuilder sb = new StringBuilder();
+			sb.append("declare                                                                                              ");
+			sb.append("    num   number;                                                                                    ");
+			sb.append("begin                                                                                                ");
+			sb.append("    select count(1) into num from user_tables where table_name = upper('fm_poi_cutout') ;    ");
+			sb.append("    if num = 0 then                                                                                  ");
+			sb.append("        execute immediate 'create table fm_poi_cutout (pid  NUMBER(10) not null,poi_num  VARCHAR2(36), "
+					+ "task_id  NUMBER(10) default 0 not null,task_name  VARCHAR2(200) ,gdbversion  VARCHAR2(20) ,"
+					+ "program_id NUMBER(10),program_name  VARCHAR2(200), subtask_id  NUMBER(10) default 0 not null,"
+					+ "subtask_name VARCHAR2(200),region_id   NUMBER(10),province  VARCHAR2(20) ,inforcode  NUMBER(10),"
+					+ "fetchdate  VARCHAR2(50))' ;                                       ");
+			sb.append("    end if;                                                                                          ");
+			sb.append("end;                                                                                                 ");
+			r.execute(conn, sb.toString());
+			
+			String currentDate = com.navinfo.dataservice.commons.util.StringUtils.getCurrentTime();	
+			String gdbVersion = SystemConfigFactory.getSystemConfig().getValue(PropConstant.gdbVersion);
+			String insertFmPoiCutoutSql = " insert into  fm_poi_cutout(gdbversion,pid,poi_num,task_id,subtask_id,fetchdate) "
+					+ "   select '"+gdbVersion+"' gdbversion,p.pid,i.poi_num,p.task_id,p.subtask_id,'"+currentDate+"' fetchdate from poi_task_tab p,ix_poi i where p.pid = i.pid  ";
+			System.out.println("insertFmPoiCutoutSql: "+insertFmPoiCutoutSql.toString());
+			r.execute(conn, insertFmPoiCutoutSql);	
+			conn.commit();
+			
+			System.out.println(" 完毕.");
+		} catch (SQLException e) {
+			conn.rollback();
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+	}
+
+	private static void insertFmPoiCutoutStat(Connection conn, int taskId) throws Exception {
+		PreparedStatement perstmt = null;
+
+		try {
+			QueryRunner r = new QueryRunner();
+			StringBuilder sb = new StringBuilder();
+			sb.append("declare                                                                                              ");
+			sb.append("    num   number;                                                                                    ");
+			sb.append("begin                                                                                                ");
+			sb.append("    select count(1) into num from user_tables where table_name = upper('fm_poi_cutout_stat') ;    ");
+			sb.append("    if num = 0 then                                                                                  ");
+			sb.append("        execute immediate 'create table fm_poi_cutout_stat (id number(10) , task_id    NUMBER(10) default 0 not null,task_poi_num number(6) default 0 not null,subtask_id_num  CLOB,fetchdate  VARCHAR2(50))' ;                                       ");
+			sb.append("        execute immediate 'create sequence fm_poi_cutout_stat_SEQ  minvalue 1 maxvalue 9999999999 start with 1  increment by 1 cache 20 ' ;                                       ");
+			sb.append("    end if;                                                                                          ");
+			sb.append("end;                                                                                                 ");
+			r.execute(conn, sb.toString());
+			
+			String currentDate = com.navinfo.dataservice.commons.util.StringUtils.getCurrentTime();	
+			String insertFmPoiCutoutStat = " insert into  fm_poi_cutout_stat(id,task_id,task_poi_num,subtask_id_num,fetchdate) VALUES(fm_poi_cutout_stat_SEQ.NEXTVAL,?,?,?,'"+currentDate+"')  ";
+			System.out.println("insertFmPoiCutoutStat.toString(): "+insertFmPoiCutoutStat.toString());
+			List<Map<String, Object>>  subtaskIdNums = querysubtaskIdNumsByTaskId(conn,taskId);
+			int task_poi_num =queryTotalByTaskId(conn, taskId);
+				if(perstmt==null){
+					perstmt = conn.prepareStatement(insertFmPoiCutoutStat);
+				}
+				
+				perstmt.setInt(1, taskId);
+				perstmt.setInt(2, task_poi_num);
+				JSONArray jsonArray = JSONArray.fromObject(subtaskIdNums);
+//				JSONObject jsonObject = JSONObject.fromObject(subtaskIdNums);
+				Clob clob = conn.createClob();
+					clob.setString(1, jsonArray.toString());
+				perstmt.setClob(3, clob);
+				
+				if(perstmt!=null){
+					perstmt.execute();
+					conn.commit();
+				}
+			System.out.println(" 完毕.");
+		} catch (SQLException e) {
+			conn.rollback();
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+		
+	}
+	
+	private static List<Map<String, Object>> querysubtaskIdNumsByTaskId(Connection conn, int taskId) throws Exception {
+		try{
+			String selectSql = " select p.subtask_id,count(1) NUM  from POI_TASK_TAB p WHERE P.TASK_ID = "+taskId+"  group by p.subtask_id  ";
+			System.out.println("querysubtaskIdNumsByTaskId: "+selectSql);
+			ResultSetHandler< List<Map<String, Object>>> rs = new ResultSetHandler< List<Map<String, Object>>>() {
+				@Override
+				public  List<Map<String, Object>> handle(ResultSet rs) throws SQLException {
+					List<Map<String, Object>> results = new ArrayList<Map<String, Object>>();
+					while(rs.next()){
+						Map<String, Object> map = new HashMap<String, Object>();
+						map.put("subtask_id", rs.getLong("subtask_id"));
+						map.put("num",  rs.getInt("num"));
+						results.add(map);
+					}
+					return results;
+				}
+			};
+			QueryRunner run = new QueryRunner();
+			List<Map<String, Object>> result = run.query(conn,selectSql, rs);
+			return result;
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+			throw new Exception("查询subtaskIdNums 失败，原因为:"+e.getMessage(),e);
+		}finally{
+//			DbUtils.commitAndCloseQuietly(conn);
+		}
+	}
+	
+	private static int queryTotalByTaskId(Connection conn, int taskId) throws Exception {
+		try{
+			String selectSql = " select count(1) total  from POI_TASK_TAB p WHERE P.TASK_ID = "+taskId+" ";
+			System.out.println("queryTotalByTaskId: "+selectSql);
+			ResultSetHandler< Integer> rs = new ResultSetHandler< Integer>() {
+				@Override
+				public  Integer handle(ResultSet rs) throws SQLException {
+					int result = 0;
+					while(rs.next()){
+						result =  rs.getInt("total");
+					}
+					return result;
+				}
+			};
+			QueryRunner run = new QueryRunner();
+			int result = run.query(conn,selectSql, rs);
+			return result;
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+			throw new Exception("查询subtaskIdNums 失败，原因为:"+e.getMessage(),e);
+		}finally{
+//			DbUtils.commitAndCloseQuietly(conn);
+		}
+	}
+
 	/**
 	 * @Title: insertDataTotable
 	 * @Description: 向大区库的中间表中添加数据
@@ -275,10 +507,10 @@ public class InitDataPoi2Tab {
 			Clob subtsClob = ConnectionUtil.createClob(conn);
 			subtsClob.setString(1, subts);
 			StringBuilder insertPoiTaskTempDataSql = new StringBuilder();
-			insertPoiTaskTempDataSql.append( " insert into POI_TASK_TEMP (pid,task_id) select distinct pid, "+taskId+" task  from ("
-				+ "  select s.pid   from poi_edit_status s where s.quick_subtask_id  in (select to_number(column_value) from table(clob_to_table(?))) "
+			insertPoiTaskTempDataSql.append( " insert into POI_TASK_TEMP (pid,task_id,subtask_id) select distinct pid, "+taskId+" task,subtask_id  from ("
+				+ "  select s.pid,s.quick_subtask_id subtask_id   from poi_edit_status s where s.quick_subtask_id  in (select to_number(column_value) from table(clob_to_table(?))) "
 				+ " 	union all "
-				+ "  select s.pid   from poi_edit_status s where s.medium_subtask_id in (select to_number(column_value) from table(clob_to_table(?))) "
+				+ "  select s.pid,s.medium_subtask_id subtask_id   from poi_edit_status s where s.medium_subtask_id in (select to_number(column_value) from table(clob_to_table(?))) "
 				+ " ) ");
 		
 			System.out.println("insertPoiTaskTempDataSql.toString(): "+insertPoiTaskTempDataSql.toString());
@@ -620,6 +852,7 @@ public class InitDataPoi2Tab {
 		sb.append("(                                      ");
 		sb.append("  pid     NUMBER(10) not null,         ");
 		sb.append("  task_id NUMBER(10) default 0 not null,");
+		sb.append("  subtask_id NUMBER(10) default 0 not null,");
 		sb.append("  constraint PK_POI_TASK_TEMP primary key (PID) ");
 		sb.append(")      ");
 		r.execute(conn, sb.toString());

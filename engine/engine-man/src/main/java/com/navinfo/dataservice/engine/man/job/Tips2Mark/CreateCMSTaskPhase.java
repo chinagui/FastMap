23506e9748ea6261ba174c1bc0faf8109b3788f4
Hansong9527 <825762215@qq.com>
2017-10-18 10:30:06 +0800

    修正bug

--- engine/engine-man/src/main/java/com/navinfo/dataservice/engine/man/job/Tips2Mark/CreateCMSTaskPhase.java
+++ engine/engine-man/src/main/java/com/navinfo/dataservice/engine/man/job/Tips2Mark/CreateCMSTaskPhase.java
@@ -280,11 +280,11 @@ public class CreateCMSTaskPhase extends JobPhase {
             	
             	log.info("querypoiSql :" + poiSql);
             	
-            	ResultSetHandler<Map<Integer, Integer>> handler = new ResultSetHandler<Map<Integer, Integer>>() {
-            		public Map<Integer, Integer> handle(ResultSet rs) throws SQLException {
-            			Map<Integer, Integer> meshesCount = new HashMap<>();
+            	ResultSetHandler<Map<String, Integer>> handler = new ResultSetHandler<Map<String, Integer>>() {
+            		public Map<String, Integer> handle(ResultSet rs) throws SQLException {
+            			Map<String, Integer> meshesCount = new HashMap<>();
             			while(rs.next()) {
-            				meshesCount.put(rs.getInt("mesh_id"), rs.getInt("count(1)"));
+            				meshesCount.put(rs.getString("mesh_id"), rs.getInt("count(1)"));
             			}
             			return meshesCount;
             		}

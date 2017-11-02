558784303b8789079483bd84cc4bfd2c5753f0f5
zhaokaikai <zhaokaikai@navinfo.com>
2017-11-02 16:26:39 +0800

    改精度

--- dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/plus/model/basic/AbstractIx.java
+++ dao/dao-glm-data/src/main/java/com/navinfo/dataservice/dao/plus/model/basic/AbstractIx.java
@@ -66,6 +66,7 @@ public abstract class AbstractIx extends BasicRow {
 		if (this.checkValue("GEOMETRY", this.geometry, geometry)) {
 			this.geometry = geometry;
 		}
+		
 	}
 
 	public double getXGuide() {

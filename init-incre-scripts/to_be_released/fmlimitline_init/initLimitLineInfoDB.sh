cf9bc9d2b17d52847ab18416ece3c5363188749d
zhangli5174 <zhangli5174@navinfo.com>
2017-11-10 11:13:26 +0800

    整理数据库文件夹

--- init-incre-scripts/to_be_released/fmlimitline_init/initLimitLineInfoDB.sh
+++ init-incre-scripts/to_be_released/fmlimitline_init/initLimitLineInfoDB.sh
@@ -1,9 +1,7 @@
 export NLS_LANG=.AL32UTF8
 source ./initLimitLineInfoDB.conf
 
-sqlplus $manager_username/$manager_password@$limitlineinfo_ip @./createLimitLineInfoUser.sql $limitlineinfo_user
-sqlplus $fmsys_url @./limitLineInfoInsertDbhub.sql $limitlineinfo_user $limitlineinfo_server_biz
-sqlplus $limitlineinfo_user/$limitlineinfo_user@$limitlineinfo_ip @./create_info_table.sql
+sqlplus $limitlineinfo_url @./create_info_table_1109.sql
 
 
 
